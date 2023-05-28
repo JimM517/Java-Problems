d15098d836d1df3fcdc767c94b741824b374d6ac
JimM517 <80083198+JimM517@users.noreply.github.com>
2023-05-28 19:57:12 -0400

    Test commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Exercises exercises = new Exercises();
+
+        int[] nums = {2, 4, 5, 18, 20, 45, 2, 10, 6};
+
+        int[] testArr = {1, 4, 5, 10, 40, 20, 17};
+
+        System.out.println(Arrays.toString(exercises.fizzArrayThree(5, 10)));
+    }
+}
