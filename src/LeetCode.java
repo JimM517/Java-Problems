58d36ceeee4595a9a17e4f7ae0a70fa32613c5d0
JimM517 <80083198+JimM517@users.noreply.github.com>
2023-08-23 21:43:37 -0400

    Completed three sum

--- src/LeetCode.java
+++ src/LeetCode.java
@@ -601,6 +601,52 @@ public List<List<Integer>> minimumAbsDifference(int[] arr) {
     }
 
 
+    // 15 3Sum
+    // Given an integer array nums, return all the triplets
+    // [nums[i], nums[j], nums[k]]
+    // such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0
+
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> results = new ArrayList<>();
+
+        // sort input array
+        Arrays.sort(nums);
+
+        for (int i = 0; i < nums.length - 2; i++) {
+            // we already saw this value, skip
+            if (i > 0 && (nums[i] == nums[i - 1])) {
+                continue;
+            }
+            int left = i + 1;
+            int right = nums.length - 1;
+            while(left < right) {
+                int threeSum = nums[i] + nums[left] + nums[right];
+                if (threeSum == 0) {
+                    results.add(Arrays.asList(nums[i], nums[left], nums[right]));
+                    while (left < right && nums[left] == nums[left + 1]) {
+                        left++;
+                    }
+                    while (left < right && nums[right] == nums[right - 1]) {
+                        right--;
+                    }
+                    left++;
+                    right--;
+                } else if (threeSum < 0) {
+                    left++;
+                } else {
+                    right--;
+                }
+            }
+
+        }
+
+
+        return results;
+
+
+    }
+
+
 
 
 
