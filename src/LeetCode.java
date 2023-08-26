cb73cd59d2eceb0942e442278edddb0c651580f3
JimM517 <80083198+JimM517@users.noreply.github.com>
2023-08-26 10:22:58 -0400

    Attempted valid parentheses

--- src/LeetCode.java
+++ src/LeetCode.java
@@ -714,6 +714,36 @@ public List<List<Integer>> minimumAbsDifference(int[] arr) {
     }
 
 
+    // 20 Valid Parentheses
+    public boolean isValid(String s) {
+
+        Stack<Character> stack = new Stack<>();
+
+
+        for (char ch : s.toCharArray()) {
+            if (ch == '(' || ch == '[' || ch == '{') {
+                stack.push(ch);
+            } else if (ch == ')' || ch == ']' || ch == '}') {
+                if (stack.isEmpty()) {
+                    return false;
+                }
+
+                char top = stack.pop();
+                if ((ch == ')' && top != '(') || (ch == ']' && top != '[') || (ch == '}' && top != '{')) {
+                    return false;
+                }
+
+            }
+        }
+
+        return stack.isEmpty() ? true : false;
+
+
+
+    }
+
+
+
 
 
 
