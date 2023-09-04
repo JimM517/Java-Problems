49539a2cb1a8ed6dc86836ab99d6d6d3028695ed
JimM517 <80083198+JimM517@users.noreply.github.com>
2023-09-04 18:15:36 -0400

    Completed evalRPN, with help

--- src/LeetCode.java
+++ src/LeetCode.java
@@ -927,6 +927,38 @@ public List<List<Integer>> minimumAbsDifference(int[] arr) {
 
 
 
+    // 150 Evaluate Reverse Polish Notation
+    public int evalRPN(String[] tokens) {
+
+        Stack<Integer> stack = new Stack<>();
+
+        for (int i = 0; i < tokens.length; i++) {
+            if (tokens[i].equals("+")) {
+                stack.push(stack.pop() + stack.pop());
+            } else if (tokens[i].equals("-")) {
+                int a = stack.pop();
+                int b = stack.pop();
+                stack.push(b - a);
+            } else if (tokens[i].equals("*")) {
+                stack.push(stack.pop() * stack.pop());
+            } else if (tokens[i].equals("/")) {
+                int a = stack.pop();
+                int b = stack.pop();
+                stack.push(b / a);
+            } else {
+                stack.push(Integer.parseInt(tokens[i]));
+            }
+        }
+
+        return stack.get(0);
+
+
+    }
+
+
+
+
 
 
 }
+;
