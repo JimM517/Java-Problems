b5397dcbb4a115727e6569fd8b22c8b5330a1a16
JimM517 <80083198+JimM517@users.noreply.github.com>
2023-09-20 21:42:43 -0400

    Completed sign of product array

--- src/LeetCode.java
+++ src/LeetCode.java
@@ -1148,6 +1148,24 @@ public List<List<Integer>> minimumAbsDifference(int[] arr) {
     }
 
 
+    // 1822 Sign of the Product of An Array
+
+    public int arraySign(int[] nums) {
+
+        int sign = 1;
+
+        for (int num : nums) {
+            if (num == 0) {
+                return 0;
+            } else if (num < 0) {
+                sign = -sign;
+            }
+        }
+
+        return sign;
+
+
+    }
 
 
 
