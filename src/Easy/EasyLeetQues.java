04e60255cb2e133643931fb0442f86baff3c2ee1
JimM517 <80083198+JimM517@users.noreply.github.com>
2023-09-21 20:42:52 -0400

    Completed roman to integer

---
+++ src/Easy/EasyLeetQues.java
@@ -0,0 +1,46 @@
+package Easy;
+
+import java.util.HashMap;
+
+public class EasyLeetQues {
+
+    // 13 Roman to Integer
+
+    public int romanToInt(String s) {
+
+        HashMap<Character, Integer> map = new HashMap<>();
+        map.put('I', 1);
+        map.put('V', 5);
+        map.put('X', 10);
+        map.put('L', 50);
+        map.put('C', 100);
+        map.put('D', 500);
+        map.put('M', 1000);
+
+        int ans = 0;
+
+        for(int i=0;i<s.length()-1;i++){
+            char ch= s.charAt(i);
+            char chc= s.charAt(i+1);
+            if(ch=='I' && (chc=='V' || chc=='X')){
+                ans+=-1*map.get(ch);
+            }
+            else if(ch=='X' && (chc=='L' || chc=='C')){
+                ans+=-1*map.get(ch);
+            }
+            else if(ch=='C' && (chc=='D' || chc=='M')){
+                ans+=-1*map.get(ch);
+            }
+            else ans+=map.get(ch);
+        }
+        char ch=s.charAt(s.length()-1);
+        ans+=map.get(ch);
+        return ans;
+    }
+
+
+
+
+
+
+}
