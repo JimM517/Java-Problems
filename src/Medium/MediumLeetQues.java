c1f8adeb4a10590387b61113fea39edae592eea0
JimM517 <80083198+JimM517@users.noreply.github.com>
2023-09-21 20:56:00 -0400

    Completed reverse int

--- src/Medium/MediumLeetQues.java
+++ src/Medium/MediumLeetQues.java
@@ -7,6 +7,29 @@ public class MediumLeetQues {
 
     public int reverse(int x) {
 
+        boolean isNegative = x < 0;
+
+
+        long num = Math.abs((long) x);
+
+
+        long reversed = 0;
+        while (num > 0) {
+            reversed = reversed * 10 + num % 10;
+            num /= 10;
+        }
+
+
+        if (reversed > Integer.MAX_VALUE || reversed < Integer.MIN_VALUE) {
+            return 0;
+        }
+
+
+        if (isNegative) {
+            reversed = -reversed;
+        }
+
+        return (int) reversed;
 
 
     }
