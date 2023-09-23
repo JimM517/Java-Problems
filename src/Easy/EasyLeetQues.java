95ca8f43ceec3e0594b11da326101c838c01aab3
JimM517 <80083198+JimM517@users.noreply.github.com>
2023-09-23 19:55:57 -0400

    Completed move zeroes

--- src/Easy/EasyLeetQues.java
+++ src/Easy/EasyLeetQues.java
@@ -40,6 +40,26 @@ public class EasyLeetQues {
 
 
 
+    // 283 Move Zeroes
+    public void moveZeroes(int[] nums) {
+
+            int start = 0;
+
+
+            for (int i = 0; i < nums.length; i++) {
+                if (nums[i] != 0) {
+                    if (i != start) {
+                        int temp = nums[start];
+                        nums[start] = nums[i];
+                        nums[i] = temp;
+                    }
+                }
+                start++;
+            }
+
+
+    }
+
 
 
 
