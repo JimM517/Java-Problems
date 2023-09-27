2a43ea5f44e543423349fc192db741be00ce96b1
JimM517 <80083198+JimM517@users.noreply.github.com>
2023-09-27 19:25:45 -0400

    Completed max profit

--- src/InterviewQuestions/InterviewPrep.java
+++ src/InterviewQuestions/InterviewPrep.java
@@ -113,4 +113,30 @@ public class InterviewPrep {
 
 
 
+    // 121 Best Time to Buy and Sell Stock
+
+    public int maxProfit(int[] prices) {
+
+       if (prices.length == 0) {
+           return 0;
+       }
+
+       int start = prices[0];
+       int maxProfit = 0;
+
+       for (int i = 0; i < prices.length; i++) {
+           if (prices[i] < start) {
+               start = prices[i];
+           } else {
+               int currProfit = prices[i] - start;
+               if (currProfit > maxProfit) {
+                   maxProfit = currProfit;
+               }
+           }
+       }
+       return maxProfit;
+    }
+
+
+
 }
