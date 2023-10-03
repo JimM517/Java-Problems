08fee60b3b19530b8aaeb3fd72e2eafa757c16db
JimM517 <80083198+JimM517@users.noreply.github.com>
2023-10-02 22:19:40 -0400

    Added recursion section

---
+++ src/Recursion/RecursiveProbs.java
@@ -0,0 +1,21 @@
+package Recursion;
+
+public class RecursiveProbs {
+
+    // factorial
+    public int factorial(int n) {
+        if (n == 1) {
+            return 1;
+        }
+        return n * factorial(n - 1);
+    }
+
+
+
+
+
+
+
+
+
+}
