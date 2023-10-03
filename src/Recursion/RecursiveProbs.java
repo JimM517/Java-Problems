76737492cb1cb9ac898345b53b994e595c5bba90
JimM517 <80083198+JimM517@users.noreply.github.com>
2023-10-02 22:30:48 -0400

    Completed bunny ears 2

--- src/Recursion/RecursiveProbs.java
+++ src/Recursion/RecursiveProbs.java
@@ -11,6 +11,24 @@ public class RecursiveProbs {
     }
 
 
+    // Bunny Ears
+    public int bunnyEars2(int bunnies) {
+        // base case
+        if (bunnies == 0) {
+            return 0;
+        }
+
+        if (bunnies % 2 == 0) {
+            return 3 + bunnyEars2(bunnies - 1);
+        } else {
+            return 2 + bunnyEars2(bunnies - 1);
+        }
+
+
+
+    }
+
+
 
 
 
