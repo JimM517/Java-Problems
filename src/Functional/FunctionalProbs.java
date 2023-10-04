da3e1f3e5eeb8cec7a5db613dc3eb79120c2a678
JimM517 <80083198+JimM517@users.noreply.github.com>
2023-10-03 21:52:47 -0400

    Completed doubling, working with lambdas

--- src/Functional/FunctionalProbs.java
+++ src/Functional/FunctionalProbs.java
@@ -1,4 +1,25 @@
 package Functional;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class FunctionalProbs {
+
+    // doubling
+
+    public List<Integer> doubling(List<Integer> nums) {
+        return nums.stream() // convert list into stream of integers
+                .map(num -> num * 2) // map operation to double each element
+                .collect(Collectors.toList()); // collect modified elements into new list
+    }
+
+
+
+
+
+
+
+
+
+
 }
