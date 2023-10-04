bd2c10517bb161791a4908a18da4c987c1c31934
JimM517 <80083198+JimM517@users.noreply.github.com>
2023-10-03 21:58:14 -0400

    Completed square

--- src/Functional/FunctionalProbs.java
+++ src/Functional/FunctionalProbs.java
@@ -6,14 +6,24 @@ import java.util.stream.Collectors;
 public class FunctionalProbs {
 
     // doubling
-
     public List<Integer> doubling(List<Integer> nums) {
+        // solution one
+        // nums.replaceAll(num -> num * 2);
+        // return nums;
+
+        // solution two
         return nums.stream() // convert list into stream of integers
                 .map(num -> num * 2) // map operation to double each element
                 .collect(Collectors.toList()); // collect modified elements into new list
     }
 
 
+    // square
+    public List<Integer> square(List<Integer> nums) {
+        return nums.stream()
+                .map(num -> num * num)
+                .collect(Collectors.toList());
+    }
 
 
 
