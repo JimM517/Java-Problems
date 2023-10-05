a68392329013f5bab932871aed36f2f40990bcdb
JimM517 <80083198+JimM517@users.noreply.github.com>
2023-10-04 21:48:15 -0400

    Completed reverse linked list

--- src/Easy/EasyLeetQues.java
+++ src/Easy/EasyLeetQues.java
@@ -84,6 +84,23 @@ public class EasyLeetQues {
     }
 
 
+    // 206 Reverse Linked List
+//    public ListNode reverseList(ListNode head) {
+//        ListNode prev = null;
+//        ListNode curr = head;
+//
+//        while(curr != null) {
+//            ListNode next = curr.next;
+//            curr.next = prev;
+//            prev = curr;
+//            curr = next;
+//        }
+//        return prev;
+//
+//
+//    }
+
+
 
 
 
