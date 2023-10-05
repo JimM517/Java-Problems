c277e6b0c742ca766ed1c218cc5e82408f67ad3b
JimM517 <80083198+JimM517@users.noreply.github.com>
2023-10-04 21:23:07 -0400

    Completed math 1

--- src/Functional/FunctionalProbs.java
+++ src/Functional/FunctionalProbs.java
@@ -53,6 +53,21 @@ public class FunctionalProbs {
 
 
 
+    // math1
+    public List<Integer> math1(List<Integer> nums) {
+
+        return nums.stream()
+                .map(num -> (num + 1) * 10)
+                .collect(Collectors.toList());
+
+
+    }
+
+
+    //
+
+
+
 
 
 }
