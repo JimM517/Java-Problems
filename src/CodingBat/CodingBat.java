590feb2dd1e40f576c27ced3e2ddd779211eb5b8
JimM517 <80083198+JimM517@users.noreply.github.com>
2023-10-05 21:09:34 -0400

    Started some map problems from coding bat

--- src/CodingBat/CodingBat.java
+++ src/CodingBat/CodingBat.java
@@ -1,6 +1,23 @@
 package CodingBat;
 
+import java.util.Map;
+
 public class CodingBat {
+
+
+    // mapBully
+    public Map<String, String> mapBully(Map<String, String> map) {
+
+        if (map.containsKey("a")) {
+            map.put("b", map.get("a"));
+
+            map.put("a", "");
+        }
+
+        return map;
+
+    }
+
 
 
 
