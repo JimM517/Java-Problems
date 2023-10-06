1af33c3df0038e4394255cd7d4cc2397753dd786
JimM517 <80083198+JimM517@users.noreply.github.com>
2023-10-05 21:16:21 -0400

    Completed map share

--- src/CodingBat/CodingBat.java
+++ src/CodingBat/CodingBat.java
@@ -20,6 +20,20 @@ public class CodingBat {
 
 
 
+    // mapShare
+    public Map<String, String> mapShare(Map<String, String> map) {
+
+        if (map.containsKey("a")) {
+            map.put("b", map.get("a"));
+        }
+
+        map.remove("c");
+
+        return map;
+
+    }
+
+
 
 
 
