590bba633f793661c7fc721ea3767ab71c7f3a0a
JimM517 <80083198+JimM517@users.noreply.github.com>
2023-10-05 21:57:52 -0400

    Completed topping1

--- src/CodingBat/CodingBat.java
+++ src/CodingBat/CodingBat.java
@@ -47,6 +47,21 @@ public class CodingBat {
     }
 
 
+    // topping1
+    public Map<String, String> topping1(Map<String, String> map) {
+
+        if (map.containsKey("ice cream")) {
+            map.put("ice cream", "cherry");
+        }
+        map.put("bread", "butter");
+
+        return map;
+
+
+
+    }
+
+
 
 
 
