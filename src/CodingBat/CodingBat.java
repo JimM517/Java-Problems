faea9acb6e7a77540598e4607a9a80924bd1ee77
JimM517 <80083198+JimM517@users.noreply.github.com>
2023-10-06 21:57:38 -0400

    Completed mapAB2

--- src/CodingBat/CodingBat.java
+++ src/CodingBat/CodingBat.java
@@ -92,6 +92,21 @@ public class CodingBat {
     }
 
 
+    // mapAB2
+    public Map<String, String> mapAB2(Map<String, String> map) {
+
+        if ((map.containsKey("a") && map.containsKey("b")) && (map.get("a").equals(map.get("b")))) {
+            map.remove("a");
+            map.remove("b");
+        }
+        return map;
+
+    }
+
+
+
+
+
 
 
 
