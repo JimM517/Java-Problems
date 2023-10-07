f29e718b9c730e430967ae8e04e29b36126790d3
JimM517 <80083198+JimM517@users.noreply.github.com>
2023-10-06 22:00:47 -0400

    Completed mapAB3

--- src/CodingBat/CodingBat.java
+++ src/CodingBat/CodingBat.java
@@ -104,6 +104,26 @@ public class CodingBat {
     }
 
 
+    // mapAB3
+    public Map<String, String> mapAB3(Map<String, String> map) {
+
+        if (map.containsKey("a") && !map.containsKey("b")) {
+            map.put("b", map.get("a"));
+        }
+
+        if (map.containsKey("b") && !map.containsKey("a")) {
+            map.put("a", map.get("b"));
+        }
+        return map;
+
+
+    }
+
+
+
+
+
+
 
 
 
