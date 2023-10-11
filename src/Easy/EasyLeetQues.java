a9b04c9140c1cc754d8aa9ebf24854d90cc4c745
JimM517 <80083198+JimM517@users.noreply.github.com>
2023-10-11 19:37:39 -0400

    Completed richest customer wealth

--- src/Easy/EasyLeetQues.java
+++ src/Easy/EasyLeetQues.java
@@ -118,6 +118,34 @@ public class EasyLeetQues {
 
 
 
+    // 1672 richest customer wealth
+
+    public int maximumWealth(int[][] accounts) {
+
+        int max = 0;
+
+        for (int i = 0; i < accounts.length; i++) {
+            int temp = 0;
+            for (int j = 0; j < accounts[i].length; j++) {
+                temp += accounts[i][j];
+            }
+
+
+            if (temp > max) {
+                max = temp;
+            }
+        }
+        return max;
+
+    }
+
+
+
+
+
+
+
+
 
 
 
