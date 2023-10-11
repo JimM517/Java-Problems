70f118840d900aa4d152df4e8daaa14f0d6050ee
JimM517 <80083198+JimM517@users.noreply.github.com>
2023-10-10 21:13:36 -0400

    Completed noZ

--- src/Functional/FunctionalProbs.java
+++ src/Functional/FunctionalProbs.java
@@ -123,6 +123,17 @@ public class FunctionalProbs {
 
 
 
+    // noZ
+    public List<String> noZ(List<String> strings) {
+
+        strings.removeIf(str -> str.contains("z"));
+
+        return strings;
+
+    }
+
+
+
 
 
 
