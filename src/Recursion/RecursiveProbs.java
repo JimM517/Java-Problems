b1616348f233bf8e0c09d048088906adcaa10a68
JimM517 <80083198+JimM517@users.noreply.github.com>
2023-10-10 21:03:02 -0400

    Fixed error?

--- src/Recursion/RecursiveProbs.java
+++ src/Recursion/RecursiveProbs.java
@@ -48,7 +48,7 @@ public class RecursiveProbs {
         if (n % 10 == 7) {
             return 1 + count7(n / 10);
         } else {
-            return count7(n / 10);if
+            return count7(n / 10);
         }
 
 
