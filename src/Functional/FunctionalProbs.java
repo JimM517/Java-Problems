df65267a2968d39d86104d8f101f15cfcfcaaaf1
JimM517 <80083198+JimM517@users.noreply.github.com>
2023-10-10 22:05:53 -0400

    Completed no34

--- src/Functional/FunctionalProbs.java
+++ src/Functional/FunctionalProbs.java
@@ -142,6 +142,18 @@ public class FunctionalProbs {
     }
 
 
+    // no34
+    public List<String> no34(List<String> strings) {
+
+
+        strings.removeIf(str -> str.length() == 3 || str.length() == 4);
+        return strings;
+
+    }
+
+
+
+
 
 
 
