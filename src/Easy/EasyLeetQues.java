58a50a09e132b24240466a638890c502e0a6e469
JimM517 <80083198+JimM517@users.noreply.github.com>
2023-10-11 21:01:44 -0400

    Completed numJewels

--- src/Easy/EasyLeetQues.java
+++ src/Easy/EasyLeetQues.java
@@ -1,6 +1,8 @@
 package Easy;
 
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
 
 public class EasyLeetQues {
 
@@ -173,7 +175,28 @@ public class EasyLeetQues {
         }
         return result;
 
+    }
 
+
+    // 771 Jewels and Stones
+    public int numJewelsInStones(String jewels, String stones) {
+
+        Set<Character> jewelSet = new HashSet<>();
+
+
+        for (char jewel : jewels.toCharArray()) {
+            jewelSet.add(jewel);
+        }
+
+        int count = 0;
+
+        for (char stone : stones.toCharArray()) {
+            if (jewelSet.contains(stone)) {
+                count++;
+            }
+        }
+
+        return count;
 
 
     }
