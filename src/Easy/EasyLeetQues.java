b122c6695d4cc63b0db79c44ffbbd14a326ade87
JimM517 <80083198+JimM517@users.noreply.github.com>
2023-10-12 21:20:57 -0400

    Completed smaller numbers than current

--- src/Easy/EasyLeetQues.java
+++ src/Easy/EasyLeetQues.java
@@ -239,6 +239,23 @@ public class EasyLeetQues {
 //    }
 
 
+    // 1365 How many numbers are smaller than the current number
+    public int[] smallerNumbersThanCurrent(int[] nums) {
+
+        int[] result = new int[nums.length];
+
+        for (int i = 0; i < nums.length; i++) {
+            int count = 0;
+            for (int j = 0; j < nums.length; j++) {
+                if (nums[j] < nums[i] && j != i) {
+                    count++;
+                }
+            }
+            result[i] = count;
+        }
+
+        return result;
+    }
 
 
 
