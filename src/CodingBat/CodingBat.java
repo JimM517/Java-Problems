df260c46a21a05846cb721239967dd96e17ca28b
JimM517 <80083198+JimM517@users.noreply.github.com>
2023-10-14 10:06:29 -0400

    Completed pairs

--- src/CodingBat/CodingBat.java
+++ src/CodingBat/CodingBat.java
@@ -170,7 +170,18 @@ public class CodingBat {
     }
 
 
+    // pairs
+    public Map<String, String> pairs(String[] strings) {
 
+        Map<String, String> results = new HashMap<>();
+
+
+        for (int i = 0; i < strings.length; i++) {
+            results.put(strings[i].substring(0, 1), strings[i].substring(strings[i].length() - 1));
+        }
+
+        return results;
+    }
 
 
 
