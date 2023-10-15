c7673bf15282de00ce7a3139bc06b5bec264eff5
JimM517 <80083198+JimM517@users.noreply.github.com>
2023-10-15 19:15:00 -0400

    Completed scores increasing

--- src/CodingBat/CodingBat.java
+++ src/CodingBat/CodingBat.java
@@ -324,6 +324,21 @@ public class CodingBat {
     }
 
 
+    // scoresIncreasing
+    public boolean scoresIncreasing(int[] scores) {
+
+        for (int i = 0; i < scores.length - 1; i++) {
+            if (scores[i] > scores[i + 1]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+
+
+
+
 
 
 
