8a7b70866d1f4de2743713e6db04e400ddc85205
JimM517 <80083198+JimM517@users.noreply.github.com>
2023-10-15 19:22:18 -0400

    Completed scores 100

--- src/CodingBat/CodingBat.java
+++ src/CodingBat/CodingBat.java
@@ -336,7 +336,20 @@ public class CodingBat {
     }
 
 
+    // scores100
+    public boolean scores100(int[] scores) {
 
+        for (int i = 0; i < scores.length - 1; i++) {
+
+            if ((scores[i] == 100) && (scores[i + 1] == 100)) {
+                return true;
+            }
+
+
+        }
+
+        return false;
+    }
 
 
 
