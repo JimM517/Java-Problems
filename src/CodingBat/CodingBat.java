4e4cfc4f9c9e3d3881ad9d992537bad14c962363
JimM517 <80083198+JimM517@users.noreply.github.com>
2023-10-16 19:02:09 -0400

    Completed wordsFront

--- src/CodingBat/CodingBat.java
+++ src/CodingBat/CodingBat.java
@@ -416,6 +416,25 @@ public class CodingBat {
 
 
 
+    // wordsFront
+    public String[] wordsFront(String[] words, int n) {
+
+        String[] results = new String[n];
+
+        for (int i = 0; i < n; i++) {
+            results[i] = words[i];
+        }
+        return results;
+
+    }
+
+
+
+
+
+
+
+
 
 
 
