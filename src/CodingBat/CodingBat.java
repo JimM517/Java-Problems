4049e6702931703434fc2637b48252974cf1de89
JimM517 <80083198+JimM517@users.noreply.github.com>
2023-10-16 18:57:51 -0400

    Completed scores Average

--- src/CodingBat/CodingBat.java
+++ src/CodingBat/CodingBat.java
@@ -369,6 +369,33 @@ public class CodingBat {
 
 
 
+    // scoresAverage
+    public int scoresAverage(int[] scores) {
+
+        int firstHalf = average(scores, 0, scores.length / 2);
+        int secondHalf = average(scores, scores.length / 2, scores.length);
+
+        if (firstHalf > secondHalf) {
+            return firstHalf;
+        } else {
+            return secondHalf;
+        }
+
+    }
+
+    private int average(int[] scores, int start, int end) {
+        int sum = 0;
+        for (int i = start; i < end; i++) {
+            sum += scores[i];
+        }
+        // number of scores that are calculated in the average
+        int count = end - start;
+
+        return sum / count;
+
+    }
+
+
 
 
 
