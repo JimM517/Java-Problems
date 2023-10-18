1ea67677254b28705ca2a9010d241d8b7461782c
JimM517 <80083198+JimM517@users.noreply.github.com>
2023-10-17 21:04:05 -0400

    completed copyEndy

--- src/CodingBat/CodingBat.java
+++ src/CodingBat/CodingBat.java
@@ -505,6 +505,33 @@ public class CodingBat {
     }
 
 
+    // copyEndy
+    public int[] copyEndy(int[] nums, int count) {
+
+        int[] result = new int[count];
+        int endyCount = 0;
+
+        for (int i = 0; endyCount < count && i < nums.length; i++) {
+            if (isEndy(nums[i])) {
+                result[endyCount] = nums[i];
+                endyCount++;
+            }
+        }
+        return result;
+
+    }
+
+
+    private boolean isEndy(int n) {
+
+        if ((n >= 0 && n <= 10) || (n >= 90 && n <= 100)) {
+            return true;
+        }
+        return false;
+
+    }
+
+
 
 
 
