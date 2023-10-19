1caa6de7b189d792eabdeb2e467f91751ac380f5
JimM517 <80083198+JimM517@users.noreply.github.com>
2023-10-18 20:18:14 -0400

    Completed wordsWithout

--- src/CodingBat/CodingBat.java
+++ src/CodingBat/CodingBat.java
@@ -573,6 +573,35 @@ public class CodingBat {
     }
 
 
+    // wordsWithout
+    public String[] wordsWithout(String[] words, String target) {
+
+        int count = 0;
+
+
+
+        for (int i = 0; i < words.length; i++) {
+            if (words[i].equals(target)) {
+                count++;
+            }
+        }
+
+        String[] result = new String[words.length - count];
+
+        int j = 0;
+        for (int i = 0; i < words.length; i++) {
+            if (!words[i].equals(target)) {
+                result[j] = words[i];
+                j++;
+            }
+        }
+
+
+        return result;
+    }
+
+
+
 
 
 
