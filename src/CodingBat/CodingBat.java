1edd8a663752eada399a153452205c41bbae5cbb
JimM517 <80083198+JimM517@users.noreply.github.com>
2023-10-19 21:36:01 -0400

    Completed bigDiff

--- src/CodingBat/CodingBat.java
+++ src/CodingBat/CodingBat.java
@@ -700,6 +700,23 @@ public class CodingBat {
 
     }
 
+    // bigDiff
+    public int bigDiff(int[] nums) {
+
+        int smallest = nums[0];
+        int largest = nums[0];
+
+        for (int i = 1; i < nums.length; i++) {
+            smallest = Math.min(smallest, nums[i]);
+            largest = Math.max(largest, nums[i]);
+        }
+        return largest - smallest;
+
+    }
+
+
+
+
 
 
 
