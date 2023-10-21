aba43e3951b2513bd659bce3321e3769175cec4f
JimM517 <80083198+JimM517@users.noreply.github.com>
2023-10-20 21:48:56 -0400

    Completed sum13

--- src/CodingBat/CodingBat.java
+++ src/CodingBat/CodingBat.java
@@ -759,7 +759,22 @@ public class CodingBat {
     }
 
 
-    //
+    // sum13
+    public int sum13(int[] nums) {
+
+        int total = 0;
+        int i = 0;
+
+        while (i < nums.length) {
+            if (nums[i] == 13) {
+                i += 2;
+            } else {
+                total += nums[i];
+                i++;
+            }
+        }
+        return total;
+    }
 
 
 
