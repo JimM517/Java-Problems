f81036731bba94870d0f2e45225d1f4f7ac09485
JimM517 <80083198+JimM517@users.noreply.github.com>
2023-10-20 21:42:24 -0400

    Completed centered average

--- src/CodingBat/CodingBat.java
+++ src/CodingBat/CodingBat.java
@@ -737,6 +737,31 @@ public class CodingBat {
     }
 
 
+    // centeredAverage
+    public int centeredAverage(int[] nums) {
+
+      // find smallest and largest in array
+      int max = nums[0];
+      int min = nums[0];
+
+      int sum = 0;
+
+      for (int i = 0; i < nums.length; i++) {
+          sum += nums[i];
+
+          max = Math.max(max, nums[i]);
+          min = Math.min(min, nums[i]);
+      }
+
+      return (sum - max - min) / (nums.length - 2);
+
+
+    }
+
+
+    //
+
+
 
 
 
