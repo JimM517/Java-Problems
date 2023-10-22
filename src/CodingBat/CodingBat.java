e779bfd69abf99f3163dfc1bebddbbb2f2429f31
JimM517 <80083198+JimM517@users.noreply.github.com>
2023-10-22 18:33:55 -0400

    Completed modThree

--- src/CodingBat/CodingBat.java
+++ src/CodingBat/CodingBat.java
@@ -1024,6 +1024,32 @@ public class CodingBat {
     }
 
 
+    // modThree
+    public boolean modThree(int[] nums) {
+//
+//        int oddCount = 0;
+//        int evenCount = 0;
+
+
+        for (int i = 0; i < nums.length - 2; i++) {
+           if (nums[i] % 2 == 0) {
+               if (nums[i + 1] % 2 == 0 && nums[i + 2] % 2 == 0) {
+                   return true;
+               }
+           }
+            if (nums[i] % 2 == 1) {
+                if (nums[i + 1] % 2 == 1 && nums[i + 2] % 2 == 1) {
+                    return true;
+                }
+            }
+
+        }
+        return false;
+
+
+    }
+
+
 
 
 
