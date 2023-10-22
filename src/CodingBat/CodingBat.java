dc1f30527df4042f5b23c38637547a3ad756e95e
JimM517 <80083198+JimM517@users.noreply.github.com>
2023-10-22 16:44:35 -0400

    Completed more14

--- src/CodingBat/CodingBat.java
+++ src/CodingBat/CodingBat.java
@@ -845,6 +845,30 @@ public class CodingBat {
     }
 
 
+    // more14
+    public boolean more14(int[] nums) {
+
+        int oneCount = 0;
+        int fourCount = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+
+            if (nums[i] == 1) {
+                oneCount++;
+            }
+            if (nums[i] == 4) {
+                fourCount++;
+            }
+        }
+
+        if (oneCount > fourCount) {
+            return true;
+        }
+        return false;
+
+    }
+
+
 
 
 
