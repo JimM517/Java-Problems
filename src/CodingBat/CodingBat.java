768b0eb8a9a45d17cfc5b116ad39f41844c1c786
JimM517 <80083198+JimM517@users.noreply.github.com>
2023-10-22 17:18:03 -0400

    Completed isEverywhere

--- src/CodingBat/CodingBat.java
+++ src/CodingBat/CodingBat.java
@@ -928,6 +928,19 @@ public class CodingBat {
     }
 
 
+    // isEverywhere
+    public boolean isEverywhere(int[] nums, int val) {
+
+        for (int i = 0; i < nums.length - 1; i++) {
+            if (nums[i] != val && nums[i + 1] != val) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+
+
 
 
 
