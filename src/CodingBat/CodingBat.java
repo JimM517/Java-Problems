faac548032a314f1dc6cdc9b2dfb39d727e3dfdb
JimM517 <80083198+JimM517@users.noreply.github.com>
2023-10-22 17:06:34 -0400

    completed fizzArray2

--- src/CodingBat/CodingBat.java
+++ src/CodingBat/CodingBat.java
@@ -893,6 +893,24 @@ public class CodingBat {
     }
 
 
+    // fizzArray2
+    public String[] fizzArray2(int n) {
+
+        String[] result = new String[n];
+
+
+        for (int i = 0; i < n; i++) {
+            result[i] = String.valueOf(i);
+        }
+
+        return result;
+    }
+
+
+
+
+
+
 
 
 
