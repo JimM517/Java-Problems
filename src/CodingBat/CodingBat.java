4ea4ac364beaf5882db9b1028c63104351e18330
JimM517 <80083198+JimM517@users.noreply.github.com>
2023-10-23 20:42:40 -0400

    Completed triple up

--- src/CodingBat/CodingBat.java
+++ src/CodingBat/CodingBat.java
@@ -1114,6 +1114,21 @@ public class CodingBat {
 
     }
 
+    //tripleUp
+    public boolean tripleUp(int[] nums) {
+
+        for (int i = 0; i < nums.length - 2; i++) {
+           if (nums[i] + 1 == nums[i + 1] && nums[i + 1] + 1 == nums[i + 2]) {
+               return true;
+           }
+        }
+
+        return false;
+    }
+
+
+
+
 
 
 
