793b333a2ae52330a2daa7e462c1fbb4d27b5eee
JimM517 <80083198+JimM517@users.noreply.github.com>
2023-10-24 20:21:01 -0400

    Completed calculate average again

--- src/RandomPractice.java
+++ src/RandomPractice.java
@@ -631,6 +631,28 @@ public class RandomPractice {
     }
 
 
+    // calculateAverageTwo
+    public double calculateAverageTwo(int[] nums) {
+
+        int total = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            total += nums[i];
+        }
+        return (double) total / nums.length;
+
+    }
+
+
+
+
+
+
+
+
+
+
+
 
 
 
