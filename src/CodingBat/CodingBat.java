39969fd2b687536d0c9b1909822a504b1f9b215e
JimM517 <80083198+JimM517@users.noreply.github.com>
2023-10-27 20:56:57 -0400

    Completed zero front

--- src/CodingBat/CodingBat.java
+++ src/CodingBat/CodingBat.java
@@ -1275,6 +1275,33 @@ public class CodingBat {
 
 
 
+    // zeroFront
+    public int[] zeroFront(int[] nums) {
+
+        int[] result = new int[nums.length];
+
+        int zeroIndex = 0;
+
+        for (int num : nums) {
+            if (num == 0) {
+                result[zeroIndex] = 0;
+                zeroIndex++;
+            }
+        }
+
+
+
+        for (int num : nums) {
+            if (num != 0) {
+                result[zeroIndex] = num;
+                zeroIndex++;
+            }
+        }
+        return result;
+    }
+
+
+
 
 
 
