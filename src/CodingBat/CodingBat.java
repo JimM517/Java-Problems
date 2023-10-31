c3b9ea8c35fa5c3e918b7caf5724ab2154dc7403
JimM517 <80083198+JimM517@users.noreply.github.com>
2023-10-30 20:38:32 -0400

    Completed userCompare

--- src/CodingBat/CodingBat.java
+++ src/CodingBat/CodingBat.java
@@ -676,20 +676,18 @@ public class CodingBat {
 
     // TODO userCompare -> correct for more than half, still failing two tests
     public int userCompare(String aName, int aId, String bName, int bId) {
-         int nameCompare = aName.compareTo(bName);
 
-         if (nameCompare == 0) {
-             if (aId < bId) {
-                 return -1;
-             } else if (aId > bId) {
-                 return 1;
-             } else {
-                 return 0;
-             }
-         } else {
-             return nameCompare;
-         }
+        if (aName.compareTo(bName) < 0) {
+            return -1;
+        } else if (aName.compareTo(bName) > 0) {
+            return 1;
+        } else if (aId < bId) {
+            return -1;
+        } else if (aId > bId) {
+            return 1;
+        }
 
+        return 0;
     }
 
 
