27883def5e7a91bc44836c3819598b62d2546900
JimM517 <80083198+JimM517@users.noreply.github.com>
2023-10-31 20:44:00 -0400

    Completed countHi

--- src/CodingBat/CodingBat.java
+++ src/CodingBat/CodingBat.java
@@ -1513,6 +1513,33 @@ public class CodingBat {
 
 
 
+    // countHi
+
+    public int countHi(String str) {
+
+        int count = 0;
+
+
+        if (str.length() < 2) {
+            return 0;
+        }
+
+        for (int i = 0; i < str.length() - 1; i++) {
+
+            if (str.charAt(i) == 'h' && str.charAt(i + 1) == 'i') {
+                count++;
+            }
+
+
+        }
+
+
+        return count;
+    }
+
+
+
+
 
 
 
