c1d273277e16bd417afa4873d235543fcb237f40
JimM517 <80083198+JimM517@users.noreply.github.com>
2023-10-31 21:51:11 -0400

    Completed right2

--- src/CodingBat/CodingBat.java
+++ src/CodingBat/CodingBat.java
@@ -1608,6 +1608,17 @@ public class CodingBat {
     }
 
 
+    // right2
+    public String right2(String str) {
+
+        String lastTwo = str.substring(str.length() - 2);
+        String rest = str.substring(0, str.length() - 2);
+
+
+        return lastTwo + rest;
+    }
+
+
 
 
 
