f6a056eeb7f4066892e024cc126adfbe3e904643
JimM517 <80083198+JimM517@users.noreply.github.com>
2023-11-02 20:56:41 -0400

    Completed twoChar

--- src/CodingBat/CodingBat.java
+++ src/CodingBat/CodingBat.java
@@ -1673,7 +1673,17 @@ public class CodingBat {
     }
 
 
+    // twoChar
+    public String twoChar(String str, int index) {
 
+        if (index < 0 || index + 2 > str.length()) {
+            return str.substring(0, 2);
+        }
+
+        return str.substring(index, index + 2);
+
+
+    }
 
 
 
