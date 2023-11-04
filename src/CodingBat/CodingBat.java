59db856bf491369ae24d543ff3a53d3d6e9de4f7
JimM517 <80083198+JimM517@users.noreply.github.com>
2023-11-04 08:37:25 -0400

    Completed without2

--- src/CodingBat/CodingBat.java
+++ src/CodingBat/CodingBat.java
@@ -1817,6 +1817,17 @@ public class CodingBat {
         return firstTwo + firstTwo + firstTwo;
     }
 
+    // without2
+    public String without2(String str) {
+
+        if (str.length() >= 2 && str.substring(0, 2).equals(str.substring(str.length() - 2))) {
+            return str.substring(2);
+        } else {
+            return str;
+        }
+
+    }
+
 
 
 
