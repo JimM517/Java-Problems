436099955e8c52c96471963ad869f8b018962522
JimM517 <80083198+JimM517@users.noreply.github.com>
2023-11-03 21:24:00 -0400

    Completed conCat

--- src/CodingBat/CodingBat.java
+++ src/CodingBat/CodingBat.java
@@ -1728,7 +1728,24 @@ public class CodingBat {
 
     }
 
+    // conCat
+    public String conCat(String a, String b) {
 
+        if (a.length() == 0) {
+            return b;
+        }
+
+        if (b.length() == 0) {
+            return a;
+        }
+
+        if (a.substring(a.length() - 1).equals(b.substring(0, 1))) {
+            return a.substring(0, 2) + b;
+        }
+
+        return a + b;
+
+    }
 
 
 
