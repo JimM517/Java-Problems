1907bd2de82d2914c840c0505ebae4a5bf7fa458
JimM517 <80083198+JimM517@users.noreply.github.com>
2023-11-03 21:38:29 -0400

    Completed frontAgain

--- src/CodingBat/CodingBat.java
+++ src/CodingBat/CodingBat.java
@@ -1776,6 +1776,18 @@ public class CodingBat {
 
 
 
+    // frontAgain
+    public boolean frontAgain(String str) {
+
+        if (str.length() < 2) {
+            return false;
+        }
+
+        return str.substring(0, 2).equals(str.substring(str.length() - 2));
+
+    }
+
+
 
 
 
