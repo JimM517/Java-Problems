a9af941f6c519507a27be2343dc956cb17f79069
JimM517 <80083198+JimM517@users.noreply.github.com>
2023-11-04 09:18:10 -0400

    Completed range sum BST

--- src/Easy/EasyLeetQues.java
+++ src/Easy/EasyLeetQues.java
@@ -259,6 +259,27 @@ public class EasyLeetQues {
 
 
 
+    // 938 - Sum Range BST
+//    public int rangeSumBST(TreeNode root, int low, int high) {
+//
+//        if (root == null) {
+//            return 0;
+//        }
+//
+//        if (root.val < low) {
+//            return rangeSumBST(root.right, low, high);
+//        } else if (root.val > high) {
+//            return rangeSumBST(root.left, low high);
+//        } else {
+//            return root.val + rangeSumBST(root.left, low, high) + rangeSumBST(root.right, low, high);
+//        }
+//
+//
+//
+//    }
+
+
+
 
 
 
