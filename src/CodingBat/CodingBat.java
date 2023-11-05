8160e32b8519a9ba037af475f0d4082207de6b4d
JimM517 <80083198+JimM517@users.noreply.github.com>
2023-11-05 09:19:03 -0500

    Completed endOther

--- src/CodingBat/CodingBat.java
+++ src/CodingBat/CodingBat.java
@@ -1932,6 +1932,19 @@ public class CodingBat {
     }
 
 
+    // endOther
+    public boolean endOther(String a, String b) {
+
+        a = a.toLowerCase();
+        b = b.toLowerCase();
+
+
+        return a.endsWith(b) || b.endsWith(a);
+
+    }
+
+
+
 
 
 
