b908640a30c5e8884b22f1aa6472b1b34ed78d8b
JimM517 <80083198+JimM517@users.noreply.github.com>
2023-11-05 09:32:15 -0500

    Completed bobThere

--- src/CodingBat/CodingBat.java
+++ src/CodingBat/CodingBat.java
@@ -1962,6 +1962,18 @@ public class CodingBat {
 
 
 
+    // bobThere
+    public boolean bobThere(String str) {
+
+      for (int i = 0; i < str.length() - 2; i++) {
+          if (str.charAt(i) == 'b' && str.charAt(i + 2) == 'b') {
+              return true;
+          }
+      }
+
+        return false;
+    }
+
 
 
 
