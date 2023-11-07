ac3a6252ab024191508dc2e1718116468563eb9a
JimM517 <80083198+JimM517@users.noreply.github.com>
2023-11-06 20:56:25 -0500

    completed balance xy

--- src/CodingBat/CodingBat.java
+++ src/CodingBat/CodingBat.java
@@ -1976,6 +1976,30 @@ public class CodingBat {
 
 
 
+    // xyBalance
+    public boolean xyBalance(String str) {
+
+        boolean hasY = false;
+
+
+        for (int i = str.length() - 1; i >= 0; i--) {
+            if (str.charAt(i) == 'y') {
+                hasY = true;
+            }
+
+
+            if (str.charAt(i) == 'x' && !hasY) {
+                return false;
+            }
+        }
+      return true;
+    }
+
+
+
+
+
+
 
 
 
