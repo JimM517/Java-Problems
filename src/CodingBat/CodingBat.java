e7342ef58582422080bf2fe4838673d85e55b973
JimM517 <80083198+JimM517@users.noreply.github.com>
2023-11-06 21:05:28 -0500

    Completed repeatEnd

--- src/CodingBat/CodingBat.java
+++ src/CodingBat/CodingBat.java
@@ -2018,6 +2018,20 @@ public class CodingBat {
     }
 
 
+    // repeatEnd
+    public String repeatEnd(String str, int n) {
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < n; i++) {
+            sb.append(str.substring(str.length() - n));
+        }
+
+        return sb.toString();
+    }
+
+
+
 
 
 
