10d8db035e564de61a16cfbc5190f192197f3d1d
JimM517 <80083198+JimM517@users.noreply.github.com>
2023-11-08 19:30:19 -0500

    Completed sameStarChar

--- src/CodingBat/CodingBat.java
+++ src/CodingBat/CodingBat.java
@@ -2130,6 +2130,24 @@ public class CodingBat {
     }
 
 
+    // sameStarChar
+    public boolean sameStarChar(String str) {
+
+        for (int i = 1; i < str.length() - 1; i++) {
+            if (str.charAt(i) == '*') {
+                if (str.charAt(i - 1) != str.charAt(i + 1)) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+
+
+
+
+
 
 
 
