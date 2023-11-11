0e0a25a11f55e2f9d5280444c641eed78a68de38
JimM517 <80083198+JimM517@users.noreply.github.com>
2023-11-10 20:33:59 -0500

    bigdiff

--- src/RandomPractice.java
+++ src/RandomPractice.java
@@ -738,6 +738,20 @@ public class RandomPractice {
 
 
 
+    // bigDiff
+    public int bigDiff(int[] nums) {
+
+        int smallest = nums[0];
+        int largest = nums[0];
+
+        for (int i = 0; i < nums.length; i++) {
+            smallest = Math.min(smallest, nums[i]);
+            largest = Math.max(largest, nums[i]);
+        }
+        return largest - smallest;
+    }
+
+
 
 
 
