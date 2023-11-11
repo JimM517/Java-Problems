1b9ba1f37954fc2901fb46edcf8d337f8e8ee0a3
JimM517 <80083198+JimM517@users.noreply.github.com>
2023-11-11 09:59:45 -0500

    Completed fizzArray two

--- src/RandomPractice.java
+++ src/RandomPractice.java
@@ -920,6 +920,19 @@ public class RandomPractice {
     }
 
 
+    // fizzArray2
+    public String[] fizzArray2(int n) {
+
+        String[] result = new String[n];
+
+        for (int i = 0; i < n; i++) {
+            result[i] = String.valueOf(i);
+        }
+
+        return result;
+    }
+
+
 
 
 
