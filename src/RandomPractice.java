795b726bad8fd9e2c76569e4fe45ed5a711e96f4
JimM517 <80083198+JimM517@users.noreply.github.com>
2023-11-12 09:39:33 -0500

    Completed is everywhere

--- src/RandomPractice.java
+++ src/RandomPractice.java
@@ -954,6 +954,24 @@ public class RandomPractice {
 
 
 
+    // isEverywhere
+    public boolean isEverywhere(int[] nums, int val) {
+
+        for (int i = 0; i < nums.length - 1; i++) {
+            if (nums[i] != val && nums[i + 1] != val) {
+                return false;
+            }
+        }
+
+        return true;
+
+    }
+
+
+
+
+
+
 
 
 
