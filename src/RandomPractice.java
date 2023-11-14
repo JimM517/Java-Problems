e2526bbb1174cce69ba417a527f90a564ed349ca
JimM517 <80083198+JimM517@users.noreply.github.com>
2023-11-13 21:05:39 -0500

    scores increasing

--- src/RandomPractice.java
+++ src/RandomPractice.java
@@ -1119,6 +1119,21 @@ public class RandomPractice {
     }
 
 
+    // scoresIncreasing
+    public boolean scoresIncreasing(int[] scores) {
+
+        for (int i = 0; i < scores.length - 1; i++) {
+            if (scores[i] > scores[i + 1]) {
+                return false;
+            }
+        }
+
+
+        return true;
+
+    }
+
+
 
 
 
