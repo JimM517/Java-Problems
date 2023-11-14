247e492010e0cad78dbe78c4632f3cd6d82cb554
JimM517 <80083198+JimM517@users.noreply.github.com>
2023-11-13 21:16:21 -0500

    spaces

--- src/RandomPractice.java
+++ src/RandomPractice.java
@@ -1134,7 +1134,18 @@ public class RandomPractice {
     }
 
 
+    // scores100
+    public boolean scores100(int[] scores) {
 
+        for (int i = 0; i < scores.length - 1; i++) {
+            if (scores[i] == 100 && scores[i + 1] == 100) {
+                return true;
+            }
+        }
+
+        return false;
+
+    }
 
 
 
