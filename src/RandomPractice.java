d5a13a7dc8649c096ba1f3938bdf096938042ec0
JimM517 <80083198+JimM517@users.noreply.github.com>
2023-11-13 20:54:41 -0500

    modThree

--- src/RandomPractice.java
+++ src/RandomPractice.java
@@ -1047,6 +1047,33 @@ public class RandomPractice {
 
 
 
+    // modThree
+    public boolean modThree(int[] nums) {
+
+
+        for (int i = 0; i < nums.length - 2; i++) {
+            if (nums[i] % 2 == 0) {
+                if (nums[i + 1] % 2 == 0 && nums[i + 2] % 2 == 0) {
+                    return true;
+                }
+            }
+            if (nums[i] % 2 == 1) {
+                if (nums[i + 1] % 2 == 1 && nums[i + 2] % 2 == 1) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+
+
+
+    }
+
+
+
+
+
 
 
 
