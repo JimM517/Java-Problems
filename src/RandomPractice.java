a288ef2f14e3f2df93f8e64efbf9820f3ac59aed
JimM517 <80083198+JimM517@users.noreply.github.com>
2023-11-13 20:58:54 -0500

    haveThree

--- src/RandomPractice.java
+++ src/RandomPractice.java
@@ -1071,7 +1071,28 @@ public class RandomPractice {
     }
 
 
+    // haveThree
+    public boolean haveThree(int[] nums) {
 
+        int threeCount = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+
+            if (nums[i] == 3) {
+                threeCount++;
+                if (i > 0 && nums[i - 1] == 3) {
+                    return false;
+                }
+            }
+
+
+
+        }
+
+        return threeCount == 3;
+
+
+    }
 
 
 
