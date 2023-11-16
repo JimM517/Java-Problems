af7a23bae5ab431b0fb9a149a88a5f684061bb0c
JimM517 <80083198+JimM517@users.noreply.github.com>
2023-11-15 20:42:29 -0500

    fibonacci recursion

--- src/RandomPractice.java
+++ src/RandomPractice.java
@@ -1252,6 +1252,22 @@ public class RandomPractice {
 
 
 
+    // fibonacci using recursion
+    public int fibonacci(int n) {
+
+            if (n == 0) {
+                return 0;
+            } else if (n == 1) {
+                return 1;
+            }
+
+
+        return  fibonacci(n - 1) + fibonacci(n - 2);
+
+    }
+
+
+
 
 
 
