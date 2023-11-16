db397dd74cad8d334ff629b9a3b261549fa3c942
JimM517 <80083198+JimM517@users.noreply.github.com>
2023-11-15 20:30:41 -0500

    two sum

--- src/RandomPractice.java
+++ src/RandomPractice.java
@@ -1170,6 +1170,44 @@ public class RandomPractice {
 
 
 
+    // tripleUp
+
+    public boolean tripleUp(int[] nums) {
+
+        for (int i = 0; i < nums.length - 2; i++) {
+
+            if (nums[i] + 1 == nums[i + 1] && nums[i + 1] + 1 == nums[i + 2]) {
+                return true;
+            }
+
+
+        }
+
+        return false;
+
+    }
+
+
+
+    // twoSum
+    public int[] twoSum(int[] nums, int target) {
+
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(target - nums[i])) {
+                return new int[]{map.get(target - nums[i]), i};
+            } else {
+                map.put(nums[i], i);
+            }
+        }
+
+
+        return new int[]{-1, -1};
+    }
+
+
+
 
 
 
