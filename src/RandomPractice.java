3d3547c7b9bcea40b07b39a05e95e923b7666d4a
JimM517 <80083198+JimM517@users.noreply.github.com>
2023-11-17 20:13:34 -0500

    countYZ

--- src/RandomPractice.java
+++ src/RandomPractice.java
@@ -1401,6 +1401,38 @@ public class RandomPractice {
     }
 
 
+    // countYZ
+    public int countYZ(String str) {
+//
+//        int count = 0;
+//        String[] splitWord = str.split("\\W ");
+//
+//        for (int i = 0; i < splitWord.length; i++) {
+//            String lowerCase = splitWord[i].toLowerCase();
+//         if (lowerCase.endsWith("y") || lowerCase.endsWith("z")) {
+//                count++;
+//            }
+//        }
+//
+//        return count;
+
+        int len = str.length();
+        int count = 0;
+        str = str.toLowerCase();
+
+        for (int i = 0; i < len; i++) {
+
+            if (str.charAt(i) == 'y' || str.charAt(i) == 'z') {
+                if (i < len-1 && !Character.isLetter(str.charAt(i+1)))
+                    count++;
+                else if (i == len-1)
+                    count++;
+            }
+        }
+        return count;
+    }
+
+
 
 
 
