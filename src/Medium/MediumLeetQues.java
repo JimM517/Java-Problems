86ea552c25880776f78b024312a5ca67b0154d74
JimM517 <80083198+JimM517@users.noreply.github.com>
2023-11-18 13:42:26 -0500

    find min

--- src/Medium/MediumLeetQues.java
+++ src/Medium/MediumLeetQues.java
@@ -230,6 +230,36 @@ public class MediumLeetQues {
 
 
 
+    // 153. Find Minimum in Rotated Sorted Array
+    public int findMinAgain(int[] nums) {
+
+        int left = 0;
+        int right = nums.length - 1;
+
+
+        while (left < right) {
+
+
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] > nums[right]) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+
+        }
+
+        return nums[left];
+
+
+    }
+
+
+
+
+
+
 
 
 
