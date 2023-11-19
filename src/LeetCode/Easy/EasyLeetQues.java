dc39abacff0f2bf5a6574ef68afbc66e3133a9f3
JimM517 <80083198+JimM517@users.noreply.github.com>
2023-11-19 16:16:50 -0500

    Two Sum

--- src/LeetCode/Easy/EasyLeetQues.java
+++ src/LeetCode/Easy/EasyLeetQues.java
@@ -387,6 +387,32 @@ public class EasyLeetQues {
     }
 
 
+    // 1. Two sum
+    public int[] twoSum(int[] nums, int target) {
+
+        Map<Integer, Integer> result = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            if (result.containsKey(target - nums[i])) {
+                return new int[]{result.get(target - nums[i]), i};
+            } else {
+                result.put(nums[i], i);
+            }
+        }
+
+
+        return new int[]{-1, -1};
+
+    }
+
+
+
+
+
+
+
+
+
 
 
 
