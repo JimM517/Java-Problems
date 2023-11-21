71b5bc65bca0884eb3b4d8c0667451bb910ca615
JimM517 <80083198+JimM517@users.noreply.github.com>
2023-11-20 20:39:01 -0500

    topping 1

--- src/RandomPractice.java
+++ src/RandomPractice.java
@@ -1531,6 +1531,21 @@ public class RandomPractice {
     }
 
 
+    // topping 1
+    public Map<String, String> topping1(Map<String, String> map) {
+
+        if (map.containsKey("ice cream")) {
+            map.put("ice cream", "cherry");
+        }
+
+        map.put("bread", "butter");
+
+        return map;
+
+    }
+
+
+
 
 
 
