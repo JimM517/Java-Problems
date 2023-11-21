028f42b574432873c118017379915d85329f3fd3
JimM517 <80083198+JimM517@users.noreply.github.com>
2023-11-20 21:04:46 -0500

    mapAB3

--- src/RandomPractice.java
+++ src/RandomPractice.java
@@ -1596,6 +1596,19 @@ public class RandomPractice {
     }
 
 
+    // mapAB3
+    public Map<String, String> mapAB3(Map<String, String> map) {
+
+        if (map.containsKey("a") && !map.containsKey("b")) {
+            map.put("b", map.get("a"));
+        }
+        if (map.containsKey("b") && !map.containsKey("a")) {
+            map.put("a", map.get("b"));
+        }
+        return map;
+    }
+
+
 
 
 
