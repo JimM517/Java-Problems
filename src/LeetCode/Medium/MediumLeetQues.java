1df623840fa8489ebb6d951aa15862c4b4493e5a
JimM517 <80083198+JimM517@users.noreply.github.com>
2023-11-24 09:59:22 -0500

    two sum two

--- src/LeetCode/Medium/MediumLeetQues.java
+++ src/LeetCode/Medium/MediumLeetQues.java
@@ -346,7 +346,33 @@ public class MediumLeetQues {
 
 
 
+    // 167. Two Sum II - sorted input array
+    public int[] twoSum(int[] numbers, int target) {
 
+
+        int start = 0;
+        int end = numbers.length - 1;
+
+
+        while (start < end) {
+
+            int sum = numbers[start] + numbers[end];
+
+            if (sum > target) {
+                end--;
+            } else if (sum < target) {
+                start++;
+            } else {
+                return new int[]{start + 1, end + 1};
+            }
+
+
+        }
+
+
+        return null;
+
+    }
 
 
 
