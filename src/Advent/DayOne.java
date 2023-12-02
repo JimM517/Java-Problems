9464169870db8f8b9f2dbcb5f151a1096d6d7af0
JimM517 <80083198+JimM517@users.noreply.github.com>
2023-12-02 11:01:21 -0500

    day one completed

--- src/Advent/DayOne.java
+++ src/Advent/DayOne.java
@@ -14,18 +14,24 @@ public class DayOne {
 
         File file = new File(fPath + "AdventInput.txt");
 
-
+        int total = 0;
 
         try {
             Scanner sc = new Scanner(file);
 
             while (sc.hasNext()) {
-
+                // get each line
                 String line = sc.nextLine();
+                // get the number from each line
+                String numStr = parseString(line);
+                // convert to int
+                int num = Integer.parseInt(numStr);
+                // add the total
+                total += num;
 
-                parseString(line);
 
             }
+            System.out.println("The total is " + total);
 
         } catch (FileNotFoundException e) {
             throw new RuntimeException(e);
@@ -39,13 +45,38 @@ public class DayOne {
 
 
 
-    public String parseString(String line) {
+    public static String parseString(String line) {
+
+
+        char num1 = ' ';
+        char num2 = ' ';
+
+        for (int i = 0; i < line.length(); i++) {
+
+            char numberOne = line.charAt(i);
+
+            if (Character.isDigit(numberOne)) {
+                num1 = numberOne;
+                break;
+            }
+
+        }
+
+        for (int i = line.length() - 1; i >= 0; i--) {
+
+            char numberTwo = line.charAt(i);
+
+            if (Character.isDigit(numberTwo)) {
+                num2 = numberTwo;
+                break;
+            }
+
+
+        }
 
 
 
-
-
-
+        return "" + num1 + num2;
 
     }
 
