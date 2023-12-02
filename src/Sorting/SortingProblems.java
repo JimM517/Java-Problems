ee546cbac201b10649abdf9a24dbe9d21b24e045
JimM517 <80083198+JimM517@users.noreply.github.com>
2023-12-01 20:58:48 -0500

    bubble sort

--- src/Sorting/SortingProblems.java
+++ src/Sorting/SortingProblems.java
@@ -58,6 +58,33 @@ public class SortingProblems {
 
 
 
+        // bubble sort
+    public void bubbleSort(int[] nums) {
+
+        int n = nums.length;
+        int temp = 0;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 1; j < (n - 1); j++) {
+
+               if (nums[j - 1] > nums[j]) {
+
+                   // swap elements
+
+                   temp = nums[j - 1];
+                   nums[j - 1] = nums[j];
+                   nums[j] = temp;
+               }
+
+            }
+        }
+
+
+
+
+
+    }
+
 
 
 
