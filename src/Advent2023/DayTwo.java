d243eee6e99b856a334a878f54891c2cd9ab62f6
JimM517 <80083198+JimM517@users.noreply.github.com>
2023-12-04 19:14:02 -0500

    working day two

--- src/Advent2023/DayTwo.java
+++ src/Advent2023/DayTwo.java
@@ -1,4 +1,47 @@
 package Advent2023;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class DayTwo {
+
+
+    public static void main(String[] args) {
+
+        String fPath = "src/InputFiles/";
+
+        File file = new File(fPath + "CubeInput.txt");
+
+        try {
+
+
+            Scanner sc = new Scanner(file);
+
+            while (sc.hasNext()) {
+
+
+
+
+
+
+
+
+            }
+
+
+
+
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+
+
+
+
+
+    }
+
+
+
 }
