e33cf29d1062e7a55d86ae70a8d0140efc09acf2
JimM517 <80083198+JimM517@users.noreply.github.com>
2023-12-04 19:53:53 -0500

    Day two completed

--- src/Advent2023/DayTwo.java
+++ src/Advent2023/DayTwo.java
@@ -50,14 +50,82 @@ public class DayTwo {
         // get game id
         int gameId = Integer.parseInt(line.substring(5, colon));
 
-
+        // get just the games
         String stripGame = line.substring(colon + 2);
 
+        // split into array for each game as ; indicates end of one game
+        String[] subset = stripGame.split("; ");
 
-        System.out.println(stripGame);
+        boolean isValidGame = true;
 
 
-        return 1;
+        // loop over array of games
+        for (String group : subset) {
+
+            // split each line by , as this separates different colors
+            String[] colors = group.split(", ");
+
+
+            for (String color : colors) {
+
+                // get index of spaces, need to isolate number
+                int indexSpace = color.indexOf(" ");
+
+                // this is the number
+                String numStr = color.substring(0, indexSpace);
+
+                // this is the color that corresponds to numStr
+                String colorName = color.substring(indexSpace + 1);
+
+
+
+                int num = Integer.parseInt(numStr);
+
+                // check if each num/colorName is valid per parameters
+                boolean isValid = isValidCount(num, colorName);
+
+                if (!isValid) {
+                    isValidGame = false;
+                }
+
+            }
+
+        }
+
+        if (isValidGame) {
+            return gameId;
+        }
+
+        return 0;
+    }
+
+
+
+
+
+    public static boolean isValidCount(int num, String colorName) {
+
+        switch(colorName) {
+
+            case "red":
+                if (num <= 12) {
+                    return true;
+                }
+                return false;
+            case "green":
+                if (num <= 13) {
+                    return true;
+                }
+                return false;
+            case "blue":
+                if (num <= 14) {
+                    return true;
+                }
+                return false;
+        }
+
+
+        return false;
     }
 
 
