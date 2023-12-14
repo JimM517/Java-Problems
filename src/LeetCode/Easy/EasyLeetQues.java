6b3098768c9552a24ef63176d87717230b717f31
JimM517 <80083198+JimM517@users.noreply.github.com>
2023-12-13 21:06:38 -0500

    ransom note

--- src/LeetCode/Easy/EasyLeetQues.java
+++ src/LeetCode/Easy/EasyLeetQues.java
@@ -653,7 +653,30 @@ public class EasyLeetQues {
     }
 
 
+    // 383. Ransom Note
 
+    public boolean canConstruct(String ransomNote, String magazine) {
+
+       int[] charCount = new int[26];
+
+       for (char c : magazine.toCharArray()) {
+           charCount[c - 'a']++;
+
+       }
+
+       for (char c : ransomNote.toCharArray()) {
+
+           if (--charCount[c - 'a'] < 0) {
+               return false;
+           }
+
+       }
+
+       return true;
+
+
+
+    }
 
 
 
