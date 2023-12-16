1d1eccca483fab9f8ef4ff46d753ca637f3bb105
JimM517 <80083198+JimM517@users.noreply.github.com>
2023-12-16 14:20:31 -0500

    word pattern

--- src/LeetCode/Easy/EasyLeetQues.java
+++ src/LeetCode/Easy/EasyLeetQues.java
@@ -811,6 +811,37 @@ public class EasyLeetQues {
     }
 
 
+    // 209. Word Pattern
+    public boolean wordPattern(String pattern, String s) {
+
+        String[] arr = s.split(" ");
+
+        if(pattern.length()!=arr.length)
+            return false;
+
+        HashMap<Character, String> map = new HashMap<Character, String>();
+
+        for(int i=0; i<arr.length; i++)
+        {
+            char c = pattern.charAt(i);
+            String str = arr[i];
+            if(map.containsKey(c))
+            {
+                if(!map.get(c).equals(str))
+                    return false;
+            }
+            else
+            {
+                if(map.containsValue(str))
+                    return false;
+                map.put(c, str);
+            }
+        }
+        return true;
+
+
+
+        }
 
 
 
@@ -819,4 +850,19 @@ public class EasyLeetQues {
 
 
 
-}
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
