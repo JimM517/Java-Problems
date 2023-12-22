8808ad8bce6fda7af9f22651ce5d32d8248fb5e7
JimM517 <80083198+JimM517@users.noreply.github.com>
2023-12-21 20:59:32 -0500

    binary search in pivot indexed array

--- src/LeetCode/Medium/MediumLeetQues.java
+++ src/LeetCode/Medium/MediumLeetQues.java
@@ -803,7 +803,42 @@ public class MediumLeetQues {
 
 
 
+    // 33. Search in Rotated Sorted Array
+    public int search(int[] nums, int target) {
 
+        // binary search
+
+
+        int start = 0;
+        int end = nums.length - 1;
+
+
+        while (start <= end) {
+
+            int mid = (start + end) / 2;
+
+            if (nums[mid] == target) {
+                return mid;
+            }
+            // left portion
+            else if (nums[start] <= nums[mid]) {
+                    if (target > nums[mid] || target < nums[start]) {
+                        start = mid + 1;
+                    } else {
+                        end = mid - 1;
+                    }
+             // right portion
+            } else {
+                if (target < nums[mid] || target > nums[end]) {
+                    end = mid - 1;
+                } else {
+                    start = mid + 1;
+                }
+            }
+        }
+
+        return -1;
+    }
 
 
 
