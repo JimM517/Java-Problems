c2d84d0729a6eba3c9f6e798b8caad652e73cf4f
JimM517 <80083198+JimM517@users.noreply.github.com>
2023-12-22 21:25:43 -0500

    two sum

--- src/Leet75/Leet75.java
+++ src/Leet75/Leet75.java
@@ -1,4 +1,39 @@
 package Leet75;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Leet75 {
+
+
+    // two sum
+    public int[] twoSum(int[] nums, int target) {
+
+        Map<Integer, Integer> result = new HashMap<>();
+
+
+        for (int i = 0; i < nums.length; i++) {
+
+            if (result.containsKey(target - nums[i])) {
+                return new int[]{result.get(target - nums[i]), i};
+            } else {
+                result.put(nums[i], i);
+            }
+
+
+
+        }
+        return new int[]{-1, -1};
+    }
+
+
+
+
+
+
+
+
+
+
+
 }
