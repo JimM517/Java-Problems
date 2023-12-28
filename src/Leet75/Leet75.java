a0b62eed799cfdbf9858da144c2b5196fcbe33e6
JimM517 <80083198+JimM517@users.noreply.github.com>
2023-12-28 10:14:33 -0500

    missingNumber

--- src/Leet75/Leet75.java
+++ src/Leet75/Leet75.java
@@ -394,6 +394,22 @@ public class Leet75 {
     }
 
 
+    // 268. Missing Number
+    public int missingNumber(int[] nums) {
+
+        int result = nums.length;
+
+
+        for (int i = 0; i < nums.length; i++) {
+
+            result += i - nums[i];
+
+        }
+        return result;
+
+    }
+
+
 
 
 
