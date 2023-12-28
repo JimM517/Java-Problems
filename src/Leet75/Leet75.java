c9e480f96292a9b9abb95babd21fc472d6b4a32a
JimM517 <80083198+JimM517@users.noreply.github.com>
2023-12-28 16:15:09 -0500

    coin change

--- src/Leet75/Leet75.java
+++ src/Leet75/Leet75.java
@@ -430,7 +430,29 @@ public class Leet75 {
 
 
 
+    // 322. Coin Change
+    public int coinChange(int[] coins, int amount) {
 
+        // dynamic programming approach -> bottom up
+
+        int[] dp = new int[(amount + 1)];
+
+        for (int i = 1; i <= amount; i++) {
+
+            int min = Integer.MAX_VALUE;
+
+            for (int j = 0; j < coins.length; j++) {
+
+                if (i >= coins[j] && dp[i - coins[j]] != -1) {
+                    min = Math.min(min, dp[i - coins[j]]);
+                }
+
+            }
+
+            dp[i] = (min == Integer.MAX_VALUE) ? -1 : min + 1;
+        }
+        return dp[amount];
+    }
 
 
 
