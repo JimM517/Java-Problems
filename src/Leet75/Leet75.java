77155fbdc90ac92f45e0395451f729d243238d2f
JimM517 <80083198+JimM517@users.noreply.github.com>
2024-01-01 09:40:41 -0500

    unique paths

--- src/Leet75/Leet75.java
+++ src/Leet75/Leet75.java
@@ -713,6 +713,36 @@ public class Leet75 {
 
 
 
+    // 62. Unique Paths
+    public int uniquePaths(int m, int n) {
+
+        int[][] gridDp = new int[m][n];
+
+        gridDp[m - 1][n - 1] = 1;
+
+        for (int i = m - 1; i >= 0; i--) {
+
+            for (int j = n - 1; j >= 0; j--) {
+
+                if (i != m - 1) {
+                    gridDp[i][j] = gridDp[i + 1][j];
+                }
+
+                if (j != n - 1) {
+                    gridDp[i][j] += gridDp[i][j + 1];
+                }
+
+            }
+
+
+        }
+
+        return gridDp[0][0];
+
+
+    }
+
+
 
 
 
