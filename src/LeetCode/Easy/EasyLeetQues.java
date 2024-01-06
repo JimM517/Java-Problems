7d03c6361d7aa2a2e643346f6ad5efbc3105847d
JimM517 <80083198+JimM517@users.noreply.github.com>
2024-01-05 21:19:02 -0500

    can place flowers

--- src/LeetCode/Easy/EasyLeetQues.java
+++ src/LeetCode/Easy/EasyLeetQues.java
@@ -953,7 +953,25 @@ public class EasyLeetQues {
 
 
 
+    // 605. Can Place Flowers
+    public boolean canPlaceFlowers(int[] flowerbed, int n) {
 
+       int size = flowerbed.length;
+
+       if (n == 0) return true;
+
+       for (int i = 0; i < size; i++) {
+
+           if (flowerbed[i] == 0 && (i == 0 || flowerbed[i - 1] == 0) && (i == size - 1 || flowerbed[i + 1] == 0)) {
+               n--;
+               if (n == 0) return true;
+               flowerbed[i] = 1;
+           }
+
+
+       }
+        return false;
+    }
 
 
 
