796cfafa65bf0bd64692a3cf2139f216bb6a7cc0
JimM517 <80083198+JimM517@users.noreply.github.com>
2024-01-07 18:44:59 -0500

    added hard leet 1639

--- src/LeetCode/Hard/HardLeetQues.java
+++ src/LeetCode/Hard/HardLeetQues.java
@@ -3,6 +3,37 @@ package LeetCode.Hard;
 public class HardLeetQues {
 
 
+    // 1639. Number of ways to form a target string given a dictionary
+    /** Had this in an interview! **/
+    public int numWays(String[] words, String target) {
+
+        // brute force by backtracking or recursion?
+
+        // O(w * k + t * k)
+
+        int n = words[0].length();
+        int m = target.length();
+        int mod = 1000000007;
+        int[] dp = new int[m+1];
+        dp[0] = 1;
+
+        int[][] count = new int[n][26];
+        for (String word : words) {
+            for (int i = 0; i < n; i++) {
+                count[i][word.charAt(i) - 'a']++;
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = m-1; j >= 0; j--) {
+                dp[j+1] += (int)((long)dp[j] * count[i][target.charAt(j) - 'a'] % mod);
+                dp[j+1] %= mod;
+            }
+        }
+
+        return dp[m];
+
+    }
 
 
 
