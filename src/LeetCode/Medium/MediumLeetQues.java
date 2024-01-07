23759400309e3c61c9bae7705ce0202b6a0d07a6
JimM517 <80083198+JimM517@users.noreply.github.com>
2024-01-07 18:57:34 -0500

    dna sequences

--- src/LeetCode/Medium/MediumLeetQues.java
+++ src/LeetCode/Medium/MediumLeetQues.java
@@ -1007,6 +1007,32 @@ public class MediumLeetQues {
 
 
 
+    // 187. Repeated DNA Sequences
+    public List<String> findRepeatedDnaSequences(String s) {
+
+        Set<String> seen = new HashSet<>();
+
+        Set<String> repeated = new HashSet<>();
+
+        for (int i = 0; i + 9 < s.length(); i++) {
+
+            String ten = s.substring(i, i + 10);
+
+            if (!seen.add(ten)) {
+                repeated.add(ten);
+            }
+
+        }
+
+
+        return new ArrayList<>(repeated);
+
+
+
+    }
+
+
+
 
 
 
