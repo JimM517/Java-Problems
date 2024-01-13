7c91c80e5468534d112e9c104160382afb8f030c
JimM517 <80083198+JimM517@users.noreply.github.com>
2024-01-12 20:43:49 -0500

    Ugly number

--- src/LeetCode/Easy/EasyLeetQues.java
+++ src/LeetCode/Easy/EasyLeetQues.java
@@ -1091,6 +1091,17 @@ public class EasyLeetQues {
 
 
 
+    // 263. Ugly Number
+    public boolean isUgly(int n) {
+
+        while (n >= 5 && n % 5 == 0) n /= 5;
+        while (n >= 3 && n % 3 == 0) n /= 3;
+        while (n >= 2 && n % 2 == 0) n /= 2;
+
+        return n == 1;
+
+    }
+
 
 
 
