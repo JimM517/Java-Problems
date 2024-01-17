6c65e693c2121d84fd8c277f55398a7c3752a677
JimM517 <80083198+JimM517@users.noreply.github.com>
2024-01-17 18:40:20 -0500

    best time to buy and sell stock...again

--- src/InterviewQuestions/InterviewPrep.java
+++ src/InterviewQuestions/InterviewPrep.java
@@ -231,6 +231,42 @@ public class InterviewPrep {
 
 
 
+    // 121. Best time to buy and sell stock
+    public int maxProfitRevisited(int[] prices) {
+
+        if (prices.length == 0) {
+            return 0;
+        }
+
+
+        int start = prices[0];
+        int maxProfit = 0;
+
+        for (int i = 0; i < prices.length; i++) {
+
+
+            if (prices[i] < start) {
+                start = prices[i];
+            } else {
+
+                int currentProfit = prices[i] - start;
+
+                if (currentProfit > maxProfit) {
+                    maxProfit = currentProfit;
+                }
+
+
+
+            }
+
+        }
+
+
+        return maxProfit;
+
+
+
+    }
 
 
 
