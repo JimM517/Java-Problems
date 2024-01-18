0f3a435e595018a7b2dd7e069d2bd735a3d3cd8c
JimM517 <80083198+JimM517@users.noreply.github.com>
2024-01-17 20:24:36 -0500

    first unique char

--- src/InterviewQuestions/InterviewPrep.java
+++ src/InterviewQuestions/InterviewPrep.java
@@ -415,6 +415,26 @@ public class InterviewPrep {
 
 
 
+    // 387. First Unique Character String
+    public int firstUniqChar(String s) {
+
+        Map<Character, Integer> count = new HashMap<>();
+
+        for (char c : s.toCharArray()) {
+
+            count.put(c, count.getOrDefault(c, 0) + 1);
+        }
+
+
+        for (int i = 0; i < s.length(); i++) {
+
+           if (count.get(s.charAt(i)) == 1) return i;
+
+        }
+        return -1;
+
+    }
+
 
 
 
