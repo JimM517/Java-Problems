49160c9697f69785afae0f8588b0f39ca4f3908a
JimM517 <80083198+JimM517@users.noreply.github.com>
2024-01-17 20:07:22 -0500

    reverse int

--- src/InterviewQuestions/InterviewPrep.java
+++ src/InterviewQuestions/InterviewPrep.java
@@ -316,6 +316,36 @@ public class InterviewPrep {
 
 
 
+    // 7. Reverse Integer
+    public int reverse(int x) {
+
+
+        boolean isNegative = x < 0;
+
+        long num = Math.abs((long) x);
+
+        long reversed = 0;
+
+        while (num > 0) {
+            reversed = reversed * 10 + num % 10;
+            num /= 10;
+        }
+
+
+        if (reversed > Integer.MAX_VALUE || reversed < Integer.MIN_VALUE) {
+            return 0;
+        }
+
+
+        if (isNegative) {
+            reversed = -reversed;
+        }
+
+
+        return (int) reversed;
+
+
+    }
 
 
 
