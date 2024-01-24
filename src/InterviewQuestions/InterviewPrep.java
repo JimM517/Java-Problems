ba1a855c11845e0d1fb1b1189d353236a1b0c644
JimM517 <80083198+JimM517@users.noreply.github.com>
2024-01-23 20:47:43 -0500

    median or two sorted arrays

--- src/InterviewQuestions/InterviewPrep.java
+++ src/InterviewQuestions/InterviewPrep.java
@@ -764,4 +764,61 @@ public class InterviewPrep {
 
 
 
+
+    /**** HARD ****/
+
+    // 4. Median of Two Sorted Arrays
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+
+        int n = nums1.length;
+        int m = nums2.length;
+
+        int[] arr = new int[m + n];
+
+        int i = 0;
+        int j = 0;
+        int k = 0;
+
+        while (i < n & j < m) {
+            if (nums1[i] < nums2[j]) {
+                arr[k] = nums1[i];
+                i++;
+                k++;
+            } else {
+                arr[k] = nums2[j];
+                j++;
+                k++;
+            }
+        }
+
+        while (i < n) {
+            arr[k] = nums1[i];
+            i++;
+            k++;
+        }
+
+        while (j < m) {
+            arr[k] = nums2[j];
+            j++;
+            k++;
+        }
+
+
+        int length = arr.length;
+
+        double median = 0;
+
+        int ans = length / 2;
+
+        if (length % 2 == 0) {
+            median = arr[ans - 1] + arr[ans] / 2.0;
+        } else {
+            median = arr[ans];
+        }
+        return median;
+    }
+
+
+
+
 }
