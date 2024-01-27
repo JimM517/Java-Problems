92c426884edc677a74601d13a86d25384ae11bd5
JimM517 <80083198+JimM517@users.noreply.github.com>
2024-01-27 15:01:07 -0500

    min abs diff

--- src/LeetCode/Easy/EasyLeetQues.java
+++ src/LeetCode/Easy/EasyLeetQues.java
@@ -1155,6 +1155,37 @@ public class EasyLeetQues {
 
 
 
+    // 1200. Minimum absolute difference
+    public List<List<Integer>> minimumAbDiff(int[] arr) {
+
+        List<List<Integer>> result = new ArrayList<>();
+
+        Arrays.sort(arr);
+
+        int min = Integer.MAX_VALUE;
+
+        for (int i = 0; i < arr.length - 1; i++) {
+
+            int diff = arr[i + 1] - arr[i];
+
+            if (diff < min) {
+                min = diff;
+
+                result.clear();
+
+                result.add(Arrays.asList(arr[i], arr[i + 1]));
+            } else if (diff == min) {
+                result.add(Arrays.asList(arr[i], arr[i + 1]));
+            }
+        }
+
+        return result;
+
+    }
+
+
+
+
 
     }
 
