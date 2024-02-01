603c850756ba6d40107f4f7e529df22c69135b2a
JimM517 <80083198+JimM517@users.noreply.github.com>
2024-01-31 20:00:18 -0500

    combination sum II

--- src/LeetCode/Medium/MediumLeetQues.java
+++ src/LeetCode/Medium/MediumLeetQues.java
@@ -1442,4 +1442,59 @@ public class MediumLeetQues {
 
 
 
+    // combination sum II
+    public List<List<Integer>> combinationSumII(int[] nums, int target) {
+
+        List<List<Integer>> list = new ArrayList<>();
+
+        Arrays.sort(nums);
+
+        comboBack(list, new ArrayList<>(), nums, target, 0);
+
+        return list;
+
+
+    }
+
+
+
+    public void comboBack(List<List<Integer>> list, List<Integer> temp, int[] nums, int remain, int start) {
+
+        if (remain < 0) {
+            return;
+        } else if (remain == 0) {
+            list.add(new ArrayList<>(temp));
+        } else {
+            for (int i = start; i < nums.length; i++) {
+                if (i > start && nums[i] == nums[i - 1]) {
+                    continue;
+                }
+                temp.add(nums[i]);
+                comboBack(list, temp, nums, remain - nums[i], i + 1);
+                temp.remove(temp.size() - 1);
+            }
+        }
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
