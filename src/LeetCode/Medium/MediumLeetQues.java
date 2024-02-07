7ee0a1af46bc038d71551c2c6e9bf887039125a1
JimM517 <80083198+JimM517@users.noreply.github.com>
2024-02-06 20:58:24 -0500

    num of k pair sums

--- src/LeetCode/Medium/MediumLeetQues.java
+++ src/LeetCode/Medium/MediumLeetQues.java
@@ -1550,6 +1550,31 @@ public class MediumLeetQues {
 
 
 
+    // 1679. Max Number of k-sum pairs
+    public int maxOperations(int[] nums, int k) {
+
+        int count = 0;
+
+        Arrays.sort(nums);
+
+        int i = 0;
+        int j = nums.length - 1;
+
+
+        while (i < j) {
+            if ((nums[i] + nums[j]) == k) {
+                count++;
+                i++;
+                j--;
+            } else if ((nums[i] + nums[j]) > k) {
+                j--;
+            } else {
+                i++;
+            }
+        }
+
+        return count;
+    }
 
 
 
