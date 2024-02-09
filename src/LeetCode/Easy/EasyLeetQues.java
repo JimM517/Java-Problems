818190a0998146e36a3a618da607780bea18f9da
JimM517 <80083198+JimM517@users.noreply.github.com>
2024-02-08 21:26:17 -0500

    sorted squares

--- src/LeetCode/Easy/EasyLeetQues.java
+++ src/LeetCode/Easy/EasyLeetQues.java
@@ -1242,7 +1242,35 @@ public class EasyLeetQues {
 
 
 
+    // 977. Sqaures of a Sorted Array
+    public int[] sortedSquares(int[] nums) {
 
+        int[] result = new int[nums.length];
+
+        int left = 0;
+        int right = nums.length - 1;
+
+        int index = nums.length - 1;
+
+        while (left <= right) {
+
+
+            if (Math.abs(nums[left]) > Math.abs(nums[right])) {
+                result[index] = nums[left] * nums[left];
+                left++;
+            }
+            else {
+                result[index] = nums[right] * nums[right];
+                right--;
+            }
+
+
+            index--;
+        }
+
+        return result;
+
+    }
 
 
 
