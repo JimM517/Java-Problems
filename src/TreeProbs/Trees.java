d9c9a961efa3e7b21ee1cfa5fed6e2cd0203eb95
JimM517 <80083198+JimM517@users.noreply.github.com>
2024-02-10 09:03:16 -0500

    invert binary tree

---
+++ src/TreeProbs/Trees.java
@@ -0,0 +1,44 @@
+package TreeProbs;
+
+public class Trees {
+
+
+    // 226 Invert Binary Tree
+//    public TreeNode invertTree(TreeNode root) {
+//
+//        if (root == null) {
+//            return null;
+//        }
+//
+//        Queue<TreeNode> q = new LinkedList<>();
+//        q.add(root);
+//        while (q.size() > 0) {
+//            int size = q.size();
+//            for (int i = 0; i < size; i++) {
+//                TreeNode temp = q.remove();
+//                invert(temp);
+//                if (temp.left != null) q.add(temp.left);
+//                if (temp.right != null) q.add(temp.right);
+//            }
+//        }
+//
+//        return root;
+//
+//    }
+//
+//
+//
+//    private static void invert(TreeNode root) {
+//        if (root == null) {
+//            return;
+//        }
+//
+//        TreeNode temp = root.right;
+//        root.right = root.left;
+//        root.left = temp;
+//    }
+
+
+
+
+}
