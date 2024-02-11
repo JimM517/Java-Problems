8048b92b4b34d5ce6714f6bdaac61a520ffaf03f
JimM517 <80083198+JimM517@users.noreply.github.com>
2024-02-11 10:21:21 -0500

    next greatest

--- src/LeetCode/Easy/EasyLeetQues.java
+++ src/LeetCode/Easy/EasyLeetQues.java
@@ -1373,6 +1373,34 @@ public class EasyLeetQues {
 
 
 
+    // 496. Next Greater Element
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+
+        Map<Integer, Integer> result = new HashMap<>();
+
+        Stack<Integer> st = new Stack<>();
+
+        for (int num : nums2) {
+
+            while (!st.isEmpty() && st.peek() < num) {
+                result.put(st.pop(), num);
+            }
+            st.push(num);
+
+        }
+
+
+        for (int i = 0; i < nums1.length; i++) {
+            nums1[i] = result.getOrDefault(nums1[i], -1);
+        }
+
+
+
+        return nums1;
+
+
+
+    }
 
 
 
