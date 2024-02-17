5e34e25cfbb96fa85eee5a3756a6be1856e3eae4
JimM517 <80083198+JimM517@users.noreply.github.com>
2024-02-17 15:58:31 -0500

    remove element

--- src/ReviewProblems/LeetCode150.java
+++ src/ReviewProblems/LeetCode150.java
@@ -38,6 +38,21 @@ public class LeetCode150 {
 
 
 
+        // 27. Remove Element
+        public int removeElement(int[] nums, int val) {
+
+            int k = 0;
+
+            for (int i = 0; i < nums.length; i++) {
+
+                if (nums[i] != val) {
+                    nums[k] = nums[i];
+                    k++;
+                }
+            }
+            return k;
+
+    }
 
 
 
