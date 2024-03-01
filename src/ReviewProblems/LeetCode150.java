26c6c9da1d511d3f2e61bdb29bbf3fd4eb807ca4
JimM517 <80083198+JimM517@users.noreply.github.com>
2024-02-29 20:31:56 -0500

    three sum

--- src/ReviewProblems/LeetCode150.java
+++ src/ReviewProblems/LeetCode150.java
@@ -716,7 +716,7 @@ public class LeetCode150 {
 
 
 
-    //. Container with most water
+    // 11. Container with most water
     public int maxArea(int[] height) {
 
         int left = 0;
@@ -749,6 +749,61 @@ public class LeetCode150 {
 
 
 
+
+    // 15. 3Sum
+    public List<List<Integer>> threeSum(int[] nums) {
+
+        List<List<Integer>> results = new ArrayList<>();
+
+        Arrays.sort(nums);
+
+
+        for (int i = 0; i < nums.length - 2; i++) {
+
+
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+
+
+            int j = i + 1;
+            int k = nums.length - 1;
+
+            // basically two sum two
+
+            while (j < k) {
+
+
+                int sum = nums[i] + nums[j] + nums[k];
+
+
+                if (sum == 0) {
+                    results.add(Arrays.asList(nums[i], nums[j], nums[k]));
+
+                    while (j < k && nums[j] == nums[j + 1]) {
+                        j++;
+                    }
+
+
+                    while (j < k && nums[k] == nums[k - 1]) {
+                        k--;
+                    }
+
+
+                    j++;
+                    k--;
+                } else if (sum < 0) {
+                    j++;
+                } else {
+                    k--;
+                }
+
+
+            }
+        }
+
+        return results;
+    }
 
 
 
