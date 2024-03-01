315110de76a07b77c66280a00bd3810b21342cc7
JimM517 <80083198+JimM517@users.noreply.github.com>
2024-02-29 20:21:21 -0500

    container with most water

--- src/ReviewProblems/LeetCode150.java
+++ src/ReviewProblems/LeetCode150.java
@@ -716,6 +716,40 @@ public class LeetCode150 {
 
 
 
+    //. Container with most water
+    public int maxArea(int[] height) {
+
+        int left = 0;
+        int right = height.length - 1;
+
+        int max = 0;
+
+        while (left < right) {
+
+            int w = right - left;
+            int h = Math.min(height[left], height[right]);
+
+            int area = h * w;
+
+            max = Math.max(max, area);
+
+            if (height[left] < height[right]) {
+                left++;
+            } else if (height[left] > height[right]) {
+                right--;
+            } else {
+                left++;
+                right--;
+            }
+        }
+
+        return max;
+    }
+
+
+
+
+
 
 
 
