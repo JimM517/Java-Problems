9cf3693a1c3ceee1bcb73d123df97f9c53004f0a
JimM517 <80083198+JimM517@users.noreply.github.com>
2024-03-01 21:15:19 -0500

    two sum....again lol

--- src/ReviewProblems/LeetCode150.java
+++ src/ReviewProblems/LeetCode150.java
@@ -853,4 +853,75 @@ public class LeetCode150 {
 
 
 
+
+    // 205. Isomorphic Strings
+    public boolean isIsomorphic(String s, String t) {
+
+        if (s.length() != t.length()) {
+            return false;
+        }
+
+
+        Map<Character, Character> map1 = new HashMap<>();
+        Map<Character, Boolean> map2 = new HashMap<>();
+
+        for (int i = 0; i < s.length(); i++) {
+
+            char ch1 = s.charAt(i);
+            char ch2 = t.charAt(i);
+
+            if (map1.containsKey(ch1)) {
+                if (map1.get(ch1) != ch2) {
+                    return false;
+                }
+            } else {
+                if (map2.containsKey(ch2)) {
+                    return false;
+                } else {
+                    map1.put(ch1, ch2);
+                    map2.put(ch2, true);
+                }
+            }
+        }
+        return true;
+    }
+
+
+
+
+
+    // 1. obligatory Two Sum
+    public int[] twoSum(int[] nums, int target) {
+
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+
+            if (map.containsKey(target - nums[i])) {
+                return new int[]{map.get(target - nums[i]), i};
+            } else {
+                map.put(nums[i], i);
+            }
+
+
+        }
+
+        return new int[]{-1, -1};
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
 }
