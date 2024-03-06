d7b223cef9f1be22b178d5dc83b1b7a4f5eb1cd7
JimM517 <80083198+JimM517@users.noreply.github.com>
2024-03-05 20:35:34 -0500

    word pattern

--- src/ReviewProblems/LeetCode150.java
+++ src/ReviewProblems/LeetCode150.java
@@ -979,10 +979,43 @@ public class LeetCode150 {
 
 
 
-
-
-
     }
+
+
+
+    // 290. Word Pattern
+    public boolean wordPattern(String pattern, String s) {
+
+        String[] arr = s.split(" ");
+
+        if (pattern.length() != arr.length) {
+            return false;
+        }
+
+
+        Map<Character, String> map = new HashMap<>();
+
+        for (int i = 0; i < arr.length; i++) {
+
+            char c = pattern.charAt(i);
+
+            String str = arr[i];
+
+            if (map.containsKey(c)) {
+                if (!map.get(c).equals(str)) {
+                    return false;
+                }
+            } else {
+                if (map.containsValue(str)) {
+                    return false;
+                }
+                map.put(c, str);
+            }
+        }
+        return true;
+    }
+
+
 
 
 
