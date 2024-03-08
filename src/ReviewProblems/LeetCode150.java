4813cb8ce632a935c3720ce0a9658871c1837033
JimM517 <80083198+JimM517@users.noreply.github.com>
2024-03-07 21:01:42 -0500

    simplify path

--- src/ReviewProblems/LeetCode150.java
+++ src/ReviewProblems/LeetCode150.java
@@ -1219,6 +1219,42 @@ public class LeetCode150 {
 
 
 
+    // 71. Simplify path
+    public String simplifyPath(String path) {
+
+        Stack<String> result = new Stack<>();
+
+        StringBuilder sb = new StringBuilder();
+
+
+        String[] str = path.split("/");
+
+
+        for (int i = 0; i < str.length; i++) {
+
+            if (!result.isEmpty() && str[i].equals("..")) {
+                result.pop();
+            } else if (!str[i].equals("") && !str[i].equals(".") && !str[i].equals("..")) {
+                result.push(str[i]);
+            }
+
+        }
+
+        if (result.isEmpty()) {
+            return "/";
+        }
+
+        while (!result.isEmpty()) {
+            sb.insert(0, result.pop()).insert(0, "/");
+        }
+
+        return sb.toString();
+
+    }
+
+
+
+
 
 
 
