779e8f711bcf5eb2f84da6aee269b18dfac6c52a
JimM517 <80083198+JimM517@users.noreply.github.com>
2024-03-08 20:49:30 -0500

    summary ranges

--- src/ReviewProblems/LeetCode150.java
+++ src/ReviewProblems/LeetCode150.java
@@ -1175,6 +1175,42 @@ public class LeetCode150 {
     }
 
 
+    /** Intervals **/
+
+
+    // 228. Summary ranges
+    public List<String> summaryRanges(int[] nums) {
+
+        List<String> list = new ArrayList<>();
+
+        int n = nums.length;
+        int i = 0;
+
+        while (i < n) {
+
+            int j = i;
+
+            while (j < n - 1 && nums[j + 1] == nums[j] + 1) {
+                j++;
+            }
+
+
+            if (i == j) {
+                list.add(nums[i] + "");
+            } else {
+                list.add(nums[i] + "->" + nums[j]);
+            }
+
+            i = j + 1;
+
+        }
+
+
+        return list;
+
+    }
+
+
 
 
 
