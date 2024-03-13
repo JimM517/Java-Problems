b910a57e96268527674ad3fe7c57327e51ec1eaa
JimM517 <80083198+JimM517@users.noreply.github.com>
2024-03-13 18:38:07 -0400

    max circular sum array

--- src/ReviewProblems/KadanesAlgo/KadanesAlgo.java
+++ src/ReviewProblems/KadanesAlgo/KadanesAlgo.java
@@ -37,4 +37,45 @@ public class KadanesAlgo {
 
 
 
+    // 918. Maximum sum circular array
+    public int maxSubarraySumCircular(int[] nums) {
+
+      int n = nums.length;
+
+      int sum = 0;
+      for (int num : nums) {
+          sum += num;
+      }
+
+
+      int current = nums[0];
+      int max = nums[0];
+
+      int currentMin = nums[0];
+      int min = nums[0];
+
+
+      for (int i = 1; i < n; i++) {
+
+          current = Math.max(current + nums[i], nums[i]);
+          max = Math.max(max, current);
+
+
+          currentMin = Math.min(currentMin + nums[i], nums[i]);
+          min = Math.min(min, currentMin);
+
+
+
+      }
+
+      if (min == sum) {
+          return max;
+      }
+
+        return Math.max(max, sum - min);
+    }
+
+
+
+
 }
