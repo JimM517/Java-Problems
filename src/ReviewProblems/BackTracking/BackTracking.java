86bf16586c954c6758993f259696cc55b2386b6c
JimM517 <80083198+JimM517@users.noreply.github.com>
2024-03-20 18:36:15 -0400

    attempted n-queens

--- src/ReviewProblems/BackTracking/BackTracking.java
+++ src/ReviewProblems/BackTracking/BackTracking.java
@@ -197,6 +197,45 @@ public class BackTracking {
 
 
 
+    /** doesn't work **/
+    int count = 0;
+
+    // 52. N-Queens II
+    public int totalNQueens(int n) {
+
+        dfs(0, n, 0, 0, 0);
+
+        return count;
+    }
+
+
+
+
+
+
+    private void dfs(int row, int n, int column, int diag, int antiDiag) {
+
+        if (row == n) {
+            count++;
+            return;
+        }
+
+
+        for (int i = 0; i < n; i++) {
+            boolean isColSafe = ((1 << i) & column) == 0;
+            boolean isDiagSafe = ((1 << (n - 1 + row - 1)) & diag) == 0;
+            boolean isAntiDiagSafe = ((1 << (row + i)) & antiDiag) == 0;
+            if (isColSafe && isDiagSafe && isAntiDiagSafe) {
+                dfs(row + 1, n, (1 << i) | column, (1 << (n - 1 + row - i)) | diag, (1 << (row + i)) | antiDiag);
+            }
+        }
+
+
+
+    }
+
+
+
 
 
 
