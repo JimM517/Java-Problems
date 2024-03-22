6e929cbf61d822480816b4ac48bde2d9cc7d2e6f
JimM517 <80083198+JimM517@users.noreply.github.com>
2024-03-21 20:30:45 -0400

    string gcd

--- src/RandomPractice.java
+++ src/RandomPractice.java
@@ -2055,6 +2055,27 @@ public class RandomPractice {
 
 
 
+    // 1071. greatest common divisor of strings
+    public String gcdOfStrings(String str1, String str2) {
+
+      if (!(str1 + str2).equals(str2 + str1)) {
+          return "";
+      }
+      int gdcLen = gcd(str1.length(), str2.length());
+
+      return str1.substring(0, gdcLen);
+
+    }
+
+
+    private int gcd(int x, int y) {
+        if (y == 0) {
+            return x;
+        } else {
+            return gcd(y, x & y);
+        }
+    }
+
 
 
 
