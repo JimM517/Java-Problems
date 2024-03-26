8bc30712772661912440db9b0cfa8bec179f1617
JimM517 <80083198+JimM517@users.noreply.github.com>
2024-03-25 20:58:45 -0400

    single number

--- src/LeetCode/Easy/EasyLeetQues.java
+++ src/LeetCode/Easy/EasyLeetQues.java
@@ -1432,6 +1432,31 @@ public class EasyLeetQues {
 
 
 
+    // 136. Single Number
+    public int singleNumber(int[] nums) {
+
+        Map<Integer,Integer> res = new HashMap<>();
+
+        for (int num : nums) {
+            res.put(num, res.getOrDefault(num, 0) + 1);
+        }
+
+        for (Map.Entry<Integer, Integer> entry : res.entrySet()) {
+            if (entry.getValue() == 1) {
+                return entry.getKey();
+            }
+        }
+
+
+        return -1;
+
+    }
+
+
+
+
+
+
 
 
 
