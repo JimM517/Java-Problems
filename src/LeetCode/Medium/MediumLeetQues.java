74f291982284c5b12b2ca2f377990116e3921d09
JimM517 <80083198+JimM517@users.noreply.github.com>
2024-03-25 21:26:51 -0400

    3sum closest

--- src/LeetCode/Medium/MediumLeetQues.java
+++ src/LeetCode/Medium/MediumLeetQues.java
@@ -440,6 +440,37 @@ public class MediumLeetQues {
     }
 
 
+    // 3Sum closest
+    public int threeSumClosest(int[] nums, int target) {
+
+            Arrays.sort(nums);
+
+            int closestSum = Integer.MAX_VALUE;
+
+            for (int i = 0; i < nums.length - 2; i++) {
+
+                int j = i + 1;
+                int k = nums.length - 1;
+
+                while (j < k) {
+                    int sum = nums[i] + nums[j] + nums[k];
+
+                    if (Math.abs(target - sum) < Math.abs(target - closestSum)) {
+                        closestSum = sum;
+                    }
+
+                    if (sum > target) {
+                        k--;
+                    } else {
+                        j++;
+                    }
+
+                }
+            }
+            return closestSum;
+    }
+
+
 
 
     // 5. Longest Palindromic Substring
