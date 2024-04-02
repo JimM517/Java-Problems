64370b63512e6cefff7e40b9ec6e237e2b791657
JimM517 <80083198+JimM517@users.noreply.github.com>
2024-04-01 21:03:52 -0400

    rando coding bat

--- src/CodingBat/CodingBat.java
+++ src/CodingBat/CodingBat.java
@@ -1437,6 +1437,44 @@ public class CodingBat {
 
     }
 
+    // fix34
+    public int[] fix34(int[] nums) {
+
+      int i = 0;
+
+      while (i < nums.length && i != 3) {
+          i++;
+      }
+
+      int j = i + 1;
+
+      while (j < nums.length && j != 4) {
+          j++;
+      }
+
+      while (i < nums.length) {
+          if (nums[i] == 3) {
+              int temp = nums[i + 1];
+              nums[i + 1] = nums[j];
+              nums[j] = temp;
+
+              while (j < nums.length && nums[j] != 4) {
+                  j++;
+              }
+          }
+          i++;
+      }
+
+        return nums;
+
+
+
+    }
+
+
+
+
+
 
 
     // evenOdd
