8ba4a0564c9ce9ed619280002b3d255a58a04120
JimM517 <80083198+JimM517@users.noreply.github.com>
2024-04-06 13:35:00 -0400

    character counter

--- src/CodeWars/KyuSeven/KyuSeven.java
+++ src/CodeWars/KyuSeven/KyuSeven.java
@@ -1,5 +1,10 @@
 package CodeWars.KyuSeven;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class KyuSeven {
 
     // change two-dimensional array
@@ -19,6 +24,52 @@ public class KyuSeven {
         return array;
 
     }
+
+
+
+
+
+
+    // character counter
+    public boolean validateWord(String word) {
+
+        String lower = word.toLowerCase();
+
+        Map<Character, Integer> charMap = new HashMap<>();
+
+        for (Character idx : lower.toCharArray()) {
+
+            charMap.put(idx, charMap.getOrDefault(idx, 0) + 1);
+        }
+
+
+        List<Integer> charCount = new ArrayList<>(charMap.values());
+
+
+        for (int i = 0; i < charCount.size() - 1; i++) {
+            if (charCount.get(i) != charCount.get(i + 1)) {
+                return false;
+            }
+        }
+
+
+        return true;
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
