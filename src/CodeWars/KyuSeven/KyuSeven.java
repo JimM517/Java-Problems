5629a223c742a4f69185874c7c77dafee6af528d
JimM517 <80083198+JimM517@users.noreply.github.com>
2024-04-06 13:27:07 -0400

    change two dimensional array

--- src/CodeWars/KyuSeven/KyuSeven.java
+++ src/CodeWars/KyuSeven/KyuSeven.java
@@ -5,9 +5,18 @@ public class KyuSeven {
     // change two-dimensional array
     public int[][] matrix(int[][] array) {
 
+        for (int i = 0; i < array.length; i++) {
+
+            if (array[i][i] < 0) {
+                array[i][i] = 0;
+            } else {
+                array[i][i] = 1;
+            }
 
 
+        }
 
+        return array;
 
     }
 
