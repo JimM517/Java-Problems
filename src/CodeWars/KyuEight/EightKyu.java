08091615a8675fc2786c55bb8894ab4a0be6535d
JimM517 <80083198+JimM517@users.noreply.github.com>
2024-04-07 16:44:29 -0400

    sentence smash

--- src/CodeWars/KyuEight/EightKyu.java
+++ src/CodeWars/KyuEight/EightKyu.java
@@ -51,4 +51,21 @@ public class EightKyu {
 
 
 
+    // sentence smash
+    public String smash(String... words) {
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < words.length; i++) {
+            sb.append(words[i]);
+
+            if (i < words.length - 1) {
+                sb.append(" ");
+            }
+        }
+        return sb.toString();
+    }
+
+
+
 }
