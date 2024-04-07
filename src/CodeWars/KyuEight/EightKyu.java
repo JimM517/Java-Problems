a355ee22df0172acc93b75ea9c3abb575e287147
JimM517 <80083198+JimM517@users.noreply.github.com>
2024-04-07 17:03:38 -0400

    collinearity

--- src/CodeWars/KyuEight/EightKyu.java
+++ src/CodeWars/KyuEight/EightKyu.java
@@ -68,4 +68,16 @@ public class EightKyu {
 
 
 
+
+
+    // collinear vectors
+    public boolean collinearity(int x1, int y1, int x2, int y2) {
+
+        return x1 * y2 == y1 * x2;
+
+    }
+
+
+
+
 }
