8b583b5f3ad5a9d5f83d1d6a5d15cd234b34f18a
JimM517 <80083198+JimM517@users.noreply.github.com>
2024-04-08 20:44:49 -0400

    spiral matrix

--- src/LeetCodeOneFifty/Matrix/Matrix.java
+++ src/LeetCodeOneFifty/Matrix/Matrix.java
@@ -41,13 +41,59 @@ public class Matrix {
 
     // 54. spiral matrix
     // TODO finish this problem
-    public List<Integer> spiralOrder(int[][] nums) {
+    public List<Integer> spiralOrder(int[][] matrix) {
+
+        List<Integer> result = new ArrayList<>();
+
+
+        if (matrix == null || matrix.length == 0) {
+            return result;
+        }
 
 
 
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+        int top = 0, bottom = rows - 1, left = 0, right = cols - 1;
 
 
-        return new ArrayList<>();
+
+        while (top <= bottom && left <= right) {
+
+            // traverse top row
+            for (int i = left; i <= right; i++) {
+                result.add(matrix[top][i]);
+            }
+            top++;
+
+
+            // traverse right column
+            for (int i = top; i <= bottom; i++) {
+                result.add(matrix[i][right]);
+            }
+            right--;
+
+
+            // traverse bottom row
+            // ensure not to revisit top row
+            if (top <= bottom) {
+                for (int i = right; i >= left; i--) {
+                    result.add(matrix[bottom][i]);
+                }
+                bottom--;
+            }
+
+            // traverse left column
+            // ensure not to revisit the right column
+            if (left <= right) {
+                for (int i = bottom; i >= top; i--) {
+                    result.add(matrix[i][left]);
+                }
+                left++;
+            }
+
+        }
+        return result;
 
     }
 
