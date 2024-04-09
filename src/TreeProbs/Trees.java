2019439dbf6badb60ec9327ee3ae31da33e426db
JimM517 <80083198+JimM517@users.noreply.github.com>
2024-04-09 18:50:36 -0400

    validate BST

--- src/TreeProbs/Trees.java
+++ src/TreeProbs/Trees.java
@@ -2,6 +2,28 @@ package TreeProbs;
 
 public class Trees {
 
+    private class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {};
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+
+
+
+
+
 
     // 226 Invert Binary Tree
 //    public TreeNode invertTree(TreeNode root) {
@@ -37,6 +59,32 @@ public class Trees {
 //        root.right = root.left;
 //        root.left = temp;
 //    }
+
+
+
+    // 98 Validate Binary Search Tree
+    public boolean isValidBST(TreeNode root) {
+
+            return validate(root, Long.MIN_VALUE, Long.MAX_VALUE);
+    }
+
+    private static boolean validate(TreeNode root, long min, long max) {
+        // if root is null then its valid
+        if (root == null) {
+            return true;
+        }
+
+        // if root is less than min value or greater than max value return false
+        if (root.val <= min || root.val >= max) {
+            return false;
+        }
+
+        // check on both sides if both are true then only its valid
+        return validate(root.left, min, root.val) && validate(root.right, root.val, max);
+
+
+    }
+
 
 
 
