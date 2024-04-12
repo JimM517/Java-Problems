19e5c58729571bcb8139025ee36e68fcbe9894b8
JimM517 <80083198+JimM517@users.noreply.github.com>
2024-04-11 20:16:10 -0400

    remove k digits

--- src/LeetCode/Medium/MediumLeetQues.java
+++ src/LeetCode/Medium/MediumLeetQues.java
@@ -1710,6 +1710,50 @@ public class MediumLeetQues {
 
 
 
+    // 402. remove k digits
+    public String removeKDigits(String num, int k) {
+
+
+        Stack<Character> stack = new Stack<>();
+
+        for (char digit : num.toCharArray()) {
+            while (!stack.isEmpty() && k > 0 && stack.peek() > digit) {
+                stack.pop();
+                k--;
+            }
+            stack.push(digit);
+        }
+
+
+
+
+        while (k > 0 && !stack.isEmpty()) {
+            stack.pop();
+            k--;
+        }
+
+
+
+        StringBuilder sb = new StringBuilder();
+
+        while (!stack.isEmpty()) {
+            sb.append(stack.pop());
+        }
+
+        sb.reverse();
+
+
+        while(sb.length() > 0 && sb.charAt(0) == '0') {
+            sb.deleteCharAt(0);
+        }
+
+
+        return sb.length() > 0 ? sb.toString() : "0";
+
+    }
+
+
+
 
 
 
