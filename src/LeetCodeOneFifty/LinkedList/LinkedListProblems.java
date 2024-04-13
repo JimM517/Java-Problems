65de89cd44ee75b98ce483f103d1fce98db58995
JimM517 <80083198+JimM517@users.noreply.github.com>
2024-04-12 21:07:20 -0400

    add two numbers

--- src/LeetCodeOneFifty/LinkedList/LinkedListProblems.java
+++ src/LeetCodeOneFifty/LinkedList/LinkedListProblems.java
@@ -139,6 +139,45 @@ public class LinkedListProblems {
 
 
 
+    // 2. add two numbers
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+
+        int carry = 0;
+
+        ListNode pre = new ListNode(-1);
+        ListNode temp = pre;
+
+        while (l1 != null || l2 != null) {
+
+
+            int sum = 0;
+
+            if (l1 != null) {
+                sum += l1.val;
+                l1 = l1.next;
+            }
+            if (l2 != null) {
+                sum += l2.val;
+                l2 = l2.next;
+            }
+            sum += carry;
+            temp.next = new ListNode(sum % 10);
+            temp = temp.next;
+            carry = sum / 10;
+
+
+        }
+
+        if (carry != 0) {
+            temp.next = new ListNode(carry);
+        }
+
+        return pre.next;
+
+
+    }
+
+
 
 
 
