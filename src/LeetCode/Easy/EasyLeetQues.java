1653242d7eab2ba650898a8930be1fb4fd785568
JimM517 <80083198+JimM517@users.noreply.github.com>
2024-04-12 21:51:28 -0400

    monotonic array

--- src/LeetCode/Easy/EasyLeetQues.java
+++ src/LeetCode/Easy/EasyLeetQues.java
@@ -1565,7 +1565,24 @@ public class EasyLeetQues {
 
 
 
+    // 896 monotonic array
+    public boolean isMonotonic(int[] nums) {
 
+        boolean increasing = true;
+        boolean decreasing = true;
+
+
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] > nums[i - 1]) {
+                decreasing = false;
+            } else if (nums[i] < nums[i - 1]) {
+                increasing = false;
+            }
+        }
+
+
+        return increasing || decreasing;
+    }
 
 
 
