41b7393e2322bdd9691739d8e73f026f084b284a
JimM517 <80083198+JimM517@users.noreply.github.com>
2024-04-12 21:38:29 -0400

    find the difference

--- src/LeetCode/Easy/EasyLeetQues.java
+++ src/LeetCode/Easy/EasyLeetQues.java
@@ -1544,7 +1544,22 @@ public class EasyLeetQues {
 
 
 
+    // 389 Find the difference
+    public char findTheDifference(String s, String t) {
 
+            char c = 0;
+
+            for (char cs : s.toCharArray()) {
+                c ^= cs;
+            }
+
+            for (char ct : t.toCharArray()) {
+                c ^= ct;
+            }
+
+            return c;
+
+    }
 
 
 
