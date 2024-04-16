f5a8bbc0c69213b2a3c1c2cee65ddf5a03054aa1
JimM517 <80083198+JimM517@users.noreply.github.com>
2024-04-16 18:46:23 -0400

    pascals triangle two

--- src/LeetCode/Easy/EasyLeetQues.java
+++ src/LeetCode/Easy/EasyLeetQues.java
@@ -1630,6 +1630,31 @@ public class EasyLeetQues {
 
 
 
+    // 119. pascals triangle II
+    public List<Integer> getRow(int rowIndex) {
+
+        List<Integer> result = new ArrayList<>();
+
+        if (rowIndex < 0) {
+            return result;
+        }
+
+
+        for (int i = 0; i <= rowIndex; i++) {
+
+           result.add(1);
+
+           for (int j = i - 1; j > 0; j--) {
+               result.set(j, result.get(j - 1) + result.get(j));
+            }
+        }
+
+
+        return result;
+    }
+
+
+
 
 
 
