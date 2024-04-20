15e24354ad1c4f7a88addca004ea52a5d847f25d
JimM517 <80083198+JimM517@users.noreply.github.com>
2024-04-19 21:08:56 -0400

    find disappeared numbers

--- src/LeetCode/Easy/EasyLeetQues.java
+++ src/LeetCode/Easy/EasyLeetQues.java
@@ -1763,7 +1763,34 @@ public class EasyLeetQues {
 
 
 
+    // 448. find all numbers disappeared in an array
+    public List<Integer> findDisappearedNumbers(int[] nums) {
 
+        List<Integer> result = new ArrayList<>();
+
+        int i = 0;
+
+        while (i < nums.length) {
+
+            if (nums[i] != i + 1 && nums[nums[i] - 1] != nums[i]) {
+                int temp = nums[nums[i] - 1];
+                nums[nums[i] - 1] = nums[i];
+                nums[i] = temp;
+            } else {
+                i++;
+            }
+
+
+        }
+
+        for (int j = 0; j < nums.length; j++) {
+            if (nums[j] != j + 1) {
+                result.add(j + 1);
+            }
+        }
+
+        return result;
+    }
 
 
 
