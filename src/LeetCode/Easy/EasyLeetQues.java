9b426e4fa77d27b51445200db8858214c520d77e
JimM517 <80083198+JimM517@users.noreply.github.com>
2024-04-21 09:53:02 -0400

    find path is exists

--- src/LeetCode/Easy/EasyLeetQues.java
+++ src/LeetCode/Easy/EasyLeetQues.java
@@ -1797,9 +1797,46 @@ public class EasyLeetQues {
 
 
 
+    // 1971. find if path exists in graph
+    public boolean validPath(int n, int[][] edges, int source, int destination) {
+
+            Map<Integer, List<Integer>> graph = new HashMap<>();
+
+            for (int[] edge : edges) {
+                int u = edge[0];
+                int v = edge[1];
+
+                graph.computeIfAbsent(u, k -> new ArrayList<>()).add(v);
+                graph.computeIfAbsent(v, k -> new ArrayList<>()).add(u);
+            }
+
+            Set<Integer> visited = new HashSet<>();
+
+
+            return searchPath(source, destination, graph, visited);
+
+
+    }
 
 
 
+
+    private boolean searchPath(int node, int destination, Map<Integer, List<Integer>> graph, Set<Integer> visited) {
+        if (node == destination) {
+            return true;
+        }
+
+        visited.add(node);
+
+        for (int neighbor : graph.getOrDefault(node, new ArrayList<>())) {
+            if (!visited.contains(neighbor)) {
+                if (searchPath(neighbor, destination, graph, visited)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
 
 
 
