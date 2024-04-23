fde735de38d1f43a2c05e9a5a9ddedefc0b1d283
JimM517 <80083198+JimM517@users.noreply.github.com>
2024-04-22 21:02:44 -0400

    game of life

--- src/LeetCodeOneFifty/Matrix/Matrix.java
+++ src/LeetCodeOneFifty/Matrix/Matrix.java
@@ -331,6 +331,81 @@ public class Matrix {
 
 
 
+    // 289. Game of life
+    public void gameOfLife(int[][] board) {
+        int[][] nextState = new int[board.length][board[0].length];
+
+
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[i].length; j++) {
+                int liveNeighbors = countLiveNeighbors(board, i, j);
+
+                if (board[i][j] == 1) {
+                    if (liveNeighbors < 2 || liveNeighbors > 3) {
+                        nextState[i][j] = 0;
+                    } else {
+                        nextState[i][j] = 1;
+                    }
+                } else {
+                    if (liveNeighbors == 3) {
+                        nextState[i][j] = 1;
+                    }
+                }
+
+
+
+
+            }
+        }
+
+
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[i].length; j++) {
+                board[i][j] = nextState[i][j];
+            }
+        }
+
+
+
+    }
+
+
+    private int countLiveNeighbors(int[][] board, int row, int col) {
+
+        int count = 0;
+
+        int[][] directions = {{-1,-1}, {-1,0}, {-1,1}, {0,-1}, {0,1}, {1,-1}, {1,0}, {1,1}};
+
+
+        for (int[] dir : directions) {
+
+
+            int newRow = row + dir[0];
+            int newCol = col + dir[1];
+
+
+
+            if (newRow >= 0 && newRow < board.length && newCol >= 0 && newCol < board[0].length) {
+                count += board[newRow][newCol];
+            }
+
+        }
+        return count;
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
