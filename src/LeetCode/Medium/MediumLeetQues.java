4b06ebb918e67bf5345bd1410f249902cf9406f4
JimM517 <80083198+JimM517@users.noreply.github.com>
2024-04-24 20:56:14 -0400

    longest ideal subsequence

--- src/LeetCode/Medium/MediumLeetQues.java
+++ src/LeetCode/Medium/MediumLeetQues.java
@@ -1755,6 +1755,26 @@ public class MediumLeetQues {
 
 
 
+    // 2370. longest ideal subsequence
+    public int longestIdealString(String s, int k) {
+
+        int[] dp = new int[26];
+
+        for (int i = 0; i < s.length(); i++) {
+
+            int a = s.charAt(i) - 'a', mx = 0;
+
+            for (int b = Math.max(0, a - k); b <= Math.min(25, a + k); b++) {
+                mx = Math.max(mx, dp[b]);
+            }
+            dp[a] = 1 + mx;
+
+
+        }
+
+        return Arrays.stream(dp).max().getAsInt();
+    }
+
 
 
 
