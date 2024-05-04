ab6978c0ba8d643a4f869419131111fc9bcc8433
JimM517 <80083198+JimM517@users.noreply.github.com>
2024-05-03 20:50:40 -0400

    divisor game

---
+++ src/DynamicProgramming/DPProblems.java
@@ -0,0 +1,19 @@
+package DynamicProgramming;
+
+public class DPProblems {
+
+
+    // 1025. Divisor Game
+    public boolean divisorGame(int n) {
+        return n % 2 == 0;
+    }
+
+
+
+
+
+
+
+
+
+}
