010cbeadf36651bd78264306d97acaac0d021e3d
JimM517 <80083198+JimM517@users.noreply.github.com>
2024-05-03 21:01:57 -0400

    max repeating substring

--- src/DynamicProgramming/DPProblems.java
+++ src/DynamicProgramming/DPProblems.java
@@ -12,6 +12,20 @@ public class DPProblems {
 
 
 
+    // 1668. maximum repeating substring
+    public int maxRepeating(String sequence, String word) {
+
+       int count = 0;
+
+       String add = word;
+
+       while (sequence.contains(word)) {
+           count++;
+           word += add;
+       }
+        return count;
+    }
+
 
 
 
