09639a5cd23c6cd10add94f33ca04446e98b4cfc
JimM517 <80083198+JimM517@users.noreply.github.com>
2024-05-15 20:48:27 -0400

    compare version numbers

--- src/LeetCode/Medium/MediumLeetQues.java
+++ src/LeetCode/Medium/MediumLeetQues.java
@@ -1966,6 +1966,41 @@ public class MediumLeetQues {
 
 
 
+    // 165. Compare Version Numbers
+    public int compareVersion(String version1, String version2) {
+
+        String[] v1 = version1.split("\\.");
+        String[] v2 = version2.split("\\.");
+
+
+        int maxLen = Math.max(v1.length, v2.length);
+
+        for (int i = 0; i < maxLen; i++) {
+            int num1 = (i < v1.length) ? Integer.parseInt(v1[i]) : 0;
+            int num2 = (i < v2.length) ? Integer.parseInt(v2[i]) : 0;
+
+            if (num1 < num2) {
+                return -1;
+            }
+            if (num1 > num2) {
+                return 1;
+            }
+
+
+
+        }
+
+        return 0;
+
+
+    }
+
+
+
+
+
+
+
 
 
 }
