c16611f4b3d60bbe5f7dd5a21177b112fc9a0b8c
JimM517 <80083198+JimM517@users.noreply.github.com>
2024-06-10 20:58:47 -0400

    height checker

--- src/LeetCode/Easy/EasyLeetQues.java
+++ src/LeetCode/Easy/EasyLeetQues.java
@@ -2083,6 +2083,55 @@ public class EasyLeetQues {
 
 
 
+
+    // 1051. height checker
+    public int heightChecker(int[] heights) {
+
+        int[] expected = new int[heights.length];
+
+        for (int i = 0; i < heights.length; i++) {
+
+          expected[i] = heights[i];
+
+        }
+
+        Arrays.sort(expected);
+
+        int count = 0;
+
+        for (int i = 0; i < expected.length; i++) {
+            if (expected[i] != heights[i]) {
+                count++;
+            }
+        }
+
+        return count;
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
 
 
