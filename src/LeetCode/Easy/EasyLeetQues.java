34362ff83adac90524ffdd5e880fe2a9559daec4
JimM517 <80083198+JimM517@users.noreply.github.com>
2024-06-12 20:43:48 -0400

    min num of moves to seat everyone

--- src/LeetCode/Easy/EasyLeetQues.java
+++ src/LeetCode/Easy/EasyLeetQues.java
@@ -2181,7 +2181,20 @@ public class EasyLeetQues {
 
 
 
+    public int minMovesToSeat(int[] seats, int[] students) {
 
+        int result = 0;
+
+        Arrays.sort(seats);
+        Arrays.sort(students);
+
+        for (int i = 0; i < seats.length; i++) {
+            result += Math.abs(seats[i] - students[i]);
+        }
+
+        return result;
+
+    }
 
 
 
