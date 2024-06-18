75e8fda858c0e0f94d14f5468dc0b2119520023b
JimM517 <80083198+JimM517@users.noreply.github.com>
2024-06-17 20:53:53 -0400

    most profit assigning work

--- src/LeetCode/Medium/MediumLeetQues.java
+++ src/LeetCode/Medium/MediumLeetQues.java
@@ -2246,5 +2246,36 @@ public class MediumLeetQues {
 
 
 
+    // 826. Most profit assigning work
+    public int maxProfitAssignment(int[] difficulty, int[] profit, int[] worker) {
+
+
+        int n = difficulty.length;
+        int[][] jobs = new int[n][2];
+
+        for (int i = 0; i < n; i++) {
+            jobs[i][0] = difficulty[i];
+            jobs[i][1] = profit[i];
+        }
+
+        Arrays.sort(jobs, (a, b) -> a[0] - b[0]);
+        Arrays.sort(worker);
+
+        int maxProfit = 0;
+        int i = 0;
+        int best = 0;
+
+        for (int ability : worker) {
+            while (i < n && ability >= jobs[i][0]) {
+                best = Math.max(best, jobs[i][1]);
+                i++;
+            }
+            maxProfit += best;
+        }
+
+        return maxProfit;
+    }
+
+
 
 }
