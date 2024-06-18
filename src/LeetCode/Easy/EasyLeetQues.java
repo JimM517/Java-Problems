5818c7d7e7e46421d8cf9682250dda7d2ea60efa
JimM517 <80083198+JimM517@users.noreply.github.com>
2024-06-18 19:20:38 -0400

    assign cookies

--- src/LeetCode/Easy/EasyLeetQues.java
+++ src/LeetCode/Easy/EasyLeetQues.java
@@ -2204,6 +2204,22 @@ public class EasyLeetQues {
 
 
 
+    // 455. assign cookies
+    public int findContentChildren(int[] g, int[] s) {
+
+     Arrays.sort(g);
+     Arrays.sort(s);
+
+     int i = 0;
+
+     for (int j = 0; i < g.length && j < s.length; j++) {
+         if (g[i] <= s[j]) {
+             i++;
+         }
+     }
+        return i;
+    }
+
 
 
 
