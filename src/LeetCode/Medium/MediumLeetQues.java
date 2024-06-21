3c57a1f0f77d59328312945c5d5427f39b5e4eea
JimM517 <80083198+JimM517@users.noreply.github.com>
2024-06-20 20:36:38 -0400

    grumpy bookstore owner

--- src/LeetCode/Medium/MediumLeetQues.java
+++ src/LeetCode/Medium/MediumLeetQues.java
@@ -2399,6 +2399,33 @@ public class MediumLeetQues {
     }
 
 
+    // 1052 grumpy bookstore owner
+    public int maxSatisfied(int[] customers, int[] grumpy, int minutes) {
+
+        int sum = 0;
+
+        int n = customers.length;
+
+        for (int i = 0; i < n; i++) {
+            sum += customers[i] * (1 - grumpy[i]);
+            grumpy[i] = customers[i] * grumpy[i];
+        }
+
+        int max = 0;
+        for (int i = 0; i < minutes; i++) {
+            max += grumpy[i];
+        }
+
+        int save = max;
+        for (int i = minutes; i < n; i++) {
+            save += grumpy[i] - grumpy[i - minutes];
+            max = save > max ? save : max;
+        }
+
+        return sum + max;
+    }
+
+
 
 
 }
