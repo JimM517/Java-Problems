3dbae2c2fbbf92e8f09daa3af19fc8320c894884
JimM517 <80083198+JimM517@users.noreply.github.com>
2024-06-21 20:11:55 -0400

    count number of nice subarrays

--- src/LeetCode/Medium/MediumLeetQues.java
+++ src/LeetCode/Medium/MediumLeetQues.java
@@ -2428,4 +2428,41 @@ public class MediumLeetQues {
 
 
 
+
+    // 1248. count number of nice subarrays
+    public int numberofSubarrays(int[] nums, int k) {
+
+        int total = 0;
+
+        int sum = 0;
+
+       for (int i = 0; i < nums.length; i++) {
+           if (nums[i] % 2 == 0) {
+               nums[i] = 0;
+           }
+           else {
+               nums[i] = 1;
+           }
+       }
+
+
+
+       Map<Integer, Integer> map = new HashMap<>();
+       map.put(0, 1);
+
+       for (int i = 0; i < nums.length; i++) {
+           sum += nums[i];
+
+           if (map.containsKey(sum - k)) {
+               total += map.get(sum - k);
+           }
+           map.put(sum, map.getOrDefault(sum, 0) + 1);
+       }
+
+        return total;
+    }
+
+
+
+
 }
