d62ea75a1cd8c0548808ecd13420ccda0b2a82b9
JimM517 <80083198+JimM517@users.noreply.github.com>
2024-06-23 09:34:29 -0400

    asteroid collision

--- src/LeetCode/Medium/MediumLeetQues.java
+++ src/LeetCode/Medium/MediumLeetQues.java
@@ -2497,6 +2497,49 @@ public class MediumLeetQues {
 
 
 
+    // 735. asteroid collision
+    public int[] asteroidCollision(int[] asteroids) {
+
+       Stack<Integer> stack = new Stack<>();
+
+       for (int asteroid : asteroids) {
+
+           boolean exploded = false;
+
+
+           while (!stack.isEmpty() && asteroid < 0 && stack.peek() > 0) {
+               int top = stack.pop();
+
+
+               if (top == -asteroid) {
+                   exploded = true;
+                   break;
+               } else if (top > -asteroid) {
+                   stack.push(top);
+                   exploded = true;
+                   break;
+               }
+
+
+           }
+           if (!exploded) {
+               stack.push(asteroid);
+           }
+       }
+
+
+       int[] result = new int[stack.size()];
+       for (int i = result.length - 1; i >= 0; i--) {
+           result[i] = stack.pop();
+       }
+       return result;
+
+    }
+
+
+
+
+
 
 
 
