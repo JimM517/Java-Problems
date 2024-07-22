905172b4c55f6df13a5097665802df9b55cdd78b
JimM517 <80083198+JimM517@users.noreply.github.com>
2024-07-22 18:56:30 -0400

    sort the people

--- src/LeetCode/Easy/EasyLeetQues.java
+++ src/LeetCode/Easy/EasyLeetQues.java
@@ -2391,7 +2391,31 @@ public class EasyLeetQues {
 
 
 
+    // 2418. Sort the people
+    public String[] sortPeople(String[] names, int[] heights) {
 
+        Map<Integer, String> map = new HashMap<>();
+
+        for (int i = 0; i < names.length; i++) {
+            map.put(heights[i], names[i]);
+        }
+
+        Arrays.sort(heights);
+
+
+        String[] result = new String[heights.length];
+        int index = 0;
+
+        for (int i = heights.length - 1; i >= 0; i--) {
+
+            result[index] = map.get(heights[i]);
+            index++;
+
+        }
+
+        return result;
+
+    }
 
 
 
