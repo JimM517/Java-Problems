c0c559a8352ed07c0c1b2ee75d4edaebd0cdb89d
JimM517 <80083198+JimM517@users.noreply.github.com>
2024-07-23 20:22:37 -0400

    sort jumbled nums

--- src/LeetCode/Medium/MediumLeetQues.java
+++ src/LeetCode/Medium/MediumLeetQues.java
@@ -2809,6 +2809,46 @@ public class MediumLeetQues {
 
 
 
+    // 2191 Sort the jumbled numbers
+    // TODO redo this problem
+    public int[] sortJumbled(int[] mapping, int[] nums) {
+        int n = nums.length, k = 0;
+        Integer[] indices = new Integer[n];
+        for (int i = 0; i < n; ++i)
+            indices[i] = i;
+        Arrays.sort(indices, Comparator.comparing(i -> convert(nums[i], mapping)));
+        int[] ans = new int[n];
+        for (int idx : indices)
+            ans[k++] = nums[idx];
+        return ans;
+
+    }
+
+
+    private int convert(int num, int[] mapping) {
+
+        if (num == 0) {
+            return mapping[0];
+        }
+
+        int n = 0;
+        int f = 1;
+
+        while (num > 0) {
+
+            n += mapping[num % 10] * f;
+            f *= 10;
+            num /= 10;
+
+        }
+
+        return n;
+
+    }
+
+
+
+
 
 
 
