16edafc81650d4d7094fdd953e97a4bd3166e443
JimM517 <80083198+JimM517@users.noreply.github.com>
2024-07-24 21:01:01 -0400

    sort an array

--- src/LeetCode/Medium/MediumLeetQues.java
+++ src/LeetCode/Medium/MediumLeetQues.java
@@ -2849,10 +2849,55 @@ public class MediumLeetQues {
 
 
 
+    // 912. sort an array
+    public int[] sortArray(int[] nums) {
+
+        mergeSort(nums, 0, nums.length - 1);
+        return nums;
+    }
+
+
+    private void mergeSort(int[] nums, int start, int end) {
+
+        if (start < end) {
+            int mid = (start + end) / 2;
+            mergeSort(nums, start, mid);
+            mergeSort(nums, mid + 1, end);
+            merge(nums, start, mid, end);
+        }
 
 
 
+    }
 
+    public void merge(int[] nums, int start, int mid, int end) {
+
+        int i = start;
+        int j = mid + 1;
+        int k = 0;
+
+        int[] temp = new int[end - start + 1];
+
+        while (i <= mid && j <= end) {
+            if (nums[i] < nums[j]) {
+                temp[k++] = nums[i++];
+            } else {
+                temp[k++] = nums[j++];
+            }
+        }
+
+        while (i <= mid) {
+            temp[k++] = nums[i++];
+        }
+        while (j <= end) {
+            temp[k++] = nums[j++];
+        }
+
+        for (int pointer = start; pointer <= end; pointer++) {
+            nums[pointer] = temp[pointer - start];
+        }
+
+    }
 
 
 
