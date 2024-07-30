28fc3aab6817ee6050f26da5c4960a596a44835c
JimM517 <80083198+JimM517@users.noreply.github.com>
2024-07-29 20:35:57 -0400

    count number of teams

--- src/LeetCode/Medium/MediumLeetQues.java
+++ src/LeetCode/Medium/MediumLeetQues.java
@@ -2976,7 +2976,43 @@ public class MediumLeetQues {
 
 
 
+    // 1395. Count number of teams
+    // TODO this works and passes but need to find better solution
+    public int numTeams(int[] rating) {
 
+        int count = 0;
+
+        for (int i = 0; i < rating.length; i++) {
+            for (int j = i + 1; j < rating.length; j++) {
+                if (rating[i] > rating[j]) {
+                    for (int k = j + 1; k < rating.length; k++) {
+                        if (rating[j] > rating[k]) {
+                            count++;
+                        }
+                    }
+                }
+            }
+        }
+
+        for (int i = 0; i < rating.length; i++) {
+            for (int j = i + 1; j < rating.length; j++) {
+                if (rating[i] < rating[j]) {
+                    for (int k = j + 1; k < rating.length; k++) {
+                        if (rating[j] < rating[k]) {
+                            count++;
+                        }
+                    }
+                }
+            }
+        }
+
+
+
+        return count;
+
+
+
+    }
 
 
 
