1f7d79c09bedf8eca451adbfd8b6016acb547b78
JimM517 <80083198+JimM517@users.noreply.github.com>
2024-08-05 20:32:32 -0400

    range of subarray sums

--- src/LeetCode/Medium/MediumLeetQues.java
+++ src/LeetCode/Medium/MediumLeetQues.java
@@ -3081,6 +3081,35 @@ public class MediumLeetQues {
 
 
 
+    // 1508. range sum of sorted subarray sums
+    public int rangeSum(int[] nums, int n, int left, int right) {
+
+        List<Integer> arr = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+
+            int pre = 0;
+            for (int j = i; j < n; j++) {
+                pre += nums[j];
+                arr.add(pre);
+            }
+
+
+        }
+
+        Collections.sort(arr);
+        int result = 0;
+        int MOD = 1000000007;
+
+        for (int i = left - 1; i < right; i++) {
+            result = (result + arr.get(i)) % MOD;
+        }
+
+        return result;
+    }
+
+
+
 
 
 
