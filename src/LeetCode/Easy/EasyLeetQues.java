e16e6812c5739651fb39c79c7c8ef78297875038
JimM517 <80083198+JimM517@users.noreply.github.com>
2024-08-14 20:48:40 -0400

    lemonade change

--- src/LeetCode/Easy/EasyLeetQues.java
+++ src/LeetCode/Easy/EasyLeetQues.java
@@ -2576,6 +2576,40 @@ public class EasyLeetQues {
 
 
 
+    // 860. Lemonade Change
+    public boolean lemonadeChange(int[] bills) {
+
+        int fiveCount = 0;
+        int tenCount = 0;
+
+        for (int i = 0; i < bills.length; i++) {
+            if (bills[i] == 5) {
+                fiveCount++;
+            } else if (bills[i] == 10) {
+                if (fiveCount == 0) {
+                    return false;
+                }
+                fiveCount--;
+                tenCount++;
+            } else if (bills[i] == 20) {
+                if (tenCount > 0 && fiveCount > 0) {
+                    tenCount--;
+                    fiveCount--;
+                } else if (fiveCount >= 3) {
+                    fiveCount -= 3;
+                } else {
+                    return false;
+                }
+            }
+        }
+
+
+        return true;
+
+
+    }
+
+
 
 
 
