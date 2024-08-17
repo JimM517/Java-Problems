b2f13db1ce03a5645954b418e77603317fcd75c9
JimM517 <80083198+JimM517@users.noreply.github.com>
2024-08-17 07:31:15 -0400

    max num points

--- src/LeetCode/Medium/MediumLeetQues.java
+++ src/LeetCode/Medium/MediumLeetQues.java
@@ -3153,11 +3153,38 @@ public class MediumLeetQues {
     public long maxPoints(int[][] points) {
 
         long result = 0;
+        int m = points.length;
+        int n = points[0].length;
+
+        long[] dp = new long[n];
+
+        for (int i = 0; i < m; i++) {
+
+            for (int j = 0; j < n; j++) {
+
+
+                dp[j] += points[i][j];
+
+            }
+
+
+            for (int j = 1; j < n; j++) {
+                dp[j] = Math.max(dp[j], dp[j - 1] - 1);
+            }
+
+
+            for (int j = n - 2; j >= 0; j--) {
+                dp[j] = Math.max(dp[j], dp[j + 1] - 1);
+            }
 
 
 
 
+        }
 
+        for (int i = 0; i < n; i++) {
+            result = Math.max(result, dp[i]);
+        }
 
         return result;
     }
