12265ccd11e10e2c7f52cfee1630c940bf5d4d98
JimM517 <80083198+JimM517@users.noreply.github.com>
2024-09-03 18:25:45 -0400

    sum digits after convert

--- src/LeetCode/Easy/EasyLeetQues.java
+++ src/LeetCode/Easy/EasyLeetQues.java
@@ -2688,6 +2688,29 @@ public class EasyLeetQues {
 
 
 
+    // 1945. Sum of digits of string after convert
+    public int getLucky(String s, int k) {
+
+        StringBuilder number = new StringBuilder();
+
+        for (char x : s.toCharArray()) {
+            number.append(x - 'a' + 1);
+        }
+
+
+
+
+        while (k > 0) {
+            int temp = 0;
+            for (char x : number.toString().toCharArray()) {
+                temp += x - '0';
+            }
+            number = new StringBuilder(String.valueOf(temp));
+            k--;
+        }
+        return Integer.parseInt(number.toString());
+    }
+
 
 
 
