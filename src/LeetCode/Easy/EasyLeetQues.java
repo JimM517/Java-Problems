4c561de13a955f8c3fd83f037d37ad41853b018c
JimM517 <80083198+JimM517@users.noreply.github.com>
2024-09-10 20:39:52 -0400

    min bits to convert num

--- src/LeetCode/Easy/EasyLeetQues.java
+++ src/LeetCode/Easy/EasyLeetQues.java
@@ -2714,7 +2714,21 @@ public class EasyLeetQues {
 
 
 
+    // 2220. Minimum bit flips to convert number
+    public int minBitFlips(int start, int goal) {
 
+      int count = 0;
+
+      int diff = start ^ goal;
+
+      while (diff > 0) {
+          count++;
+          diff = diff & (diff - 1);
+      }
+
+
+      return count;
+    }
 
 
 
