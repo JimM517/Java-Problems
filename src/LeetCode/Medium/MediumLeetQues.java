b5fdf3377216321bf547d7cd9742cba397cec3a8
JimM517 <80083198+JimM517@users.noreply.github.com>
2024-09-12 20:59:41 -0400

    xor queries of a subarray

--- src/LeetCode/Medium/MediumLeetQues.java
+++ src/LeetCode/Medium/MediumLeetQues.java
@@ -3376,6 +3376,31 @@ public class MediumLeetQues {
 
 
 
+    // 1310. XOR queries of a subarray
+    public int[] xorQueries(int[] arr, int[][] queries) {
+        int[] result = new int[queries.length];
+        int[] prefix = new int[arr.length];
+
+        prefix[0] = arr[0];
+
+
+        for (int i = 1; i < arr.length; i++) {
+            prefix[i] = prefix[i - 1] ^ arr[i];
+        }
+
+        for (int i = 0; i < queries.length; i++) {
+            int left = queries[i][0];
+            int right = queries[i][1];
+
+            if (left == 0) {
+                result[i] = prefix[right];
+            } else {
+                result[i] = prefix[right] ^ prefix[left - 1];
+            }
+        }
+
+        return result;
+    }
 
 
 
