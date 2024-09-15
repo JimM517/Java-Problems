31bd8ea8bd5adf0124b8f3753403283bedf11f1b
JimM517 <80083198+JimM517@users.noreply.github.com>
2024-09-15 09:18:09 -0400

    longest substring containing vowels

--- src/LeetCode/Medium/MediumLeetQues.java
+++ src/LeetCode/Medium/MediumLeetQues.java
@@ -3429,6 +3429,51 @@ public class MediumLeetQues {
 
 
 
+    // 1371. find the longest substring containing vowels in even counts
+    public int findTheLongestSubstring(String s) {
+
+        int n = s.length();
+        int mask = 0;
+        int maxLength = 0;
+
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(0, -1);
+
+        for (int i = 0; i < n; i++) {
+
+            char current = s.charAt(i);
+
+            if (current == 'a') {
+                mask ^= (1 << 0);
+            } else if (current == 'e') {
+                mask ^= (1 << 1);
+            } else if (current == 'i') {
+                mask ^= (1 << 2);
+            } else if (current == 'o') {
+                mask ^= (1 << 3);
+            } else if (current == 'u') {
+                mask ^= (1 << 4);
+            }
+
+            if (map.containsKey(mask)) {
+                maxLength = Math.max(maxLength, i - map.get(mask));
+            } else {
+                map.put(mask, i);
+            }
+
+
+        }
+
+        return maxLength;
+
+
+
+
+    }
+
+
+
+
 
 
 
