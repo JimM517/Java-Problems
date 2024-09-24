81f79727f0c26a970af2d6222bb990d969748713
JimM517 <80083198+JimM517@users.noreply.github.com>
2024-09-23 21:29:28 -0400

    len of longest common prefix of two arrays

--- src/LeetCode/Medium/MediumLeetQues.java
+++ src/LeetCode/Medium/MediumLeetQues.java
@@ -3631,4 +3631,47 @@ public class MediumLeetQues {
 
 
 
+
+    // 3043. find the length of the longest common prefix
+    public int longestCommonPrefix(int[] arr1, int[] arr2) {
+
+        Set<Integer> arr1Prefix = new HashSet<>();
+
+        for (int val : arr1) {
+
+            while (!arr1Prefix.contains(val) && val > 0) {
+                arr1Prefix.add(val);
+
+                val /= 10;
+            }
+        }
+
+        int longestPrefix = 0;
+
+        for (int val : arr2) {
+
+            while (!arr1Prefix.contains(val) && val > 0) {
+                val /= 10;
+            }
+            if (val > 0) {
+                longestPrefix = Math.max(longestPrefix, (int) Math.log10(val) + 1);
+            }
+
+
+        }
+
+        return longestPrefix;
+
+
+    }
+
+
+
+
+
+
+
+
+
+
 }
