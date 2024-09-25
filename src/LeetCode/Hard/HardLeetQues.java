22fbc23f8caf56f38acf46ad91081434c3f16690
JimM517 <80083198+JimM517@users.noreply.github.com>
2024-09-24 21:01:22 -0400

    sum of prefix scores of strs

--- src/LeetCode/Hard/HardLeetQues.java
+++ src/LeetCode/Hard/HardLeetQues.java
@@ -1,9 +1,6 @@
 package LeetCode.Hard;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.PriorityQueue;
+import java.util.*;
 
 public class HardLeetQues {
 
@@ -203,6 +200,64 @@ public class HardLeetQues {
         return (int) steps;
 
     }
+
+
+
+
+
+    // 2416. Sum of prefix scores of strings
+
+    class TrieNode {
+        TrieNode[] next = new TrieNode[26];
+        int count = 0;
+    }
+
+    TrieNode root = new TrieNode();
+
+    void insert(String word) {
+        TrieNode node = root;
+        for (char c : word.toCharArray()) {
+            if (node.next[c - 'a'] == null) {
+                node.next[c - 'a'] = new TrieNode();
+            }
+            node.next[c - 'a'].count++;
+            node = node.next[c - 'a'];
+        }
+    }
+
+    int count(String s) {
+        TrieNode node = root;
+        int answer = 0;
+
+        for (char c : s.toCharArray()) {
+            answer += node.next[c - 'a'].count;
+            node = node.next[c - 'a'];
+        }
+
+        return answer;
+    }
+
+    public int[] sumPrefixScores(String[] words) {
+
+        int N = words.length;
+
+        for (int i = 0; i < N; i++) {
+            insert(words[i]);
+        }
+
+        int[] scores = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            scores[i] = count(words[i]);
+        }
+
+
+        return scores;
+
+    }
+
+
+
 
 
 
