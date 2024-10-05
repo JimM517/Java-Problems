d11ff56c06fc8cd3af15b9ae9bd5a93c4970cf6c
JimM517 <80083198+JimM517@users.noreply.github.com>
2024-10-04 21:11:16 -0400

    permutation of a string

--- src/Duplicates.java
+++ src/Duplicates.java
@@ -38,4 +38,51 @@ public class Duplicates {
 
 
 
+
+    // 567. permutation in string
+    public boolean checkInclusion(String s1, String s2) {
+
+        if (s1.length() > s2.length()) {
+            return false;
+        }
+
+        int[] arrS1 = new int[26];
+        int[] arrS2 = new int[26];
+
+
+        for (int i = 0; i < s1.length(); i++) {
+            arrS1[s1.charAt(i) - 'a']++;
+            arrS2[s2.charAt(i) - 'a']++;
+        }
+
+        if (Arrays.equals(arrS1, arrS2)) {
+            return true;
+        }
+
+
+        int start = 0;
+        int end = s1.length();
+        while (end < s2.length()) {
+            arrS2[s2.charAt(start) - 'a']--;
+            arrS2[s2.charAt(end) - 'a']++;
+
+            if (Arrays.equals(arrS1, arrS2)) {
+                return true;
+            }
+            start++;
+            end++;
+
+        }
+
+        return false;
+
+    }
+
+
+
+
+
+
+
+
 }
