2af8336748a28db70e9537a9c780c8b223b50948
JimM517 <80083198+JimM517@users.noreply.github.com>
2024-10-06 09:38:55 -0400

    sentence similarity III

--- src/LeetCode/Medium/MediumContinued.java
+++ src/LeetCode/Medium/MediumContinued.java
@@ -37,5 +37,29 @@ public class MediumContinued {
 
 
 
+    // 1813. sentence similarity III
+    public boolean areSentencesSimilar(String sentence1, String sentence2) {
+
+        String[] s1 = sentence1.split(" ");
+        String[] s2 = sentence2.split(" ");
+
+        int i = 0;
+        int j = 0;
+
+        while (i < s1.length && i < s2.length && s1[i].equals(s2[i])) {
+            i++;
+        }
+
+        while (j < s1.length - i && j < s2.length - i && s1[s1.length - 1 - j].equals(s2[s2.length - 1 - j])) {
+            j++;
+        }
+
+
+        return i + j == s1.length || i + j == s2.length;
+
+    }
+
+
+
 
 }
