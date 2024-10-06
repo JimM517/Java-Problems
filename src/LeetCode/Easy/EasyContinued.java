fde90faada72be1bf13ed9d40bea557b805ff079
JimM517 <80083198+JimM517@users.noreply.github.com>
2024-10-06 10:16:53 -0400

    max product of three nums

--- src/LeetCode/Easy/EasyContinued.java
+++ src/LeetCode/Easy/EasyContinued.java
@@ -1,4 +1,25 @@
 package LeetCode.Easy;
 
+import java.util.Arrays;
+
 public class EasyContinued {
+
+    // 628. maximum product of three numbers
+    public int maximumProduct(int[] nums) {
+
+
+        Arrays.sort(nums);
+
+        int min = nums[0] * nums[1] * nums[nums.length - 1];
+        int max = nums[nums.length - 1] * nums[nums.length - 2] * nums[nums.length - 3];
+
+        return Math.max(min, max);
+
+
+    }
+
+
+
+
+
 }
