7801900b351715149efc3b4257555a0730b6c40e
JimM517 <80083198+JimM517@users.noreply.github.com>
2024-10-10 20:43:06 -0400

    number of smallest unoccupied chairs

--- src/LeetCode/Medium/MediumContinued.java
+++ src/LeetCode/Medium/MediumContinued.java
@@ -1,5 +1,6 @@
 package LeetCode.Medium;
 
+import java.util.Arrays;
 import java.util.Stack;
 
 public class MediumContinued {
@@ -163,6 +164,41 @@ public class MediumContinued {
     }
 
 
+
+
+    // 1942. the number of the smallest unoccupied chair
+    public int smallestChair(int[][] times, int targetFriend) {
+
+        int[] targetTime = times[targetFriend];
+
+        Arrays.sort(times, (a, b) -> Integer.compare(a[0], b[0]));
+
+
+        int n = times.length;
+        int[] chairTime = new int[n];
+
+        for (int[] time : times) {
+
+            for (int i = 0; i < n; i++) {
+
+                if (chairTime[i] <= time[0]) {
+                    chairTime[i] = time[1];
+                    if (Arrays.equals(time, targetTime)) {
+                        return i;
+                    }
+                    break;
+                }
+
+
+            }
+
+
+        }
+
+
+        return 0;
+
+    }
 
 
 
