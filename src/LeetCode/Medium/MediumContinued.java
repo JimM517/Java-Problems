1225135b69280636c1365f3990433f3f5510ef79
JimM517 <80083198+JimM517@users.noreply.github.com>
2024-10-16 21:10:59 -0400

    longest happy string

--- src/LeetCode/Medium/MediumContinued.java
+++ src/LeetCode/Medium/MediumContinued.java
@@ -301,5 +301,44 @@ public class MediumContinued {
 
 
 
+    // 1405. Longest Happy String
+    public String longestDiverseString(int a, int b, int c) {
+
+        StringBuilder result = new StringBuilder();
+
+        int len = a + b + c;
+
+        int A = 0;
+        int B = 0;
+        int C = 0;
+
+        for (int i = 0; i < len; i++) {
+            if ((a >= b && a >= c && A < 2) || (B == 2 && a > 0) || (C == 2 && a > 0)) {
+                result.append("a");
+                a--;
+                A++;
+                B = 0;
+                C = 0;
+            } else if ((b >= a && b >= c && B < 2) || (A == 2 && b > 0) || (C == 2 && b > 0)) {
+                result.append("b");
+                b--;
+                B++;
+                A = 0;
+                C = 0;
+
+            } else if ((c >= a && c >= b && C < 2) || (A == 2 && c > 0) || (B == 2 && c > 0)) {
+                result.append("c");
+                c--;
+                C++;
+                A = 0;
+                B = 0;
+            }
+        }
+        return result.toString();
+    }
+
+
+
+
 
 }
