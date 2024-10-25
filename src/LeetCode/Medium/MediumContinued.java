f823a885a1ed491b34bb7ae0a59caa9201e94a2c
JimM517 <80083198+JimM517@users.noreply.github.com>
2024-10-24 20:54:22 -0400

    remove subfolder from file system

--- src/LeetCode/Medium/MediumContinued.java
+++ src/LeetCode/Medium/MediumContinued.java
@@ -457,5 +457,41 @@ public class MediumContinued {
 
 
 
+    // 1233. remove sub folders from the filesystem
+    public List<String> removeSubFolders(String[] folder) {
+        Set<String> folderSet = new HashSet<>(Arrays.asList(folder));
+        List<String> result = new ArrayList<>();
+
+
+        for (String f : folder) {
+            boolean isSubFolder = false;
+            String prefix = f;
+
+            while (!prefix.isEmpty()) {
+                int pos = prefix.lastIndexOf('/');
+                if (pos == -1) {
+                    break;
+                }
+
+                prefix = prefix.substring(0, pos);
+
+
+                if (folderSet.contains(prefix)) {
+                    isSubFolder = true;
+                    break;
+                }
+            }
+
+            if (!isSubFolder) {
+                result.add(f);
+            }
+        }
+        return result;
+    }
+
+
+
+
+
 
 }
