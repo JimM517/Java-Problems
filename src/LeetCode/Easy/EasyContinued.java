ee9cf9fa9a29a6148f610108b222f1eebde56e1f
JimM517 <80083198+JimM517@users.noreply.github.com>
2024-11-02 16:38:18 -0400

    delete chars to make string fancy

--- src/LeetCode/Easy/EasyContinued.java
+++ src/LeetCode/Easy/EasyContinued.java
@@ -48,6 +48,42 @@ public class EasyContinued {
 
 
 
+    // 1957. delete characters to make a fancy string
+    public String makeFancyString(String s) {
+
+
+        StringBuilder sb = new StringBuilder();
+
+        char prev = s.charAt(0);
+        int freq = 1;
+        sb.append(s.charAt(0));
+        for (int i = 1; i < s.length(); i++) {
+            if (s.charAt(i) == prev) {
+                freq++;
+            } else {
+                prev = s.charAt(i);
+                freq = 1;
+            }
+
+            if (freq < 3) {
+                sb.append(s.charAt(i));
+            }
+        }
+
+        return sb.toString();
+    }
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 }
