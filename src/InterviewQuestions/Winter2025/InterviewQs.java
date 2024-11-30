edb52ade8a35e00195dca483e1058c23b23c1745
JimM517 <80083198+JimM517@users.noreply.github.com>
2024-11-30 14:29:13 -0500

    minimum suffix flips

--- src/InterviewQuestions/Winter2025/InterviewQs.java
+++ src/InterviewQuestions/Winter2025/InterviewQs.java
@@ -1,4 +1,42 @@
 package InterviewQuestions.Winter2025;
 
 public class InterviewQs {
+
+
+    // 1529. Minimum suffix flips
+    public int minFlips(String target) {
+
+        int max = 0;
+        char bit = '0';
+
+        for (int i = 0; i < target.length(); i++) {
+            if (target.charAt(i) != bit) {
+                max++;
+                bit = target.charAt(i);
+            }
+        }
+        return max;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
