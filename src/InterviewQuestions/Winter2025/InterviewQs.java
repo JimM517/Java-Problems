b422d498189b2f04d70333ddf0d92fba19b2eb96
JimM517 <80083198+JimM517@users.noreply.github.com>
2024-11-30 14:44:41 -0500

    minimum absolute difference

--- src/InterviewQuestions/Winter2025/InterviewQs.java
+++ src/InterviewQuestions/Winter2025/InterviewQs.java
@@ -1,6 +1,12 @@
 package InterviewQuestions.Winter2025;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class InterviewQs {
+
+    /** Mix of easy, medium and hard interview questions for 2024/25 **/
 
 
     // 1529. Minimum suffix flips
@@ -23,7 +29,29 @@ public class InterviewQs {
 
 
 
+    // 1200. Minimum absolute difference
+    public List<List<Integer>> minimumAbsDifference(int[] arr) {
 
+        List<List<Integer>> result = new ArrayList<>();
+
+        Arrays.sort(arr);
+
+        int min = Integer.MAX_VALUE;
+
+        for (int i = 0; i < arr.length - 1; i++) {
+            int difference = arr[i + 1] - arr[i];
+
+            if (difference < min) {
+                min = difference;
+
+                result.clear();
+                result.add(Arrays.asList(arr[i], arr[i + 1]));
+            } else if (difference == min) {
+                result.add(Arrays.asList(arr[i], arr[i + 1]));
+            }
+        }
+        return result;
+    }
 
 
 
