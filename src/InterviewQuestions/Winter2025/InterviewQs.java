cc16fc98e6868c252a3aa4a741b7d535044bf61d
JimM517 <80083198+JimM517@users.noreply.github.com>
2024-12-01 08:56:50 -0500

    sort ints by number of 1 bits

--- src/InterviewQuestions/Winter2025/InterviewQs.java
+++ src/InterviewQuestions/Winter2025/InterviewQs.java
@@ -79,6 +79,21 @@ public class InterviewQs {
 
 
 
+    // 1356. sort integer by number of 1 bits
+    public int[] sortByBits(int[] arr) {
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] += Integer.bitCount(arr[i]) * 10001;
+        }
+
+        Arrays.sort(arr);
+
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = arr[i] % 10001;
+        }
+        return arr;
+    }
 
 
 
