289e8e42c88efdad2979251eb604ce037be644b8
JimM517 <80083198+JimM517@users.noreply.github.com>
2024-12-05 18:54:44 -0500

    added new test file, check if word occurs a prefix of any word in sentence

--- src/LeetCode/Easy/EasyContinued.java
+++ src/LeetCode/Easy/EasyContinued.java
@@ -148,6 +148,29 @@ public class EasyContinued {
 
 
 
+    // 1455. check if a word occurs as a prefix of any word in a sentence
+    public int isPrefixOfWord(String sentence, String searchWord) {
+
+        String[] words = sentence.split(" ");
+        int index = -1;
+
+        for (int i = 0; i < words.length; i++) {
+
+
+            if (words[i].startsWith(searchWord)) {
+                index = i + 1;
+                break;
+            }
+        }
+        return index;
+    }
+
+
+
+
+
+
+
 
 
 
---
+++ src/TestArea.java
@@ -0,0 +1,20 @@
+import LeetCode.Easy.EasyContinued;
+
+public class TestArea {
+
+
+    public static void main(String[] args) {
+
+        String sentence = "i love eating burger";
+        String searchWord = "burg";
+
+        EasyContinued cont = new EasyContinued();
+
+        System.out.println(cont.isPrefixOfWord(sentence, searchWord));
+    }
+
+
+
+
+
+}
