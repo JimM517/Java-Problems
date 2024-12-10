16891ad6b624cb90443bd6ef95aa311886977477
JimM517 <80083198+JimM517@users.noreply.github.com>
2024-12-10 18:19:25 -0500

    longest special substring that occurs thrice

--- src/LeetCode/Medium/MediumContinued.java
+++ src/LeetCode/Medium/MediumContinued.java
@@ -779,4 +779,73 @@ public class MediumContinued {
 
 
 
+
+    // 2981. find longest special substring that occurs thrice I
+    public int maximumLength(String s) {
+
+        Map<String, Integer> map = new HashMap<>();
+
+        for (int start = 0; start < s.length(); start++) {
+            StringBuilder sb = new StringBuilder();
+
+            for (int end = start; end < s.length(); end++) {
+                if (sb.length() == 0 || sb.charAt(sb.length() - 1) == s.charAt(end)) {
+                    sb.append(s.charAt(end));
+                    map.put(sb.toString(), map.getOrDefault(sb.toString(), 0 ) + 1);
+                } else {
+                    break;
+                }
+            }
+        }
+
+        int answer = 0;
+        for (String str : map.keySet()) {
+            if (map.get(str) >= 3 && str.length() > answer) {
+                answer = str.length();
+            }
+        }
+        if (answer == 0) return -1;
+
+        return answer;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
