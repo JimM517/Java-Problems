71a1ea85af0354e57c96d2c6e509a96d024f5cd4
JimM517 <80083198+JimM517@users.noreply.github.com>
2024-12-15 09:27:46 -0500

    continous subarrays

--- src/LeetCode/Medium/MediumContinued.java
+++ src/LeetCode/Medium/MediumContinued.java
@@ -849,6 +849,39 @@ public class MediumContinued {
 
 
 
+    // 2762. continuous subarray
+    public long continuousSubarrays(int[] nums) {
+        int left = 0;
+        int right = 0;
+
+        long count = 0;
+
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>((a, b) -> nums[a] - nums[b]);
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>((a, b) -> nums[b] - nums[a]);
+
+
+        while (right < nums.length) {
+            minHeap.add(right);
+            maxHeap.add(right);
+
+
+            while (left < right && nums[maxHeap.peek()] - nums[minHeap.peek()] > 2) {
+                left++;
+
+                while (!maxHeap.isEmpty() && maxHeap.peek() < left) {
+                    maxHeap.poll();
+                }
+                while (!minHeap.isEmpty() && minHeap.peek() < left) {
+                    minHeap.poll();
+                }
+            }
+            count += right - left + 1;
+            right++;
+
+
+        }
+        return count;
+    }
 
 
 
