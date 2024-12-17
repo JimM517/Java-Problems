49abffcaae1bbce03dcd995be20e17819a1d022b
JimM517 <80083198+JimM517@users.noreply.github.com>
2024-12-16 20:37:57 -0500

    final array state after k multiplication operations I

--- src/LeetCode/Easy/EasyContinued.java
+++ src/LeetCode/Easy/EasyContinued.java
@@ -195,6 +195,39 @@ public class EasyContinued {
 
 
 
+    // 3264. final array state after k multiplication operations I
+    public int[] getFinalState(int[] nums, int k, int multiplier) {
+
+        PriorityQueue<int[]> heap = new PriorityQueue<>((a, b) -> {
+            if (a[0] == b[0]) {
+                return Integer.compare(a[1], b[1]);
+            }
+            return Integer.compare(a[0], b[0]);
+        });
+
+
+        for (int i = 0; i < nums.length; i++) {
+            heap.add(new int[] {nums[i], i});
+        }
+
+
+
+        for (int i = 0; i < k; i++) {
+            int[] arr = heap.poll();
+            int val = arr[0];
+            int idx = arr[1];
+
+            nums[idx] = val * multiplier;
+
+            heap.add(new int[] {nums[idx], idx});
+        }
+
+        return nums;
+
+    }
+
+
+
 
 
 
