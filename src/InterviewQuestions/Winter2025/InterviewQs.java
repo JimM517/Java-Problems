8bdd5e1ce2319417ef3d50fccbbcc75fa60a96e5
JimM517 <80083198+JimM517@users.noreply.github.com>
2024-12-21 07:32:55 -0500

    ugly number

--- src/InterviewQuestions/Winter2025/InterviewQs.java
+++ src/InterviewQuestions/Winter2025/InterviewQs.java
@@ -296,6 +296,16 @@ public class InterviewQs {
     }
 
 
+    // 263. ugly number
+    public boolean isUgly(int n) {
+        while (n >= 5 && n % 5 == 0) n /= 5;
+        while (n >= 3 && n % 3 == 0) n /= 3;
+        while (n >= 2 && n % 2 == 0) n /= 2;
+
+        return n == 1;
+    }
+
+
 
 
 
