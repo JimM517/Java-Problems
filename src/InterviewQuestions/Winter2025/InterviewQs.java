90be28cd85cdd910a29ff1b69aad3326eb044410
JimM517 <80083198+JimM517@users.noreply.github.com>
2024-12-21 07:18:29 -0500

    break palindrome

--- src/InterviewQuestions/Winter2025/InterviewQs.java
+++ src/InterviewQuestions/Winter2025/InterviewQs.java
@@ -254,5 +254,25 @@ public class InterviewQs {
 
 
 
+    // 1328. break a palindrome
+    public String breakPalindrome(String palindrome) {
+        int len = palindrome.length();
+        if (len == 1) {
+            return "";
+        }
+
+        char[] chars = palindrome.toCharArray();
+        int itrLen = (len / 2) - 1;
+        for (int i = 0; i <= itrLen; i++) {
+            if (chars[i] != 'a') {
+                chars[i] = 'a';
+                return String.valueOf(chars);
+            }
+        }
+        chars[len - 1] = 'b';
+        return String.valueOf(chars);
+    }
+
+
 
 }
