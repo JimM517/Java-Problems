3d4bb31d9112a080ebf02c87d15e468677ba6dc2
JimM517 <80083198+JimM517@users.noreply.github.com>
2024-12-22 08:21:44 -0500

    palindrome number

--- src/InterviewQuestions/Winter2025/InterviewQs.java
+++ src/InterviewQuestions/Winter2025/InterviewQs.java
@@ -386,6 +386,26 @@ public class InterviewQs {
 
 
 
+    // 9. palindrome number
+    public boolean isPalindrome(int x) {
+
+        String num = Integer.toString(x);
+
+        int start = 0;
+        int end = num.length() - 1;
+
+        while (start < end) {
+            if (num.charAt(start) != num.charAt(end)) {
+                return false;
+            }
+            start++;
+            end--;
+        }
+
+
+        return true;
+    }
+
 
 
 
