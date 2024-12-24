4f75cc9a6598c133f1e6c1caf7e01775b4792247
JimM517 <80083198+JimM517@users.noreply.github.com>
2024-12-23 19:04:49 -0500

    assign cookies

--- src/LeetCode/Dailys/DailyProblems.java
+++ src/LeetCode/Dailys/DailyProblems.java
@@ -1,8 +1,26 @@
 package LeetCode.Dailys;
 
+import java.util.Arrays;
+
 public class DailyProblems {
 
+    // 455. assign cookies
+    public int findContentChildren(int[] g, int[] s) {
 
+        Arrays.sort(g);
+        Arrays.sort(s);
+
+        int i = 0;
+
+        for (int j = 0; i < g.length && j < s.length; j++) {
+            if (g[i] <= s[j]) {
+                i++;
+            }
+        }
+        return i;
+
+
+    }
 
 
 
