212906731c924ad9dbe538a22b17a042ac76a81c
JimM517 <80083198+JimM517@users.noreply.github.com>
2024-12-30 09:57:22 -0500

    count ways to build good string

--- src/LeetCode/Dailys/DailyProblems.java
+++ src/LeetCode/Dailys/DailyProblems.java
@@ -148,6 +148,35 @@ public class DailyProblems {
 
 
 
+    // 2466. count ways to build good strings
+    public int countGoodStrings(int low, int high, int zero, int one) {
+
+      int[] dp = new int[high + 1];
+      dp[0] = 1;
+      int mod = 1000000007;
+
+      for (int end = 1; end <= high; end++) {
+          if (end >= zero) {
+              dp[end] += dp[end - zero];
+          }
+          if (end >= one) {
+              dp[end] += dp[end - one];
+          }
+          dp[end] %= mod;
+      }
+
+
+      int answer = 0;
+      for (int i = low; i <= high; i++) {
+          answer += dp[i];
+          answer %= mod;
+      }
+
+      return answer;
+
+
+
+    }
 
 
 
