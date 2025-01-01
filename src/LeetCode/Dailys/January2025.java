9528817d7a194928d6a516262d5c9d9b05adb5f3
JimM517 <80083198+JimM517@users.noreply.github.com>
2025-01-01 08:54:13 -0500

    max score after splitting a string

--- src/LeetCode/Dailys/January2025.java
+++ src/LeetCode/Dailys/January2025.java
@@ -3,7 +3,28 @@ package LeetCode.Dailys;
 public class January2025 {
 
 
+    // 1422. maximum score after splitting a string
+    public int maxScore(String s) {
 
+        int result = 0;
+
+        for (int i = 0; i < s.length() - 1; i++) {
+            int current = 0;
+            for (int j = 0; j <= i; j++) {
+                if (s.charAt(j) == '0') {
+                    current++;
+                }
+            }
+
+            for (int j = i + 1; j < s.length(); j++) {
+                if (s.charAt(j) == '1') {
+                    current++;
+                }
+            }
+            result = Math.max(result, current);
+        }
+        return result;
+    }
 
 
 
