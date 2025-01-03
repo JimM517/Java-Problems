4c1bdaf6a4673b048f2ee02e6aaf541b3e22aff0
JimM517 <80083198+JimM517@users.noreply.github.com>
2025-01-03 10:07:45 -0500

    added another solution for ways to split array

--- src/LeetCode/Dailys/January2025.java
+++ src/LeetCode/Dailys/January2025.java
@@ -63,27 +63,48 @@ public class January2025 {
 
     // 2270. number of ways to split array
     public int waysToSplitArray(int[] nums) {
-        int n = nums.length;
+//        int n = nums.length;
+//
+//        long[] prefix = new long[n];
+//        prefix[0] = nums[0];
+//
+//        for (int i = 1; i < n; i++) {
+//            prefix[i] = prefix[i - 1] + nums[i];
+//        }
+//
+//
+//        int count = 0;
+//
+//        for (int i = 0; i < n - 1; i++) {
+//            long leftSum = prefix[i];
+//            long rightSum = prefix[n - 1] - prefix[i];
+//
+//            if (leftSum >= rightSum) {
+//                count++;
+//            }
+//        }
+//        return count;
 
-        long[] prefix = new long[n];
-        prefix[0] = nums[0];
 
-        for (int i = 1; i < n; i++) {
-            prefix[i] = prefix[i - 1] + nums[i];
-        }
-
-
+        /** another solution **/
+        long leftSum = 0;
+        long rightSum = 0;
         int count = 0;
 
-        for (int i = 0; i < n - 1; i++) {
-            long leftSum = prefix[i];
-            long rightSum = prefix[n - 1] - prefix[i];
+        for (int num : nums) {
+            rightSum += num;
+        }
+
+        for (int i = 0; i < nums.length - 1; i++) {
+            leftSum += nums[i];
+            rightSum -= nums[i];
 
             if (leftSum >= rightSum) {
                 count++;
             }
         }
         return count;
+
     }
 
 
