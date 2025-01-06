4e03a6183f72ec7ca7913ee6d03f7c1a04d1e171
JimM517 <80083198+JimM517@users.noreply.github.com>
2025-01-06 18:28:23 -0500

    min number of operations to move all balls in each box

--- src/LeetCode/Dailys/January2025.java
+++ src/LeetCode/Dailys/January2025.java
@@ -182,6 +182,18 @@ public class January2025 {
 
 
 
+    // 1769. minimum number of operations to move all balls in each box
+    public int[] minOperations(String boxes) {
+        int[] answer = new int[boxes.length()];
+        for (int currentBox = 0; currentBox < boxes.length(); currentBox++) {
+            if (boxes.charAt(currentBox) == '1') {
+                for (int newPosition = 0; newPosition < boxes.length(); newPosition++) {
+                    answer[newPosition] += Math.abs(newPosition - currentBox);
+                }
+            }
+        }
+        return answer;
+    }
 
 
 
