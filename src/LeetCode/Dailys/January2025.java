fe0f9d492421e7a0e07f65c9d42ef180681a4bc2
JimM517 <80083198+JimM517@users.noreply.github.com>
2025-01-07 21:30:21 -0500

    count prefix and suffix pairs I

--- src/LeetCode/Dailys/January2025.java
+++ src/LeetCode/Dailys/January2025.java
@@ -236,6 +236,33 @@ public class January2025 {
 
 
 
+    // 3042. count prefix and suffix pairs I
+    public int countPrefixSuffixPairs(String[] words) {
+        int total = 0;
+
+        for (int i = 0; i < words.length; i++) {
+            for (int j = i + 1; j < words.length; j++) {
+                if (i == j) continue;
+
+                if (isPrefixAndSuffix(words[i], words[j])) {
+                    total++;
+                }
+
+            }
+        }
+        return total;
+    }
+
+
+    private boolean isPrefixAndSuffix(String str1, String str2) {
+
+        if (str2.startsWith(str1) && str2.endsWith(str1)) {
+            return true;
+        }
+
+        return false;
+
+    }
 
 
 
