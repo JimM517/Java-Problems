3003b6297e3bef22f5d45814ebba81bb449daf31
JimM517 <80083198+JimM517@users.noreply.github.com>
2025-01-12 09:18:55 -0500

    check if parentheses string is valid

--- src/LeetCode/Dailys/January2025.java
+++ src/LeetCode/Dailys/January2025.java
@@ -344,14 +344,53 @@ public class January2025 {
             }
         }
 
-        return totalOdds > k ? false : true;
+        return totalOdds <= k;
     }
 
 
 
 
 
+    // 2116. check if parentheses string can be valid
+    // TODO review this one
+    public boolean canBeValid(String s, String locked) {
 
+        int length = s.length();
+
+        if (length % 2 == 1) {
+            return false;
+        }
+
+        Stack<Integer> openBrackets = new Stack<>();
+        Stack<Integer> unlocked = new Stack<>();
+
+        for (int i = 0; i < length; i++) {
+            if (locked.charAt(i) == '0') {
+                unlocked.push(i);
+            } else if (s.charAt(i) == '(') {
+                openBrackets.push(i);
+            } else if (s.charAt(i) == ')') {
+                if (!openBrackets.empty()) {
+                    openBrackets.pop();
+                } else if (!unlocked.isEmpty()) {
+                    unlocked.pop();
+                } else {
+                    return false;
+                }
+            }
+
+        }
+
+        while (!openBrackets.isEmpty() && !unlocked.isEmpty() && openBrackets.peek() < unlocked.peek()) {
+            openBrackets.pop();
+            unlocked.pop();
+        }
+
+        if (!openBrackets.empty()) {
+            return false;
+        }
+        return true;
+    }
 
 
 
