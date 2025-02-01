9d907da63e285a70eb0c431d8719d4eba6e8d8dc
JimM517 <80083198+JimM517@users.noreply.github.com>
2025-02-01 16:53:52 -0500

    special array I

--- src/LeetCode/Dailys/February.java
+++ src/LeetCode/Dailys/February.java
@@ -1,4 +1,36 @@
 package LeetCode.Dailys;
 
 public class February {
+
+
+    // 3151. Special Array I
+    public boolean isArraySpecial(int[] nums) {
+
+
+        for (int i = 1; i < nums.length; i++) {
+            int curr = nums[i];
+            int prev = nums[i - 1];
+
+            if ((curr % 2 == 0) && (prev % 2 == 0)) {
+                return false;
+            }
+
+            if ((curr % 2 == 1) && (prev % 2 == 1)) {
+                return false;
+            }
+
+        }
+        return true;
+    }
+
+
+
+
+
+
+
+
+
+
+
 }
