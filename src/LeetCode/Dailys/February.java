15704134f6112cb92ac867dcd03c713788f4aea2
JimM517 <80083198+JimM517@users.noreply.github.com>
2025-02-02 08:14:51 -0500

    check if array is sorted and rotated

--- src/LeetCode/Dailys/February.java
+++ src/LeetCode/Dailys/February.java
@@ -27,6 +27,41 @@ public class February {
 
 
 
+    // 1752. check if array is sorted and rotated
+    public boolean check(int[] nums) {
+
+        int n = nums.length;
+
+        int[] checkedSorted = new int[n];
+
+        for (int rotationOffSet = 0; rotationOffSet < n; rotationOffSet++) {
+            int currInx = 0;
+            for (int index = rotationOffSet; index < n; index++) {
+                checkedSorted[currInx++] = nums[index];
+            }
+            for (int index = 0; index < rotationOffSet; index++) {
+                checkedSorted[currInx++] = nums[index];
+            }
+
+            boolean isSorted = true;
+            for (int index = 0; index < n - 1; index++) {
+                if (checkedSorted[index] > checkedSorted[index + 1]) {
+                    isSorted = false;
+                    break;
+                }
+            }
+
+            if (isSorted) {
+                return true;
+            }
+
+        }
+        return false;
+    }
+
+
+
+
 
 
 
