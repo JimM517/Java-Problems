5773831e3e5fd8eeb123e4959835885de4f5cf27
jim <jamesmagee517@gmail.com>
2025-02-24 20:34:08 -0500

    number of subarrays with odd sum

--- src/LeetCode/Dailys/February.java
+++ src/LeetCode/Dailys/February.java
@@ -668,7 +668,42 @@ public class February {
 
 
 
+    // 1524. number of subarrays with odd sum
+    public int numOfSubarrays(int[] arr) {
 
+        int MOD = (int) Math.pow(10, 9) + 7;
+        int n = arr.length;
+
+        for (int num = 0; num < n; num++) {
+            arr[num] %= 2;
+        }
+
+
+        int[] dpEven = new int[n], dpOdd = new int[n];
+
+        if (arr[n - 1] == 0) {
+            dpEven[n - 1] = 1;
+        } else {
+            dpOdd[n - 1] = 1;
+        }
+
+        for (int i = n - 2; i >= 0; i--) {
+            if (arr[i] == 1) {
+                dpOdd[i] = (1 + dpEven[i + 1]) % MOD;
+                dpEven[i] = dpOdd[i + 1];
+            } else {
+                dpEven[i] = (1 + dpEven[i + 1]) % MOD;
+                dpOdd[i] = dpOdd[i + 1];
+            }
+        }
+
+        int count = 0;
+        for (int odd : dpOdd) {
+            count += odd;
+            count %= MOD;
+        }
+        return count;
+    }
 
 
 
