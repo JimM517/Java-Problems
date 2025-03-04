2ad09fe8434508aafe8766d875c4d5e51aeaac70
jim <jamesmagee517@gmail.com>
2025-03-03 20:19:45 -0500

    check if number is a sum of powers of three

--- src/LeetCode/Dailys/March.java
+++ src/LeetCode/Dailys/March.java
@@ -96,6 +96,23 @@ public class March {
 
 
 
+    // 1780. check if number is a sum of powers of three
+    public boolean checkPowersOfThree(int n) {
+
+        while (n > 0) {
+
+            if (n % 3 == 2) {
+                return false;
+            }
+
+            n /= 3;
+        }
+
+
+        return true;
+
+    }
+
 
 
 
