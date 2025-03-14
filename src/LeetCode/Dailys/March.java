689adc5a33465788f7af738edf1bfe5741d4b7cd
jim <jamesmagee517@gmail.com>
2025-03-13 20:40:36 -0400

    max candies allocated to k children

--- src/LeetCode/Dailys/March.java
+++ src/LeetCode/Dailys/March.java
@@ -388,10 +388,48 @@ public class March {
 
 
 
+// 2226. maximum candies allocated to k children
+   public int maximumCandies(int[] candies, long k) {
+
+        int maxCandies = 0;
+
+
+        for (int i = 0; i < candies.length; i++) {
+            maxCandies = Math.max(maxCandies, candies[i]);
+        }
+
+        int left = 0;
+        int right = maxCandies;
+
+        while (left < right) {
+            int mid = (left + right + 1) / 2;
+
+            if (canAllocate(candies, k, mid)) {
+                left = mid;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+
+        return left;
+
+   }
+
+
+    private boolean canAllocate(int[] candies, long k, int numOfCandies) {
+
+        long maxNumOfChildren = 0;
+
+        for (int index = 0; index < candies.length; index++) {
+            maxNumOfChildren += candies[index] / numOfCandies;
+        }
+
+        return maxNumOfChildren >= k;
 
 
 
-
+    }
 
 
 
