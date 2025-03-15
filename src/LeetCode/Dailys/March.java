66f62bf78d79a248b510b344f2c3e6d5f300897f
jim <jamesmagee517@gmail.com>
2025-03-15 08:09:36 -0400

    house robber IV

--- src/LeetCode/Dailys/March.java
+++ src/LeetCode/Dailys/March.java
@@ -435,6 +435,38 @@ public class March {
 
 
 
+    // 2560. house robber IV
+    public int minCapability(int[] nums, int k) {
+
+        int min = 1;
+        int max = Arrays.stream(nums).max().getAsInt();
+        int total = nums.length;
+
+        while (min < max) {
+
+            int mid = (min + max) / 2;
+
+            int possible = 0;
+
+            for (int i = 0; i < total; i++) {
+                if (nums[i] <= mid) {
+                    possible++;
+                    i++;
+                }
+            }
+
+            if (possible >= k) {
+                max = mid;
+            } else {
+                min = mid + 1;
+            }
+
+        }
+
+      return min;
+    }
+
+
 
 
 
