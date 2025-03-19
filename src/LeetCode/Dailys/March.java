f80eb27938de8db870f4ad395a6dce54a6c21513
jim <jamesmagee517@gmail.com>
2025-03-18 20:35:28 -0400

    longest nice subarray

--- src/LeetCode/Dailys/March.java
+++ src/LeetCode/Dailys/March.java
@@ -539,6 +539,29 @@ public class March {
 
 
 
+    // 2401. longest nice subarray
+    public int longestNiceSubarray(int[] nums) {
+
+        int usedBits = 0;
+        int windowStart = 0;
+        int maxLen = 0;
+
+
+        for (int windowEnd = 0; windowEnd < nums.length; windowEnd++) {
+            while((usedBits & nums[windowEnd]) != 0) {
+                usedBits ^= nums[windowStart];
+                windowStart++;
+            }
+
+            usedBits |= nums[windowEnd];
+
+            maxLen = Math.max(maxLen, windowEnd - windowStart + 1);
+        }
+        return maxLen;
+
+    }
+
+
 
 
 
