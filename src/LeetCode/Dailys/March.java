6b1555d6098e9a88cb6c8a8927b6ff9043d8f15c
jim <jamesmagee517@gmail.com>
2025-03-19 20:24:45 -0400

    min operations to make array nice

--- src/LeetCode/Dailys/March.java
+++ src/LeetCode/Dailys/March.java
@@ -565,6 +565,26 @@ public class March {
 
 
 
+    // 3191. minimum operations to make binary array elements equal to one I
+    public int minOperations(int[] nums) {
+        int count = 0;
+        for (int i = 2; i < nums.length; i++) {
+            if (nums[i - 2] == 0) {
+                count++;
+                nums[i - 2] = nums[i - 2] ^ 1;
+                nums[i - 1] = nums[i - 1] ^ 1;
+                nums[i] = nums[i] ^ 1;
+            }
+        }
+        int sum = 0;
+        for (int num : nums) sum += num;
+        if (sum == nums.length) return count;
+        return -1;
+    }
+
+
+
+
 
 
 
