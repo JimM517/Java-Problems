ce4e63d1e907c3e86c2988167ef9781b47ec2889
jim <jamesmagee517@gmail.com>
2025-03-24 19:02:07 -0400

    count days without meetings

--- src/LeetCode/Dailys/March.java
+++ src/LeetCode/Dailys/March.java
@@ -661,6 +661,28 @@ public class March {
 
 
 
+    // 3169. count days without meetings
+    public int countDays(int days, int[][] meetings) {
+
+        int freeDays = 0, latestEnd = 0;
+
+        Arrays.sort(meetings, Comparator.comparingInt(a -> a[0]));
+
+        for (int[] meeting : meetings) {
+            int start = meeting[0], end = meeting[i];
+
+            if (start > latestEnd + 1) {
+                freeDays += start - latestEnd - 1;
+            }
+            latestEnd = Math.max(latestEnd, end);
+
+        }
+        freeDays += days - latestEnd;
+
+        return freeDays;
+
+    }
+
 
 
 
