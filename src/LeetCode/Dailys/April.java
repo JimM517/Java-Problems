087bef7d6f76fab38b8e846c2172191031814136
jim <jamesmagee517@gmail.com>
2025-04-01 20:56:15 -0400

    max value of an ordered triplet

--- src/LeetCode/Dailys/April.java
+++ src/LeetCode/Dailys/April.java
@@ -3,7 +3,7 @@ package LeetCode.Dailys;
 public class April {
 
 
-    // solving questions with brainpower
+    // 2140. solving questions with brainpower
     public long mostPoints(int[][] questions) {
 
         int n = questions.length;
@@ -28,6 +28,23 @@ public class April {
 
 
 
+    // 2873. maximum value of an ordered triplet
+    public long maximumTripletValue(int[] nums) {
+
+        int n = nums.length;
+        long maximumVal = 0;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                for (int k = j + 1; k < n; k++) {
+                    long current = (long) (nums[i] - nums[j]) * nums[k];
+                    maximumVal = Math.max(maximumVal, current);
+                }
+            }
+        }
+        return maximumVal;
+
+    }
 
 
 
