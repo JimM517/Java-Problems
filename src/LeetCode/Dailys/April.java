897a79b61f6e88a8ee033e97333d03da279eb4d4
jim <jamesmagee517@gmail.com>
2025-04-02 20:24:01 -0400

    max value of a triplet II

--- src/LeetCode/Dailys/April.java
+++ src/LeetCode/Dailys/April.java
@@ -48,6 +48,30 @@ public class April {
 
 
 
+    // 2874. maximum value of an ordered triplet II
+    public long maximumTripletValueTwo(int[] nums) {
+
+        int n = nums.length;
+        int[] prefix = new int[n];
+        int[] suffix = new int[n];
+
+        long maximumVal = 0;
+
+
+        for (int i = 1; i < nums.length; i++) {
+            prefix[i] = Math.max(prefix[i - 1], nums[i - 1]);
+            suffix[n - 1 - i] = Math.max(suffix[n - i], nums[n - i]);
+        }
+
+        for (int j = 1; j < n; j++) {
+            maximumVal = Math.max(maximumVal, (long) (prefix[j] - nums[j]) * suffix[j]);
+        }
+
+
+        return maximumVal;
+
+    }
+
 
 
 
