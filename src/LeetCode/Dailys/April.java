d94020abba9c2a3b04125e27927428b13d8bc7b8
jim <jamesmagee517@gmail.com>
2025-04-14 17:45:38 -0400

    count good triplets

--- src/LeetCode/Dailys/April.java
+++ src/LeetCode/Dailys/April.java
@@ -487,8 +487,35 @@ public class April {
 
 
 
+    // 1534. count good triplets
+    public int countGoodTriplets(int[] arr, int a, int b, int c) {
+
+        int count = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                for (int k = j + 1; k < arr.length; k++) {
+
+                    if (Math.abs(arr[i] - arr[j]) <= a) {
+                        if (Math.abs(arr[j] - arr[k]) <= b) {
+                            if (Math.abs(arr[i] - arr[k]) <= c) {
+                                count++;
+                            }
+                        }
+                    }
 
 
+                }
+            }
+        }
+
+
+        return count;
+
+
+
+
+    }
 
 
 
