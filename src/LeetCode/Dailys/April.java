41a026e0048cfc887af04d7812e26d6b8c59d7c1
jim <jamesmagee517@gmail.com>
2025-04-15 20:23:18 -0400

    count number of good subarrays

--- src/LeetCode/Dailys/April.java
+++ src/LeetCode/Dailys/April.java
@@ -576,6 +576,30 @@ public class April {
 
 
 
+    // 2537. count the number of good subarrays
+    public long countGood(int[] nums, int k) {
+
+        int n = nums.length;
+        int same = 0;
+        int right = -1;
+
+        Map<Integer, Integer> count = new HashMap<>();
+        long answer = 0;
+
+        for (int left = 0; left < n; left++) {
+            while (same < k && right + 1 < n) {
+                right++;
+                same += count.getOrDefault(nums[right], 0);
+                count.put(nums[right], count.getOrDefault(nums[right], 0) + 1);
+            }
+            if (same >= k) {
+                answer += n - right;
+            }
+            count.put(nums[left], count.get(nums[left]) - 1);
+            same -= count.get(nums[left]);
+        }
+        return answer;
+    }
 
 
 
