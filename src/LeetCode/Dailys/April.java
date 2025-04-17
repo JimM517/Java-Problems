e9e687afcd88b5304083f3b83b91029183efac17
jim <jamesmagee517@gmail.com>
2025-04-16 20:21:21 -0400

    count equal and divisible pairs

--- src/LeetCode/Dailys/April.java
+++ src/LeetCode/Dailys/April.java
@@ -603,7 +603,22 @@ public class April {
 
 
 
+    // 2176. count equal and divisible pairs in an array
+    public int countPairs(int[] nums, int k) {
 
+        int count = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] == nums[j] && (i * j) % k == 0) {
+                    count++;
+                }
+            }
+        }
+
+        return count;
+
+    }
 
 
 
