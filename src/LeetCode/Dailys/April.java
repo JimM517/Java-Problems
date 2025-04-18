0c77174a6582c1f42d0d6bbabc20e6f9c3db41d2
jim <jamesmagee517@gmail.com>
2025-04-17 20:24:06 -0400

    count and say

--- src/LeetCode/Dailys/April.java
+++ src/LeetCode/Dailys/April.java
@@ -624,7 +624,29 @@ public class April {
 
 
 
+    // 38. count and say
+    public String countAndSay(int n) {
 
+        if (n == 1) {
+            return "1";
+        }
+
+        String s = countAndSay(n - 1);
+
+        int c = 0;
+        StringBuilder answer = new StringBuilder();
+
+        for (int i = 0; i < s.length(); i++) {
+            c++;
+            if (i == s.length() - 1 || s.charAt(i) != s.charAt(i + 1)) {
+                answer.append(c).append(s.charAt(i));
+                c = 0;
+            }
+        }
+
+        return answer.toString();
+
+    }
 
 
 
