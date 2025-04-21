26627f0f47896e5ff8834e407b86c74ffd96c733
jim <jamesmagee517@gmail.com>
2025-04-21 18:14:54 -0400

    count the hidden sequence

--- src/LeetCode/Dailys/April.java
+++ src/LeetCode/Dailys/April.java
@@ -702,7 +702,25 @@ public class April {
 
 
 
+    // 2145. count the hidden sequences
+    public int numberOfArrays(int[] differences, int lower, int upper) {
 
+        int x = 0;
+        int y = 0;
+        int curr = 0;
+
+        for (int d : differences) {
+            curr += d;
+
+            x = Math.min(x, curr);
+            y = Math.max(y, curr);
+            if (y - x > upper - lower) {
+                return 0;
+            }
+        }
+
+        return (upper - lower) - (y - x) + 1;
+    }
 
 
 
