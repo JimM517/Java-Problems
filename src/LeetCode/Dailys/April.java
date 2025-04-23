bf241ce41f9b80a4d512c0e72f8268da5c9fea26
jim <jamesmagee517@gmail.com>
2025-04-22 20:19:14 -0400

    count largest group

--- src/LeetCode/Dailys/April.java
+++ src/LeetCode/Dailys/April.java
@@ -796,6 +796,44 @@ public class April {
 
 
 
+    // count largest group
+    public int countLargestGroup(int n) {
+
+        Map<Integer, Integer> map = new HashMap<>();
+        int maxValue = 0;
+        for (int i = 1; i <= n; i++) {
+            int key = 0;
+            int x = i;
+            while (x != 0) {
+                key += x % 10;
+                x /= 10;
+            }
+            map.put(key, map.getOrDefault(key, 0) + 1);
+            maxValue = Math.max(maxValue, map.get(key));
+        }
+
+        int count = 0;
+        for (Map.Entry<Integer, Integer> curr : map.entrySet()) {
+            if (curr.getValue() == maxValue) {
+                count++;
+            }
+        }
+
+
+        return count;
+
+
+    }
+
+
+
+
+
+
+
+
+
+
 
 
 
