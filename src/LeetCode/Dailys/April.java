feb6576ae63b3a9765d1bdd94042c9e32a944ce3
jim <jamesmagee517@gmail.com>
2025-04-28 19:03:36 -0400

    count subarrays with score less than k

--- src/LeetCode/Dailys/April.java
+++ src/LeetCode/Dailys/April.java
@@ -917,7 +917,23 @@ public class April {
 
 
 
+    // 2302. count subarrays with score less than k
+    public long countSubarrays(int[] nums, long k) {
 
+        int n = nums.length;
+        long result = 0;
+        long total = 0;
+
+        for (int i = 0, j = 0; j < n; j++) {
+            total += nums[j];
+            while (i <= j && total * (j - i + 1) >= k) {
+                total -= nums[i];
+                i++;
+            }
+            result += j - i + 1;
+        }
+        return result;
+    }
 
 
 
