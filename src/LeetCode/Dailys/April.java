ccf6145a430952608746b997c241fbfd27a9c3a9
jim <jamesmagee517@gmail.com>
2025-04-29 22:05:34 -0400

    find numbers with even digits

--- src/LeetCode/Dailys/April.java
+++ src/LeetCode/Dailys/April.java
@@ -968,7 +968,21 @@ public class April {
 
 
 
+    // 1295. find numbers with even number of digits
+    public int findNumbers(int[] nums) {
 
+        int result = 0;
+
+        for (int num : nums) {
+
+            String digits = Integer.toString(num);
+
+            if (digits.length() % 2 == 0) {
+                result++;
+            }
+        }
+        return result;
+    }
 
 
 
