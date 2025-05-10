c24a0f71cb518b5157f51e6ba27f205a6122c4ea
jim <jamesmagee517@gmail.com>
2025-05-10 09:12:37 -0400

    minimum equal sum of two arrays after replacing zeroes

--- src/LeetCode/Dailys/May.java
+++ src/LeetCode/Dailys/May.java
@@ -371,6 +371,44 @@ public class May {
 
 
 
+        // 2918. minimum equal sum of two arrays after replacing zeros
+        public long minSum(int[] nums1, int[] nums2) {
+
+          long sum1 = 0;
+          long sum2 = 0;
+          int numZeros1 = 0;
+          int numZeroes2 = 0;
+
+            for (int i = 0; i < nums1.length; i++) {
+
+                sum1 += nums1[i];
+
+                if (nums1[i] == 0) {
+                    sum1++;
+                    numZeros1++;
+                }
+
+            }
+
+            for (int k = 0; k < nums2.length; k++) {
+
+
+                sum2 += nums2[k];
+
+                if (nums2[k] == 0) {
+                    sum2++;
+                    numZeroes2++;
+                }
+
+            }
+
+            if ((numZeros1 == 0 && sum2 > sum1) || (numZeroes2 == 0 && sum1 > sum2)) {
+                return -1;
+            }
+
+
+            return Math.max(sum1, sum2);
+        }
 
 
 
