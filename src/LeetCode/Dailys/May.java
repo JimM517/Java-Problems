e64ac071794357d7a80030c7bbb653e21c3d87b8
jim <jamesmagee517@gmail.com>
2025-05-14 20:07:37 -0400

    longest unequal adjacent groups subsequence I

--- src/LeetCode/Dailys/May.java
+++ src/LeetCode/Dailys/May.java
@@ -581,6 +581,58 @@ public class May {
 
 
 
+        // 2900. longest unequal adjacent groups subsequence I
+    public List<String> getLongestSubsequence(String[] words, int[] groups) {
+
+        List<String> answer = new ArrayList<>();
+
+        int n = words.length;
+
+        for (int i = 0; i < n; i++) {
+            if (i == 0 || groups[i] != groups[i - 1]) {
+                answer.add(words[i]);
+            }
+        }
+            return answer;
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
