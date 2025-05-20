0e33e22de117a751e18608d178c5ca1151b567f9
jim <jamesmagee517@gmail.com>
2025-05-19 20:34:32 -0400

    fixed error

--- src/LeetCode/Dailys/May.java
+++ src/LeetCode/Dailys/May.java
@@ -785,37 +785,26 @@ public class May {
 
     // 3355. Zero Array Transformation I
     public boolean isZeroArray(int[] nums, int[][] queries) {
-
-            int[] delta = new int[nums.length + 1];
-
-            for (int[] query : queries) {
-
-                int left = query[0];
-                int right = query[1];
-
-                delta[left] += 1;
-                delta[right + 1] -= 1;
-
-
-            }
-
-            int[] operationCounts = new int[delta.length];
-            int current = 0;
-
-            for (int i = 0; i < delta.length; i++) {
-                current += delta[i];
-                operationCounts[i] -= current;
-
-            }
-
-            for (int i = 0; i < nums.length; i++) {
-                if (operationCounts[i] < nums[i]) {
-                    return false;
-                }
-            }
-
-            return true;
+        int[] deltaArray = new int[nums.length + 1];
+        for (int[] query : queries) {
+            int left = query[0];
+            int right = query[1];
+            deltaArray[left] += 1;
+            deltaArray[right + 1] -= 1;
         }
+        int[] operationCounts = new int[deltaArray.length];
+        int currentOperations = 0;
+        for (int i = 0; i < deltaArray.length; i++) {
+            currentOperations += deltaArray[i];
+            operationCounts[i] = currentOperations;
+        }
+        for (int i = 0; i < nums.length; i++) {
+            if (operationCounts[i] < nums[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
 
 
 
