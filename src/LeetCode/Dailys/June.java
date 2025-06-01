20bf180299f4d7ad751e02dbd0ddf5aba1f6f11e
jim <jamesmagee517@gmail.com>
2025-06-01 10:41:45 -0400

    distribute candies among children II

--- src/LeetCode/Dailys/June.java
+++ src/LeetCode/Dailys/June.java
@@ -7,19 +7,16 @@ public class June {
 // 2929. distribute candies among children II
     public long distributeCandies(int n, int limit) {
 
+            long result = 0;
 
+            for (int i = 0; i <= Math.min(limit, n); i++) {
+                if (n - i > 2 * limit) {
+                    continue;
+                }
+                result += Math.max(Math.min(limit, n - i) - Math.max(0, n - i - limit) + 1, 0);
 
-
-
-
-
-
-
-
-
-
-
-
+            }
+            return result;
     }
 
 
