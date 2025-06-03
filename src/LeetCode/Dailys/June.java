0f219f2be7dc289cab56ea7041e78ce278f21b2e
jim <jamesmagee517@gmail.com>
2025-06-02 20:04:40 -0400

    maximum candies you can get from boxes

--- src/LeetCode/Dailys/June.java
+++ src/LeetCode/Dailys/June.java
@@ -1,5 +1,8 @@
 package LeetCode.Dailys;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class June {
 
 
@@ -39,6 +42,63 @@ public class June {
             return cnt + candies[n - 1];
         }
 
+
+
+
+
+
+
+
+        // 1298. maximum candies you can get from boxes
+        public int maxCandies(
+                int[] status,
+                int[] candies,
+                int[][] keys,
+                int[][] containedBoxes,
+                int[] initialBoxes
+        ) {
+            int n = status.length;
+            boolean[] canOpen = new boolean[n];
+            boolean[] hasBox = new boolean[n];
+            boolean[] used = new boolean[n];
+
+            for (int i = 0; i < n; ++i) {
+                canOpen[i] = (status[i] == 1);
+            }
+
+            Queue<Integer> q = new LinkedList<>();
+            int ans = 0;
+            for (int box : initialBoxes) {
+                hasBox[box] = true;
+                if (canOpen[box]) {
+                    q.offer(box);
+                    used[box] = true;
+                    ans += candies[box];
+                }
+            }
+
+            while (!q.isEmpty()) {
+                int bigBox = q.poll();
+                for (int key : keys[bigBox]) {
+                    canOpen[key] = true;
+                    if (!used[key] && hasBox[key]) {
+                        q.offer(key);
+                        used[key] = true;
+                        ans += candies[key];
+                    }
+                }
+                for (int box : containedBoxes[bigBox]) {
+                    hasBox[box] = true;
+                    if (!used[box] && canOpen[box]) {
+                        q.offer(box);
+                        used[box] = true;
+                        ans += candies[box];
+                    }
+                }
+            }
+
+            return ans;
+        }
 
 
 
