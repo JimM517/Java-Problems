96dc60a4607af4b8f8990f296b38add71da1b1c6
jim <jamesmagee517@gmail.com>
2025-06-04 20:15:39 -0400

    lexicographically smallest equivalent string

--- src/LeetCode/Dailys/June.java
+++ src/LeetCode/Dailys/June.java
@@ -127,6 +127,57 @@ public class June {
 
 
 
+    // 1061. Lexicographically smallest equivalent string
+    private int[] parent = new int[26];
+
+    // find with path compression
+    private int find(int x) {
+        if (parent[x] != x) {
+            parent[x] = find(parent[x]);
+        }
+        return parent[x];
+    }
+
+    // union by choosing the lexicographically smaller root as the new parent
+    private void union(int a, int b) {
+        int ra = find(a);
+        int rb = find(b);
+        if (ra == rb) {
+            return;
+        }
+        if (ra < rb) {
+            parent[rb] = ra;
+        } else {
+            parent[ra] = rb;
+        }
+    }
+
+    public String smallestEquivalentString(String s1, String s2, String baseStr) {
+        // union find
+        for (int i = 0; i < 26; i++) {
+            parent[i] = i;
+        }
+
+        // merge equivalent characters
+        for (int i = 0; i < s1.length(); i++) {
+            int x = s1.charAt(i) - 'a';
+            int y = s2.charAt(i) - 'a';
+            union(x, y);
+        }
+
+        // build result by mapping each character in baseStr to its group root
+        StringBuilder sb = new StringBuilder();
+        for (char c : baseStr.toCharArray()) {
+            int idx = c - 'a';
+            int root = find(idx);
+            sb.append((char) (root + 'a'));
+        }
+
+        return sb.toString();
+    }
+
+
+
 
 
 
