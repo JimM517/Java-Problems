116aae5ee06ef79b546936c9a0b83cbad3792127
jim <jamesmagee517@gmail.com>
2025-06-05 20:29:36 -0400

    using a robot to print the lexicographically smallest string

--- src/LeetCode/Dailys/June.java
+++ src/LeetCode/Dailys/June.java
@@ -2,6 +2,7 @@ package LeetCode.Dailys;
 
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.Stack;
 
 public class June {
 
@@ -179,6 +180,31 @@ public class June {
 
 
 
+    // 2434. using a robot to print the lexicographically smallest string
+    public String robotWithString(String s) {
+
+        int[] cnt = new int[26];
+        for (char c : s.toCharArray()) {
+            cnt[c - 'a']++;
+        }
+
+        Stack<Character> stack = new Stack<>();
+        StringBuilder res = new StringBuilder();
+        char minCharacter = 'a';
+        for (char c : s.toCharArray()) {
+            stack.push(c);
+            cnt[c - 'a']--;
+            while (minCharacter != 'z' && cnt[minCharacter - 'a'] == 0) {
+                minCharacter++;
+            }
+            while (!stack.isEmpty() && stack.peek() <= minCharacter) {
+                res.append(stack.pop());
+            }
+        }
+
+        return res.toString();
+
+    }
 
 
 
