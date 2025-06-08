82319bffdb77abc730e49c64a9763836f6fda644
jim <jamesmagee517@gmail.com>
2025-06-08 11:41:13 -0400

    lexicographical numbers

--- src/LeetCode/Dailys/June.java
+++ src/LeetCode/Dailys/June.java
@@ -244,7 +244,30 @@ public class June {
 
 
 
+    // 386. lexicographical numbers
+    public List<Integer> lexicalOrder(int n) {
 
+        List<Integer> result = new ArrayList<>();
+
+        int curr = 1;
+
+        for (int i = 0; i < n; i++) {
+
+            result.add(curr);
+            if (curr * 10 <= n){
+                curr *= 10;
+            } else {
+                while (curr % 10 == 9 || curr + 1 > n) {
+                    curr /= 10;
+                }
+                curr++;
+            }
+
+
+        }
+        return result;
+
+    }
 
 
 
