835e76e3724e42b6db82455acb4e5e8e7b3a91d5
jim <jamesmagee517@gmail.com>
2025-06-14 20:28:49 -0400

    max difference by remapping a digit

--- src/LeetCode/Dailys/June.java
+++ src/LeetCode/Dailys/June.java
@@ -456,7 +456,26 @@ public class June {
 
 
 
+    // 2566. maximum difference by remapping a digit
+    public int minMaxDifference(int num) {
 
+        String s = Integer.toString(num);
+        String t = s;
+        int pos = 0;
+        while (pos < s.length() && s.charAt(pos) == '9') {
+            pos++;
+        }
+        if (pos < s.length()) {
+            s = s.replace(s.charAt(pos), '9');
+        }
+        t = t.replace(t.charAt(0), '0');
+        return Integer.parseInt(s) - Integer.parseInt(t);
+
+
+
+
+
+    }
 
 
 
