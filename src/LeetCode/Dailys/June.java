3dc4516dd0fcea009ec8e9a47920fd0ab0681820
jim <jamesmagee517@gmail.com>
2025-06-19 21:08:36 -0400

    max manhattan distance after k changes

--- src/LeetCode/Dailys/June.java
+++ src/LeetCode/Dailys/June.java
@@ -628,7 +628,40 @@ public class June {
 
 
 
+    // 3443 maximum manhattan distance after k changes
+    public int maxDistance(String s, int k) {
 
+          int latitude = 0;
+          int longitude = 0;
+          int answer = 0;
+
+          int n = s.length();
+
+          for (int i = 0; i < n; i++) {
+              char c = s.charAt(i);
+              switch (c) {
+                  case 'N':
+                      latitude++;
+                      break;
+                  case 'S':
+                      latitude--;
+                      break;
+                  case 'E':
+                      longitude++;
+                      break;
+                  case 'W':
+                      longitude--;
+                      break;
+              }
+              answer = Math.max(answer,    Math.min(
+                      Math.abs(latitude) + Math.abs(longitude) + k * 2,
+                      i + 1
+              ));
+          }
+
+
+            return answer;
+    }
 
 
 
