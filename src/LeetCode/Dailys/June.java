9989685476b5720fdbf37ce120d026cf26db17ce
jim <jamesmagee517@gmail.com>
2025-06-24 21:52:09 -0400

    kth smallest product of two sorted arrays

--- src/LeetCode/Dailys/June.java
+++ src/LeetCode/Dailys/June.java
@@ -798,8 +798,52 @@ public class June {
 
 
 
+        // 2040. Kth smallest product of two sorted arrays
+    public long kthSmallestProduct(int[] nums1, int[] nums2, int k) {
+        long left = -10000000000L;
+        long right = 10000000000L;
+
+        while (left < right) {
+            long mid = left + (right - left) / 2;
+            if (countProducts(nums1, nums2, mid) < k) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+        return left;
+    }
 
 
+    private long countProducts(int[] nums1, int[] nums2, long target) {
+            long count = 0;
+            for (int num : nums1) {
+                if (num == 0) {
+                    if (target >= 0) count += nums2.length;
+                    continue;
+                }
+
+
+
+                int low = 0, high = nums2.length;
+                while (low < high) {
+                    int mid = low + (high - low) / 2;
+                    long product = (long) num * nums2[mid];
+                    if (product <= target) {
+                        if (num > 0) low = mid + 1;
+                        else high = mid;
+                    } else {
+                        if (num > 0) high = mid;
+                        else low = mid + 1;
+                    }
+                }
+                count += (num > 0) ? low : nums2.length - high;
+            }
+
+
+            return count;
+
+    }
 
 
 
