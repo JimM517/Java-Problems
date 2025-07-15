ce2cfb27df4c3dba186c5f35d2a8eb5be66afe72
jim <jamesmagee517@gmail.com>
2025-07-14 20:39:27 -0400

    maximum matching players with trainers

--- src/LeetCode/Dailys/July.java
+++ src/LeetCode/Dailys/July.java
@@ -444,7 +444,32 @@ public class July {
 
 
 
+        // 2410. maximum matching players with trainers
+    public int matchPlayersAndTrainers(int[] players, int[] trainers) {
 
+
+        Arrays.sort(players);
+        Arrays.sort(trainers);
+
+        int m = players.length;
+        int n = trainers.length;
+
+        int count = 0;
+        int i = 0;
+        int j = 0;
+
+        while (i < m && j < n) {
+            if (players[i] <= trainers[j]) {
+                count++;
+                i++;
+                j++;
+            } else {
+                j++;
+            }
+        }
+            return count;
+
+        }
 
 
 
