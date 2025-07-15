3f6419be1f758fec15327a61864efbebf803b068
jim <jamesmagee517@gmail.com>
2025-07-14 21:08:27 -0400

    valid word

--- src/LeetCode/Dailys/July.java
+++ src/LeetCode/Dailys/July.java
@@ -472,12 +472,74 @@ public class July {
         }
 
 
+        public class ListNode {
+            int val;
+            ListNode next;
+            ListNode() {};
+            ListNode(int val) {
+                this.val = val;
+            }
+            ListNode(int val, ListNode next) {
+                this.val = val;
+                this.next = next;
+            }
+        }
+
+
+
+        // 1290. convert binary number in a linked list to integer
+    public int getDecimalValue(ListNode head) {
+
+
+        ListNode dummy = head;
+        int result = 0;
+
+        while (dummy != null) {
+
+
+           result = result * 2 + dummy.val;
+           dummy = dummy.next;
+
+
+        }
+
+        return result;
+
+    }
 
 
 
 
+    // 3136. valid word
+    public boolean isValid(String word) {
+
+        if (word.length() < 3) {
+            return false;
+        }
+        boolean hasVowel = false;
+        boolean hasConsonant = false;
+        for (char c : word.toCharArray()) {
+            if (Character.isLetter(c)) {
+                char ch = Character.toLowerCase(c);
+                if (
+                        ch == 'a' ||
+                                ch == 'e' ||
+                                ch == 'i' ||
+                                ch == 'o' ||
+                                ch == 'u'
+                ) {
+                    hasVowel = true;
+                } else {
+                    hasConsonant = true;
+                }
+            } else if (!Character.isDigit(c)) {
+                return false;
+            }
+        }
+        return hasVowel && hasConsonant;
 
 
+    }
 
 
 
