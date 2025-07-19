740c4849be5870ae2b722e9da1ecf681436c8863
jim <jamesmagee517@gmail.com>
2025-07-19 09:24:01 -0400

    minimum difference in sums after removal of elements

--- src/LeetCode/Dailys/July.java
+++ src/LeetCode/Dailys/July.java
@@ -568,8 +568,64 @@ public class July {
 
 
 
+    // 3202. find the maximum length of a valid subsequence II
+    public int maximumLengthTwo(int[] nums, int k) {
+
+        int[][] dp = new int[k][k];
+        int result = 0;
+
+        for (int num : nums) {
+            num %= k;
+            for (int prev = 0; prev < k; prev++) {
+                dp[prev][num] = dp[num][prev] + 1;
+                result = Math.max(result, dp[prev][num]);
+            }
+        }
 
 
+            return result;
+
+
+    }
+
+
+
+    // 2163. minimum difference in sums after removal of element
+    public long minimumDifference(int[] nums) {
+            int n3 = nums.length;
+            int n = n3 / 3;
+            long[] part1 = new long[n + 1];
+            long sum = 0;
+            // max heap (simulate with opposite numbers)
+            PriorityQueue<Integer> ql = new PriorityQueue<>((a, b) -> b - a);
+            for (int i = 0; i < n; ++i) {
+                sum += nums[i];
+                ql.add(nums[i]);
+            }
+            part1[0] = sum;
+            for (int i = n; i < n * 2; ++i) {
+                sum += nums[i];
+                ql.add(nums[i]);
+                sum -= ql.poll();
+                part1[i - (n - 1)] = sum;
+            }
+
+            long part2 = 0;
+            // min heap
+            PriorityQueue<Integer> qr = new PriorityQueue<>();
+            for (int i = n * 3 - 1; i >= n * 2; --i) {
+                part2 += nums[i];
+                qr.add(nums[i]);
+            }
+            long ans = part1[n] - part2;
+            for (int i = n * 2 - 1; i >= n; --i) {
+                part2 += nums[i];
+                qr.add(nums[i]);
+                part2 -= qr.poll();
+                ans = Math.min(ans, part1[i - n] - part2);
+            }
+            return ans;
+        }
 
 
 
