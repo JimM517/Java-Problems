52047228f7a7b542bead6e8f3f95a4444896ce55
jim <jamesmagee517@gmail.com>
2025-08-04 21:19:36 -0400

    fruits into basekts II

--- src/LeetCode/Dailys/August.java
+++ src/LeetCode/Dailys/August.java
@@ -144,6 +144,29 @@ public class August {
 
 
 
+    // 3477. fruits into baskets II
+    public int numOfUnplacedFruits(int[] fruits, int[] baskets) {
+        boolean[] used = new boolean[baskets.length];
+        int unplaced = 0;
+
+        for (int i = 0; i < fruits.length; i++) {
+            boolean placed = false;
+            for (int j = 0; j < baskets.length; j++) {
+                if (!used[j] && baskets[j] >= fruits[i]) {
+                    used[j] = true;
+                    placed = true;
+                    break;
+                }
+            }
+            if (!placed) {
+                unplaced++;
+            }
+        }
+        return unplaced;
+    }
+
+
+
 
 
 
