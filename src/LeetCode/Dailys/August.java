278e8fcc82cc3a84eb2d94ec1128c90e1590ba55
jim <jamesmagee517@gmail.com>
2025-08-09 10:48:55 -0400

    power of two

--- src/LeetCode/Dailys/August.java
+++ src/LeetCode/Dailys/August.java
@@ -333,6 +333,23 @@ public class August {
 
 
 
+    // 231. power of two
+    public boolean isPowerOfTwo(int n) {
+
+            if (n <= 0) {
+                return false;
+            }
+
+        while (n > 1) {
+            if (n % 2 == 1) {
+                return false;
+            }
+            n /= 2;
+        }
+        return true;
+
+    }
+
 
 
 
