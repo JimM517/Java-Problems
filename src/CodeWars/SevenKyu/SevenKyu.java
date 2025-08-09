ba8ba1139009ff63bbc3fc265fd9b2ee1c12cd00
jim <jamesmagee517@gmail.com>
2025-08-09 17:00:56 -0400

    mumbling

--- src/CodeWars/SevenKyu/SevenKyu.java
+++ src/CodeWars/SevenKyu/SevenKyu.java
@@ -307,7 +307,32 @@ public class SevenKyu {
 
 
 
+    // mumbling
+    public static String accum(String s) {
 
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < s.length(); i++) {
+
+            sb.append(Character.toUpperCase(s.charAt(i)));
+
+            for (int j = 0; j < i; j++) {
+                sb.append(Character.toLowerCase(s.charAt(i)));
+            }
+
+            if (i < s.length() - 1) {
+                sb.append("-");
+            }
+
+        }
+
+
+
+
+        return sb.toString();
+
+
+    }
 
 
 
