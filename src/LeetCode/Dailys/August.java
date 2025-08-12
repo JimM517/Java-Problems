873fc5a5c1886418c3f7f640a9cca61b118d53da
jim <jamesmagee517@gmail.com>
2025-08-11 21:48:51 -0400

    ways to express an integer as sum of powers

--- src/LeetCode/Dailys/August.java
+++ src/LeetCode/Dailys/August.java
@@ -421,6 +421,27 @@ public class August {
 
 
 
+    // 2787. ways to express an integer as sum of powers
+    public int numberOfWays(int n, int x) {
+
+        int[][] dp = new int[n + 1][n + 1];
+        dp[0][0] = 1;
+        for (int i = 1; i <= n; i++) {
+            long val = (long) Math.pow(i, x);
+            for (int j = 0; j <= n; j++) {
+                dp[i][j] = dp[i - 1][j];
+                if (j >= val) {
+                    dp[i][j] = (dp[i][j] + dp[i - 1][j - (int) val]) % MOD;
+                }
+            }
+        }
+
+        return (int) dp[n][n];
+
+    }
+
+
+
 
 
 
