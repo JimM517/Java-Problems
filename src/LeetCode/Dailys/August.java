2e5ac8336632f52c0a8e060d97e531ed5f6259f7
jim <jamesmagee517@gmail.com>
2025-08-11 21:38:33 -0400

    range product queries of powers

--- src/LeetCode/Dailys/August.java
+++ src/LeetCode/Dailys/August.java
@@ -386,6 +386,40 @@ public class August {
 
 
 
+    // 2438. range product queries of powers
+    private static final int MOD = 1000000007;
+    public int[] productQueries(int n, int[][] queries) {
+
+            List<Integer> bins = new ArrayList<>();
+            int rep = 1;
+
+            while (n > 0) {
+                if (n % 2 == 1) {
+                    bins.add(rep);
+                }
+                n /= 2;
+                rep *= 2;
+            }
+
+            int m = bins.size();
+            int[][] results = new int[m][m];
+            for (int i = 0; i < m; i++) {
+                long cur = 1;
+                for (int j = i; j < m; j++) {
+                    cur = (cur * bins.get(j)) % MOD;
+                    results[i][j] = (int) cur;
+                }
+            }
+
+            int[] answer = new int[queries.length];
+            for (int i = 0; i < queries.length; i++) {
+                answer[i] = results[queries[i][0]][queries[i][1]];
+            }
+            return answer;
+    }
+
+
+
 
 
 
