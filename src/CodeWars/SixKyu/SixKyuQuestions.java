f761052e58b17aae2b67f615c2d7004a9681a869
jim <jamesmagee517@gmail.com>
2025-08-12 22:14:34 -0400

    highest scoring word

--- src/CodeWars/SixKyu/SixKyuQuestions.java
+++ src/CodeWars/SixKyu/SixKyuQuestions.java
@@ -84,6 +84,32 @@ public class SixKyuQuestions {
 
 
 
+    // highest scoring word
+    public static String high(String s) {
+
+        String[] words = s.split(" ");
+        int maxScore = 0;
+        String maxWord = "";
+
+        for (String word : words) {
+            int score = 0;
+            for (char c : word.toCharArray()) {
+                score += c - 'a' + 1;
+            }
+            if (score > maxScore) {
+                maxScore = score;
+                maxWord = word;
+            }
+        }
+
+
+        return maxWord;
+
+    }
+
+
+
+
 
 
 
