98b910b10f855811b821c26046d3cf37ae4d01a6
jim <jamesmagee517@gmail.com>
2025-08-14 19:35:38 -0400

    square n sum

--- src/CodeWars/EightKyu/EightKyu.java
+++ src/CodeWars/EightKyu/EightKyu.java
@@ -228,6 +228,44 @@ public class EightKyu {
 
 
 
+    // square (n) sum
+    public static int squareSum(int[] n) {
+
+        int total = 0;
+
+        for (int num : n) {
+            total += Math.pow(num, 2);
+        }
+
+        return total;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
