068fd2f6d33cea62c88f1791fa98aa740431a5b7
jim <jamesmagee517@gmail.com>
2025-08-16 10:54:38 -0400

    opposites attract

--- src/CodeWars/EightKyu/EightKyu.java
+++ src/CodeWars/EightKyu/EightKyu.java
@@ -242,6 +242,13 @@ public class EightKyu {
 
 
 
+    // opposites attract
+    public static boolean isLove(final int flower1, final int flower2) {
+
+        return (flower1 % 2) != (flower2 % 2);
+
+    }
+
 
 
 
