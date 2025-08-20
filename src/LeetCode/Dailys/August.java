916cff70915e25fcfda2f8f76012beadb29ea1ac
jim <jamesmagee517@gmail.com>
2025-08-20 18:37:16 -0400

    count square submatrices with all ones

--- src/LeetCode/Dailys/August.java
+++ src/LeetCode/Dailys/August.java
@@ -575,6 +575,36 @@ public class August {
 
 
 
+    // 1277. count square submatrices with all ones
+    public int countSquares(int[][] matrix) {
+
+        int row = matrix.length;
+        int col = matrix[0].length;
+        int result = 0;
+        int prev = 0;
+
+        int[] dp = new int[col + 1];
+
+        for (int i = 1; i <= row; i++) {
+            for (int j = 1; j <= col; j++) {
+
+                if (matrix[i - 1][j - 1] == 1) {
+                    int temp = dp[j];
+                    dp[j] = 1 + Math.min(prev, Math.min(dp[j - 1], dp[j]));
+                    prev = temp;
+                    result += dp[j];
+                } else {
+                    dp[j] = 0;
+                }
+
+            }
+        }
+
+
+        return result;
+
+
+    }
 
 
 
