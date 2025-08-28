2d4e451e75c288da7d95a5bfdc85fe7a5d5db752
jim <jamesmagee517@gmail.com>
2025-08-27 20:35:54 -0400

    sort matrix by diagonals

--- src/LeetCode/Dailys/August.java
+++ src/LeetCode/Dailys/August.java
@@ -922,6 +922,39 @@ public class August {
 
 
 
+        // 3446. sort matrix by diagonals
+    public int[][] sortMatrix(int[][] grid) {
+
+            int n = grid.length;
+
+            for (int i = 0; i < n; i++) {
+                List<Integer> temp = new ArrayList<>();
+                for (int j = 0; i + j < n; j++) {
+                    temp.add(grid[i + j][j]);
+                }
+                temp.sort(Collections.reverseOrder());
+                for (int j = 0; i + j < n; j++) {
+                    grid[i + j][j] = temp.get(j);
+                }
+            }
+
+            for (int j = 1; j < n; j++) {
+                List<Integer> temp = new ArrayList<>();
+                for (int i = 0; j + i < n; i++) {
+                    temp.add(grid[i][j + i]);
+                }
+                Collections.sort(temp);
+                for (int i = 0; j + i < n; i++) {
+                    grid[i][j + i] = temp.get(i);
+                }
+            }
+
+            return grid;
+
+    }
+
+
+
 
 
 
