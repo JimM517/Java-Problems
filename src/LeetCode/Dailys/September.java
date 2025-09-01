06077b3c099142c52ee73ca40f09f4ba26f745f1
jim <jamesmagee517@gmail.com>
2025-09-01 14:04:41 -0400

    added working solution for max pass ratio

--- src/LeetCode/Dailys/September.java
+++ src/LeetCode/Dailys/September.java
@@ -2,6 +2,7 @@ package LeetCode.Dailys;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.PriorityQueue;
 
 public class September {
 
@@ -9,7 +10,7 @@ public class September {
 
 
     // 1792. maximum average pass ratio
-
+    // time limit exceeded
         public double maxAverageRatio(int[][] classes, int extraStudents) {
             List<Double> passRatios = new ArrayList<>();
 
@@ -69,6 +70,57 @@ public class September {
             return totalPassRatio / classes.length;
         }
 
+
+
+
+
+        // this solution works
+        public double maxAverageRatioTwo(int[][] classes, int extraStudents) {
+            // Lambda to calculate the gain of adding an extra student
+            PriorityQueue<double[]> maxHeap = new PriorityQueue<>((a, b) ->
+                    Double.compare(b[0], a[0])
+            );
+
+            for (int[] singleClass : classes) {
+                int passes = singleClass[0];
+                int totalStudents = singleClass[1];
+                double gain = calculateGain(passes, totalStudents);
+                maxHeap.offer(new double[] { gain, passes, totalStudents });
+            }
+
+            // Distribute extra students
+            while (extraStudents-- > 0) {
+                double[] current = maxHeap.poll();
+                double currentGain = current[0];
+                int passes = (int) current[1];
+                int totalStudents = (int) current[2];
+                maxHeap.offer(
+                        new double[] {
+                                calculateGain(passes + 1, totalStudents + 1),
+                                passes + 1,
+                                totalStudents + 1,
+                        }
+                );
+            }
+
+            // Calculate the final average pass ratio
+            double totalPassRatio = 0;
+            while (!maxHeap.isEmpty()) {
+                double[] current = maxHeap.poll();
+                int passes = (int) current[1];
+                int totalStudents = (int) current[2];
+                totalPassRatio += (double) passes / totalStudents;
+            }
+
+            return totalPassRatio / classes.length;
+        }
+
+        private double calculateGain(int passes, int totalStudents) {
+            return (
+                    (double) (passes + 1) / (totalStudents + 1) -
+                            (double) passes / totalStudents
+            );
+        }
 
 
 
