c01d0cbd5a2a221e255f0b32c132cdac53c80b66
jim <jamesmagee517@gmail.com>
2025-09-02 21:00:15 -0400

    find the number of ways to place people I

--- src/LeetCode/Dailys/September.java
+++ src/LeetCode/Dailys/September.java
@@ -125,6 +125,146 @@ public class September {
 
 
 
+        // 3025. find the number of ways to place people I
+    public int numberOfPairs(int[][] points) {
+
+         int answer = 0;
+         int n = points.length;
+
+         for (int i = 0; i < n; i++) {
+             int[] pointA = points[i];
+             for (int j = 0; j < n; j++) {
+                 int[] pointB = points[j];
+                 if (i == j || !(pointA[0] <= pointB[0] && pointA[1] >= pointB[1])) {
+                     continue;
+                 }
+                 if (n == 2) {
+                     answer++;
+                     continue;
+                 }
+
+                 boolean illegal = false;
+                 for (int k = 0; k < n; k++) {
+                     if (k == i || k == j) {
+                         continue;
+                     }
+                     int[] pointTemp = points[k];
+                     boolean isXContained = pointTemp[0] >= pointA[0] && pointTemp[0] <= pointB[0];
+                     boolean isYContained = pointTemp[1] <= pointA[1] && pointTemp[1] >= pointB[1];
+                     if (isXContained && isYContained) {
+                         illegal = true;
+                         break;
+                     }
+                 }
+                 if (!illegal) {
+                     answer++;
+                 }
+             }
+         }
+
+
+        return answer;
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
