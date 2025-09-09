6a377bb447b2f5d46f354eabb3047c27dec5e919
jim <jamesmagee517@gmail.com>
2025-09-08 21:12:36 -0400

    convert integer to the sum of two no-zero integers

--- src/LeetCode/Dailys/September.java
+++ src/LeetCode/Dailys/September.java
@@ -296,7 +296,17 @@ public class September {
 
 
 
+    // 1317. convert integer to the sum of two no-zero integers
+    public int[] getNoZeroIntegers(int n) {
 
+        for (int A = 1; A < n; A++) {
+            int B = n - A;
+            if (!String.valueOf(A).contains("0") && !String.valueOf(B).contains("0")) {
+                return new int[] {A, B};
+            }
+        }
+        return new int[0];
+    }
 
 
 
