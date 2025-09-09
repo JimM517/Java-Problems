6ed94bf9b5e6967856383ee07ce750db6fe059f5
jim <jamesmagee517@gmail.com>
2025-09-08 21:18:15 -0400

    number of people aware of secret

--- src/LeetCode/Dailys/September.java
+++ src/LeetCode/Dailys/September.java
@@ -310,6 +310,32 @@ public class September {
 
 
 
+    // 2327. number of people aware of a secret
+    public int peopleAwareOfSecret(int n, int delay, int forget) {
+        int MOD = 1_000_000_007;
+        long[] dp = new long[n + 1];
+        dp[1] = 1;
+        long share = 0;
+
+        for (int day = 2; day <= n; day++) {
+            if (day - delay >= 1) {
+                share = (share + dp[day - delay]) % MOD;
+            }
+
+            if (day - forget >= 1) {
+                share = (share - dp[day - forget] + MOD) % MOD;
+            }
+            dp[day] = share;
+        }
+
+        long result = 0;
+        for (int i = n - forget + 1; i <= n; i++) {
+            if (i >= 1) {
+                result = (result + dp[i]) % MOD;
+            }
+        }
+        return (int) result;
+    }
 
 
 
