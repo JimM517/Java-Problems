2ce2327eadef4e0f19cbc5b9072c7e76e9a65ca8
jim <jamesmagee517@gmail.com>
2025-09-14 10:01:52 -0400

    multiple of index

--- src/CodeWars/EightKyu/EightKyu.java
+++ src/CodeWars/EightKyu/EightKyu.java
@@ -432,7 +432,21 @@ public class EightKyu {
     }
 
 
+    // multiple of index
+    public static int[] multipleOfIndex(int[] array) {
 
+        List<Integer> list = new ArrayList<>();
+        if (array[0] == 0) {
+            list.add(0);
+        }
+
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] % i == 0) {
+                list.add(array[i]);
+            }
+        }
+        return list.stream().mapToInt(x -> x).toArray();
+    }
 
 
 
