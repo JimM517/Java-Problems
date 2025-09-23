77ec639d7831e35bdf03c2bde5a8b1175d77fc17
jim <jamesmagee517@gmail.com>
2025-09-22 20:51:43 -0400

    count the elements with max frequencyt

--- src/LeetCode/Dailys/September.java
+++ src/LeetCode/Dailys/September.java
@@ -964,7 +964,31 @@ class MovieRentingSystem {
 
 
 
+// 3005. count the elements with maximum frequency
+    public int maxFrequencyElements(int[] nums) {
 
+        Map<Integer, Integer> freq = new HashMap<>();
+
+        for (int num : nums) {
+            freq.put(num, freq.getOrDefault(num, 0) + 1);
+        }
+
+        int maxFreq = 0;
+        for (int f : freq.values()) {
+            maxFreq = Math.max(maxFreq, f);
+        }
+
+        int res = 0;
+        for (int f : freq.values()) {
+            if (f == maxFreq) {
+                res += f;
+            }
+        }
+
+
+        return res;
+
+    }
 
 
 
