ae538ed2e947a5747b4d6353b028eefd9a69f6cf
jim <jamesmagee517@gmail.com>
2025-09-23 20:39:27 -0400

    fraction to recurring decimal

--- src/LeetCode/Dailys/September.java
+++ src/LeetCode/Dailys/September.java
@@ -1025,6 +1025,45 @@ class MovieRentingSystem {
 
 
 
+    // 166. fraction to recurring decimal
+    public String fractionToDecimal(int numerator, int denominator) {
+
+        if (numerator == 0) {
+            return "0";
+        }
+
+        StringBuilder sb = new StringBuilder();
+        if (numerator < 0 ^ denominator < 0) {
+            sb.append("-");
+        }
+
+        long dividend = Math.abs(Long.valueOf(numerator));
+        long divisor = Math.abs(Long.valueOf(denominator));
+
+        sb.append(dividend / divisor);
+        long remainder = dividend % divisor;
+        if (remainder == 0) {
+            return sb.toString();
+        }
+
+        sb.append(".");
+        Map<Long, Integer> map = new HashMap<>();
+        while (remainder != 0) {
+            if (map.containsKey(remainder)) {
+                sb.insert(map.get(remainder), "(");
+                sb.append(")");
+                break;
+            }
+            map.put(remainder, sb.length());
+            remainder *= 10;
+            sb.append(remainder / divisor);
+            remainder %= divisor;
+        }
+
+        return sb.toString();
+
+    }
+
 
 
 
