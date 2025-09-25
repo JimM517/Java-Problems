5200e13ecaa54f98783569fb20446c25ffd56008
jim <jamesmagee517@gmail.com>
2025-09-25 19:02:44 -0400

    area or perimeter

--- src/CodeWars/EightKyu/EightKyu.java
+++ src/CodeWars/EightKyu/EightKyu.java
@@ -535,6 +535,14 @@ public class EightKyu {
 
 
 
+    // area or perimeter
+    public static int areaOrPerimeter(int l, int w) {
+
+        if (l == w) {
+            return l * w;
+        }
+        return (2 * l) + (2 * w);
+    }
 
 
 
