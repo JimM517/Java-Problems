a368270bc841fc0d6950ebcc9b057109582e8d77
jim <jamesmagee517@gmail.com>
2025-09-27 09:49:42 -0400

    valid triangle number

--- src/LeetCode/Dailys/September.java
+++ src/LeetCode/Dailys/September.java
@@ -1086,7 +1086,35 @@ class MovieRentingSystem {
 
 
 
-    // 812. largest triangle area
+    // 611. valid triangle number
+    // binary search
+        int binarySearch(int nums[], int l, int r, int x) {
+            while (r >= l && r < nums.length) {
+                int mid = (l + r) / 2;
+                if (nums[mid] >= x)
+                    r = mid - 1;
+                else
+                    l = mid + 1;
+            }
+            return l;
+        }
+        public int triangleNumber(int[] nums) {
+            int count = 0;
+            Arrays.sort(nums);
+            for (int i = 0; i < nums.length - 2; i++) {
+                int k = i + 2;
+                for (int j = i + 1; j < nums.length - 1 && nums[i] != 0; j++) {
+                    k = binarySearch(nums, k, nums.length - 1, nums[i] + nums[j]);
+                    count += k - j - 1;
+                }
+            }
+            return count;
+        }
+
+
+
+
+        // 812. largest triangle area
     public double largestTriangleArea(int[][] points) {
 
             // Gauss's area formula -> area = 0.5 * [x1 * (y2 - y3) + x2 * (y3 - y1) + x3 * (y1 - y2)]
