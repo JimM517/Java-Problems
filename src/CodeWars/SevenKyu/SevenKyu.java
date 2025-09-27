1e834610ce264ceb22354f5ae77e76712852076e
jim <jamesmagee517@gmail.com>
2025-09-27 09:57:44 -0400

    name array capping

--- src/CodeWars/SevenKyu/SevenKyu.java
+++ src/CodeWars/SevenKyu/SevenKyu.java
@@ -675,7 +675,18 @@ public class SevenKyu {
 
 
 
+    // name array capping
+    public static String[] capMe(String[] strings) {
 
+        for (int i = 0; i < strings.length; i++) {
+
+            if (strings[i].isEmpty()) {
+                continue;
+            }
+            strings[i] = strings[i].substring(0, 1).toUpperCase() + strings[i].substring(1).toLowerCase();
+        }
+        return strings;
+    }
 
 
 
