fdcdc5facd5a224e2547c5e2bc191e889fc1f46e
jim <jamesmagee517@gmail.com>
2025-09-26 20:54:43 -0400

    largest triangle area

--- src/LeetCode/Dailys/September.java
+++ src/LeetCode/Dailys/September.java
@@ -1086,7 +1086,32 @@ class MovieRentingSystem {
 
 
 
+    // 812. largest triangle area
+    public double largestTriangleArea(int[][] points) {
 
+            // Gauss's area formula -> area = 0.5 * [x1 * (y2 - y3) + x2 * (y3 - y1) + x3 * (y1 - y2)]
+
+            int N = points.length;
+
+            double result = 0;
+
+            for (int i = 0; i < N; i++) {
+                for (int j = i + 1; j < N; j++) {
+                    for (int k = j + 1; k < N; k++) {
+                        result = Math.max(result, area(points[i], points[j], points[k]));
+                    }
+                }
+            }
+
+        return result;
+
+    }
+
+
+    public double area(int[] P, int[] Q, int[] R) {
+
+        return 0.5 * Math.abs(P[0] * Q[1] + Q[0] * R[1] + R[0] * P[1] - P[1] * Q[0] - Q[1] * R[0] - R[1] * P[0]);
+    }
 
 
 
