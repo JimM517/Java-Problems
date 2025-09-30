0f2a8dc67e4d93d97c36269af69d38e2e0f5eac1
jim <jamesmagee517@gmail.com>
2025-09-30 19:01:25 -0400

    two sum

--- src/CodeWars/SixKyu/SixKyuQuestions.java
+++ src/CodeWars/SixKyu/SixKyuQuestions.java
@@ -525,6 +525,29 @@ public class SixKyuQuestions {
     }
 
 
+    // two sum
+    public static int[] twoSum(int[] nums, int target) {
+
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+
+            if (map.containsKey(target - nums[i])) {
+                return new int[]{map.get(target - nums[i]), i};
+            } else {
+                map.put(nums[i], i);
+            }
+
+
+        }
+
+        return new int[]{-1, -1};
+
+
+
+
+    }
+
 
 
 
