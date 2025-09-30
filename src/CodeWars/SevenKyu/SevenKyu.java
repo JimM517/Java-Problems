71212f3ac5e878e5dec72bbe3223efb6182f366f
jim <jamesmagee517@gmail.com>
2025-09-30 18:44:23 -0400

    sum of the first nth term of series

--- src/CodeWars/SevenKyu/SevenKyu.java
+++ src/CodeWars/SevenKyu/SevenKyu.java
@@ -775,6 +775,21 @@ public class SevenKyu {
 
 
 
+    // sum of the first nth term of series
+    public static String seriesSum(int n) {
+
+        if (n == 0) {
+            return "0.00";
+        }
+
+        double sum = 0.0;
+        for (int i = 0; i < n; i++) {
+            sum += 1.0 / (1 + 3 * i);
+        }
+        return String.format("%.2f", sum);
+
+    }
+
 
 
 
