159207cdff56f31114a3c4054fa818f808bb99f9
jim <jamesmagee517@gmail.com>
2025-10-03 18:46:35 -0400

    summing a numbers digits

--- src/CodeWars/SevenKyu/SevenKyu.java
+++ src/CodeWars/SevenKyu/SevenKyu.java
@@ -818,9 +818,18 @@ public class SevenKyu {
 
 
 
+    // summing a numbers digits
+    public static int sumDigits(int number) {
 
+        number = Math.abs(number);
+        int sum = 0;
 
-
+        while (number > 0) {
+            sum += number % 10;
+            number /= 10;
+        }
+        return sum;
+    }
 
 
 
