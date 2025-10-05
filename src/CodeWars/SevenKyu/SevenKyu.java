72f5536fe4ee342ff6cd06f348b34a05469ab093
jim <jamesmagee517@gmail.com>
2025-10-05 09:35:03 -0400

    factorial

--- src/CodeWars/SevenKyu/SevenKyu.java
+++ src/CodeWars/SevenKyu/SevenKyu.java
@@ -870,6 +870,18 @@ public class SevenKyu {
 
 
 
+    // factorial
+    public static long factorial(int n) {
+
+        if (n == 0 || n == 1) {
+            return 1;
+        }
+
+        return n * factorial(n - 1);
+
+
+
+    }
 
 
 
