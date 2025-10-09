f1c1cdfd9144dfa69629a78b74f52e9cb7260e21
jim <jamesmagee517@gmail.com>
2025-10-08 20:33:02 -0400

    find the minimum amount of time to brew potions

--- src/LeetCode/Dailys/October.java
+++ src/LeetCode/Dailys/October.java
@@ -385,7 +385,29 @@ public class October {
 
 
 
+    // 3494. find the minimum amount of time to brew potions
+    public long minTime(int[] skill, int[] mana) {
 
+            int n = skill.length, m = mana.length;
+
+            long[] done = new long[n + 1];
+
+            for (int j = 0; j < m; j++) {
+                for (int i = 0; i < n; i++) {
+                    done[i + 1] = Math.max(done[i + 1], done[i]) + (long) mana[j] * skill[i];
+                }
+                for (int i = n - 1; i > 0; i--) {
+                    done[i] = done[i + 1] - (long) mana[j] * skill[i];
+                }
+            }
+
+
+
+            return done[n];
+
+
+
+    }
 
 
 
