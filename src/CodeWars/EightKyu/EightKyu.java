c335715327703bcf89aa5c56e2ddf8f0b471e163
jim <jamesmagee517@gmail.com>
2025-10-10 18:25:12 -0400

    exclamation marks series 11: replace all vowel to exclamation mark in sentence

--- src/CodeWars/EightKyu/EightKyu.java
+++ src/CodeWars/EightKyu/EightKyu.java
@@ -690,7 +690,23 @@ public class EightKyu {
 
 
 
+ // exclamation marks series 11: replace all vowel to exclamation mark in sentence
+    public static String replace(final String s) {
 
+        StringBuilder result = new StringBuilder();
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if ("aeiouAEIOU".indexOf(c) != -1) {
+                result.append('!');
+            } else {
+                result.append(c);
+            }
+        }
+
+        return result.toString();
+
+    }
 
 
 
