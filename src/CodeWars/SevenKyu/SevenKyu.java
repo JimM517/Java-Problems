8fe3492b8d5a74d1bfbf89d92f1ce8e7cafbe039
jim <jamesmagee517@gmail.com>
2025-10-10 18:29:47 -0400

    predict your age

--- src/CodeWars/SevenKyu/SevenKyu.java
+++ src/CodeWars/SevenKyu/SevenKyu.java
@@ -907,7 +907,18 @@ public class SevenKyu {
 
 
 
+    // predict your age
+    public static int predictAge(int age1, int age2, int age3, int age4, int age5, int age6, int age7, int age8) {
 
+
+        int total = (age1 * age1) + (age2 * age2) + (age3 * age3) + (age4 * age4) + (age5 * age5) + (age6 * age6) + (age7 * age7) + (age8 * age8);
+        total = (int) Math.sqrt(total);
+        total /= 2;
+
+
+        return total;
+
+    }
 
 
 
