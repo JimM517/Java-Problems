65b22c881146ac784274072c542a23942e1e01b2
jim <jamesmagee517@gmail.com>
2025-10-14 20:05:02 -0400

    adjacent increasing subarrays detection II

--- src/LeetCode/Dailys/October.java
+++ src/LeetCode/Dailys/October.java
@@ -605,6 +605,28 @@ public class October {
     }
 
 
+    // 3350. adjacent increasing subarrays detection II
+    public int maxIncreasingSubarrays(List<Integer> nums) {
+
+        int n = nums.size();
+        int cnt = 1;
+        int precnt = 0;
+        int answer = 0;
+
+        for (int i = 1; i < n; i++) {
+            if (nums.get(i) > nums.get(i - 1)) {
+                cnt++;
+            } else {
+                precnt = cnt;
+                cnt = 1;
+            }
+            answer = Math.max(answer, Math.min(precnt, cnt));
+            answer = Math.max(answer, cnt / 2);
+        }
+
+        return answer;
+
+    }
 
 
 
