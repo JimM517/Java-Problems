3a4d50fee9f2950299b48ed16eddcd0097a5a3f1
jim <jamesmagee517@gmail.com>
2025-10-18 09:49:45 -0400

    maximum number of distinct elements after operations

--- src/LeetCode/Dailys/October.java
+++ src/LeetCode/Dailys/October.java
@@ -714,7 +714,25 @@ public class October {
 
 
 
+        // 3397. maximum number of distinct elements after operations
+    public int maxDistinctElements(int[] nums, int k) {
 
+        Arrays.sort(nums);
+        int cnt = 0;
+        int prev = Integer.MIN_VALUE;
+        for (int num : nums) {
+            int curr = Math.min(Math.max(num - k, prev + 1), num + k);
+            if (curr > prev) {
+                cnt++;
+                prev = curr;
+            }
+        }
+
+        return cnt;
+
+
+
+    }
 
 
 
