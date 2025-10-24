63759577cffe674f7e26d7a6ba130cd244e524bf
jim <jamesmagee517@gmail.com>
2025-10-23 20:21:28 -0400

    next greater numerically balanced number

--- src/LeetCode/Dailys/October.java
+++ src/LeetCode/Dailys/October.java
@@ -889,8 +889,36 @@ public class October {
 
 
 
+    // 2048. next greater numerically balances number
+    public int nextBeautifulNumber(int n) {
+
+        for (int i = n + 1; i <= 1224444; i++) {
+            if (isBalance(i)) {
+                return i;
+            }
+        }
+
+        return -1;
 
 
+    }
+
+
+    private boolean isBalance(int x) {
+
+
+        int[] count = new int[10];
+        while (x > 0) {
+            count[x % 10]++;
+            x /= 10;
+        }
+        for (int d = 0; d < 10; d++) {
+            if (count[d] > 0 && count[d] != d) {
+                return false;
+            }
+        }
+        return true;
+    }
 
 
 
