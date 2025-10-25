ecdd79d6a7bebb215eed871e328e9900c4c5ec4f
jim <jamesmagee517@gmail.com>
2025-10-25 09:50:31 -0400

    calculate money in leetcode bank

--- src/LeetCode/Dailys/October.java
+++ src/LeetCode/Dailys/October.java
@@ -923,6 +923,21 @@ public class October {
 
 
 
+    // 1716. calculate money in leetcode bank
+    public int totalMoney(int n) {
+        int k = n / 7;
+        int F = 28;
+        int L = 28 + (k - 1) * 7;
+        int arithmeticSum = k * (F + L) / 2;
+
+        int monday = k + 1;
+        int finalWeek = 0;
+        for (int day = 0; day < n % 7; day++) {
+            finalWeek += monday + day;
+        }
+        return arithmeticSum + finalWeek;
+    }
+
 
 
 
