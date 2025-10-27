3d9c90a39809e9a5cd3a814ff9f35422eb5a8af4
jim <jamesmagee517@gmail.com>
2025-10-27 19:20:49 -0400

    number of laser beams in bank

--- src/CodeWars/SixKyu/SixKyuQuestions.java
+++ src/CodeWars/SixKyu/SixKyuQuestions.java
@@ -790,6 +790,28 @@ public class SixKyuQuestions {
 
 
 
+    // 2125. number of laser beams in bank
+        public int numberOfBeams(String[] bank) {
+            int prev = 0, ans = 0;
+
+            for (String s: bank) {
+                int count = 0;
+                for (int i = 0; i < s.length(); i++)
+                    if (s.charAt(i) == '1') {
+                        count++;
+                    }
+
+                if (count > 0) {
+                    ans += prev * count;
+                    prev = count;
+                }
+            }
+
+            return ans;
+        }
+
+
+
 
 
 
