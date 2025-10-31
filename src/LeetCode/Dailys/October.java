56b9a5b400f7d1cb0cb43f719c1acd8780800489
jim <jamesmagee517@gmail.com>
2025-10-31 18:09:16 -0400

    the two sneaky numbers of digitville

--- src/LeetCode/Dailys/October.java
+++ src/LeetCode/Dailys/October.java
@@ -1007,7 +1007,26 @@ public class October {
 
 
 
+    // 3289. the two sneaky numbers of digitville
+    public int[] getSneakyNumbers(int[] nums) {
 
+
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int num : nums) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+
+        int[] result = new int[2];
+        int idx = 0;
+
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            if (entry.getValue() == 2) {
+                result[idx++] = entry.getKey();
+                if (idx == 2) break;
+            }
+        }
+        return result;
+    }
 
 
 
