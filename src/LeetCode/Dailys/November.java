3561457bc5929e7eb5729cb63ef1a919951a3d91
jim <jamesmagee517@gmail.com>
2025-11-01 09:49:11 -0400

    fixed semicolon

--- src/LeetCode/Dailys/November.java
+++ src/LeetCode/Dailys/November.java
@@ -11,7 +11,7 @@ public class November {
         ListNode next;
         ListNode() {};
         ListNode(int val) { this.val = val; }
-        ListNode(int val, ListNode next) { this.val = val; this.next = next };
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; };
     }
 
 
