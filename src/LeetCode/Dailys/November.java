5df7a9de31a786a7f943f75a087babf6bee10781
jim <jamesmagee517@gmail.com>
2025-11-01 09:47:18 -0400

    delete nodes from linked list present in array

--- src/LeetCode/Dailys/November.java
+++ src/LeetCode/Dailys/November.java
@@ -1,6 +1,202 @@
 package LeetCode.Dailys;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class November {
+
+
+    public class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {};
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next };
+    }
+
+
+    public ListNode modifiedList(int[] nums, ListNode head) {
+
+        Set<Integer> valuesToRemove = new HashSet<>();
+        for (int num : nums) {
+            valuesToRemove.add(num);
+        }
+
+        while (head != null && valuesToRemove.contains(head.val)) {
+            head = head.next;
+        }
+
+
+        if (head == null) {
+            return null;
+        }
+
+        ListNode current = head;
+        while (current.next != null) {
+            if (valuesToRemove.contains(current.next.val)) {
+                current.next = current.next.next;
+            } else {
+                current = current.next;
+            }
+        }
+        return head;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
