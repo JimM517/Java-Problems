3d98b208e88e9e082357453ea67f9f4e4aac0e8f
jim <jamesmagee517@gmail.com>
2025-11-11 20:19:47 -0500

    minimum operations to convert all elements to zero

--- src/LeetCode/Dailys/November.java
+++ src/LeetCode/Dailys/November.java
@@ -519,7 +519,27 @@ public class November {
 
 
 
+    // minimum operations to convert all elements to zero
+    public int minOperations(int[] nums) {
 
+        List<Integer> s = new ArrayList<>();
+        int res = 0;
+        for (int a : nums) {
+            while (!s.isEmpty() && s.get(s.size() - 1) > a) {
+                s.remove(s.size() - 1);
+            }
+            if (a == 0) {
+                continue;
+            }
+            if (s.isEmpty() || s.get(s.size() - 1) < a) {
+                res++;
+                s.add(a);
+            }
+        }
+
+        return res;
+
+    }
 
 
 
