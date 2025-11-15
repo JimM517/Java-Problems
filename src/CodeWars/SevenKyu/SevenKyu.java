460e48e8bbadbae7f55a7d948d0af3f29ad3a3ec
jim <jamesmagee517@gmail.com>
2025-11-15 15:44:43 -0500

    small enough

--- src/CodeWars/SevenKyu/SevenKyu.java
+++ src/CodeWars/SevenKyu/SevenKyu.java
@@ -975,6 +975,19 @@ public class SevenKyu {
 
 
 
+    // small enough?
+    public static boolean smallEnough(int[] a, int limit) {
+
+        for (int num : a) {
+            if (num > limit) {
+                return false;
+            }
+        }
+
+
+        return true;
+    }
+
 
 
 
