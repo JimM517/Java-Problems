4d5ac9865ac9ecfd54df755a10d45c6b3cf76ed3
jim <jamesmagee517@gmail.com>
2025-11-17 20:53:10 -0500

    1 bit and 2 bit characters

--- src/LeetCode/Dailys/November.java
+++ src/LeetCode/Dailys/November.java
@@ -705,6 +705,17 @@ public class November {
 
 
 
+    // 717. 1-bit and 2-bit character
+    public boolean isOneBitCharacter(int[] bits) {
+
+         int i = 0;
+         while (i < bits.length -1) {
+             i += bits[i] + 1;
+         }
+
+         return i == bits.length - 1;
+
+    }
 
 
 
