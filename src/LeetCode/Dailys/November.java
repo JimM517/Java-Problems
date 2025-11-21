c337ee1eac430e2e787a47595683ee4aa9692a7b
jim <jamesmagee517@gmail.com>
2025-11-20 20:32:27 -0500

    unique length 3 palindromic subsequence

--- src/LeetCode/Dailys/November.java
+++ src/LeetCode/Dailys/November.java
@@ -764,6 +764,38 @@ public class November {
 
 
 
+    // 1930. unique length-3 palindromic subsequence
+    public int countPalindromicSubsequence(String s) {
+
+        Set<Character> letters = new HashSet<>();
+        for (Character c : s.toCharArray()) {
+            letters.add(c);
+        }
+
+        int answer = 0;
+        for (Character letter : letters) {
+            int i = -1;
+            int j = 0;
+            for (int k = 0; k < s.length(); k++) {
+                if (s.charAt(k) == letter) {
+                    if (i == -1) {
+                        i = k;
+                    }
+                    j = k;
+                }
+            }
+            Set<Character> between = new HashSet<>();
+            for (int k = i + 1; k < j; k++) {
+                between.add(s.charAt(k));
+            }
+            answer += between.size();
+        }
+
+
+        return answer;
+
+    }
+
 
 
 
