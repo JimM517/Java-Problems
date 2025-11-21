3883a7efba0b6b3f781b2ab389e23b86afbc92e8
jim <jamesmagee517@gmail.com>
2025-11-21 18:43:44 -0500

    power of two

--- src/CodeWars/SevenKyu/SevenKyu.java
+++ src/CodeWars/SevenKyu/SevenKyu.java
@@ -991,8 +991,22 @@ public class SevenKyu {
 
 
 
+    // power of two
+    public static boolean isPowerOfTwo(long n) {
 
 
+        if (n <= 0) {
+            return false;
+        }
+
+        while (n > 1) {
+            if (n % 2 == 1) {
+                return false;
+            }
+            n /= 2;
+        }
+        return true;
+    }
 
 
 
