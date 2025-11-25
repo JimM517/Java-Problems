c278e915d4b54019c913adf0f52f4c827e737707
jim <jamesmagee517@gmail.com>
2025-11-24 21:16:34 -0500

    smallest integer divisible by k

--- src/LeetCode/Dailys/November.java
+++ src/LeetCode/Dailys/November.java
@@ -865,6 +865,20 @@ public class November {
 
 
 
+    // 1015. smallest integer divisible by k
+    public int smallestRepunitDivByK(int k) {
+
+        int remainder = 0;
+        for (int len = 1; len <= k; len++) {
+            remainder = (remainder * 10 + 1) % k;
+            if (remainder == 0) {
+                return len;
+            }
+        }
+
+         return -1;
+
+    }
 
 
 
