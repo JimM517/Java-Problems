02038d3a5fb00801d6e8299747f195ef30417bd1
jim <jamesmagee517@gmail.com>
2025-11-24 20:51:12 -0500

    binary prefix divisible by 5

--- src/LeetCode/Dailys/November.java
+++ src/LeetCode/Dailys/November.java
@@ -841,6 +841,25 @@ public class November {
 
 
 
+     // 1018. binary prefix divisible by 5
+    public List<Boolean> prefixesDivBy5(int[] nums) {
+
+        List<Boolean> answer = new ArrayList<>();
+
+        int prefix = 0;
+
+        int length = nums.length;
+
+        for (int i = 0; i < length; i++) {
+            prefix = ((prefix << 1) + nums[i]) % 5;
+            answer.add(prefix == 0);
+        }
+
+        return answer;
+
+
+
+    }
 
 
 
