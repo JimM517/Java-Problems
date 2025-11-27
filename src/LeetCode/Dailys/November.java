7aa853518bbd2ddca7d17fff3aac143787f1b3f3
jim <jamesmagee517@gmail.com>
2025-11-27 09:43:07 -0500

    maximum subarray sum with length divisible by k

--- src/LeetCode/Dailys/November.java
+++ src/LeetCode/Dailys/November.java
@@ -917,7 +917,26 @@ public class November {
 
 
 
+    // 3381. maximum subarray sum with length divisible by k
+    public long maxSubarraySum(int[] nums, int k) {
 
+            int n = nums.length;
+            long prefixSum = 0;
+            long maxSum = Long.MIN_VALUE;
+            long[] kSum = new long[k];
+            for (int i = 0; i < k; i++) {
+                kSum[i] = Long.MAX_VALUE / 2;
+            }
+            kSum[k - 1] = 0;
+            for (int i = 0; i < n; i++) {
+                prefixSum += nums[i];
+                maxSum = Math.max(maxSum, prefixSum - kSum[i % k]);
+                kSum[i % k] = Math.min(kSum[i % k], prefixSum);
+            }
+
+            return maxSum;
+
+    }
 
 
 
