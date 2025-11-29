859ce94bf6f52a99732fd2e742ce445754ec8801
jim <jamesmagee517@gmail.com>
2025-11-29 09:39:05 -0500

    minimum operations to make array sum divisible by k

--- src/LeetCode/Dailys/November.java
+++ src/LeetCode/Dailys/November.java
@@ -1012,7 +1012,21 @@ public class November {
 
 
 
+        // 3512. minimum operations to make array sum divisible by k
+    public int minOperations(int[] nums, int k) {
 
+            int sum = 0;
+
+            for (int i = 0; i < nums.length; i++) {
+
+                sum += nums[i];
+
+            }
+
+
+        return sum % k;
+
+    }
 
 
 
