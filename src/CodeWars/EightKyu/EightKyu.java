a07498d5595a94e0b62e5e5dbccf8b73dce0ef53
jim <jamesmagee517@gmail.com>
2025-12-02 18:05:42 -0500

    switch it up

--- src/CodeWars/EightKyu/EightKyu.java
+++ src/CodeWars/EightKyu/EightKyu.java
@@ -904,7 +904,25 @@ public class EightKyu {
 
 
 
+    // switch it up
+    public static String switchItUp(int number) {
 
+        return switch (number) {
+            case 1 -> "One";
+            case 2 -> "Two";
+            case 3 -> "Three";
+            case 4 -> "Four";
+            case 5 -> "Five";
+            case 6 -> "Six";
+            case 7 -> "Seven";
+            case 8 -> "Eight";
+            case 9 -> "Nine";
+            default -> "Zero";
+        };
+
+
+
+    }
 
 
 
