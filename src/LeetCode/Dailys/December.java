466bc524d54d8283ac5724d6d386cbd1f6eefe16
jim <jamesmagee517@gmail.com>
2025-12-01 20:44:44 -0500

    max running time of N computers

--- src/LeetCode/Dailys/December.java
+++ src/LeetCode/Dailys/December.java
@@ -4,6 +4,30 @@ public class December {
 
 
 
+    // 2141. maximum running time of N computers
+    public long maxRunTime(int n, int[] batteries) {
+
+        long sumPower = 0;
+        for (int power : batteries) {
+            sumPower += power;
+        }
+
+        long left = 1, right = sumPower / n;
+
+        while (left < right){
+            long target = right - (right - left) / 2;
+            long extra = 0;
+
+            for (int power : batteries)
+                extra += Math.min(power, target);
+
+            if (extra >= (long)(n * target))
+                left = target;
+            else
+                right = target - 1;
+        }
+        return left;
+    }
 
 
 
