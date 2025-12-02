80ba5f848d58fed3a3e0a9c8418def647f73d85f
jim <jamesmagee517@gmail.com>
2025-12-02 18:18:21 -0500

    strip comments

--- src/CodeWars/FourKyu/FourKyu.java
+++ src/CodeWars/FourKyu/FourKyu.java
@@ -3,7 +3,36 @@ package CodeWars.FourKyu;
 public class FourKyu {
 
 
+// strip comments
+    /** DOESNT WORK YET **/
+    public static String stripComments(String text, String[] commentSymbols) {
 
+        String[] lines = text.split("\n");
+        StringBuilder result = new StringBuilder();
+
+        for (String line : lines) {
+            String cleaned = line;
+
+            for (String symbol : commentSymbols) {
+                int index = cleaned.indexOf(symbol);
+                if (index != -1) {
+                    cleaned = cleaned.substring(0, index);
+                }
+            }
+
+            cleaned = cleaned.stripTrailing();
+
+            result.append(cleaned).append("\n");
+
+
+        }
+
+
+
+        return result.toString().stripTrailing();
+
+
+    }
 
 
 
