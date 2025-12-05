b6c24899bdf1ac9ccb3b017a1b62ab0ff831ab72
jim <jamesmagee517@gmail.com>
2025-12-04 21:30:34 -0500

    powers of I

--- src/CodeWars/SevenKyu/SevenKyu.java
+++ src/CodeWars/SevenKyu/SevenKyu.java
@@ -1161,6 +1161,22 @@ public class SevenKyu {
 
 
 
+    // powers of I
+    public static String pofi(int n) {
+
+        switch (n % 4) {
+            case 0: return "1";
+            case 1: return "i";
+            case 2: return "-1";
+            case 3: return "-i";
+        }
+
+
+        return "";
+
+
+
+    }
 
 
 
