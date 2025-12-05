634076453fc1733242a5ca89df98f4efd7f88e34
jim <jamesmagee517@gmail.com>
2025-12-05 18:54:44 -0500

    snail sort

--- src/CodeWars/FourKyu/FourKyu.java
+++ src/CodeWars/FourKyu/FourKyu.java
@@ -1,5 +1,8 @@
 package CodeWars.FourKyu;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class FourKyu {
 
 
@@ -39,7 +42,56 @@ public class FourKyu {
 
 
 
+    // snail sort
+    public static int[] snail(int[][] array) {
 
+        if (array == null || array.length == 0 || array[0].length == 0) {
+            return new int[0];
+        }
+
+        int n = array.length;
+        List<Integer> result = new ArrayList<>();
+
+        int top = 0;
+        int bottom = n - 1;
+        int left = 0;
+        int right = n - 1;
+
+        while (top <= bottom && left <= right) {
+            for (int i = left; i <= right; i++) {
+                result.add(array[top][i]);
+            }
+            top++;
+
+            for (int i = top; i <= bottom; i++) {
+                result.add(array[i][right]);
+            }
+            right--;
+
+            if (top <= bottom) {
+                for (int i = right; i >= left; i--) {
+                    result.add(array[bottom][i]);
+                }
+                bottom--;
+            }
+
+
+            if (left <= right) {
+                for (int i = bottom; i >= top; i--) {
+                    result.add(array[i][left]);
+                }
+                left++;
+            }
+
+        }
+
+
+        return result.stream().mapToInt(i -> i).toArray();
+
+
+
+
+    }
 
 
 
