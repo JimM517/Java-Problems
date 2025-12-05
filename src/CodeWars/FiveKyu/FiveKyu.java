e95839bee2b33a3431f7e2c65975b8d40e9565b4
jim <jamesmagee517@gmail.com>
2025-12-05 18:59:42 -0500

    human readable time

--- src/CodeWars/FiveKyu/FiveKyu.java
+++ src/CodeWars/FiveKyu/FiveKyu.java
@@ -427,6 +427,20 @@ public class FiveKyu {
 
 
 
+    // human readable time
+    public static String makeReadable(int seconds) {
+
+        int hours = seconds / 3600;
+        int minutes = (seconds % 3600) / 60;
+        int secs = seconds % 60;
+
+
+        return String.format("%02d:%02d:%02d", hours, minutes, secs);
+
+
+
+
+    }
 
 
 
