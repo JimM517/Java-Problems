2b03665f21be59780f703070a330e18b949ff94b
jim <jamesmagee517@gmail.com>
2025-12-07 09:45:09 -0500

    calculate average

--- src/CodeWars/EightKyu/EightKyu.java
+++ src/CodeWars/EightKyu/EightKyu.java
@@ -968,6 +968,26 @@ public class EightKyu {
 
 
 
+    // calculate average
+    public static double findAverage(int[] array) {
+
+        if (array.length == 0) {
+            return 0.0;
+        }
+
+
+        double result = 0.0;
+
+        for (int i = 0; i < array.length; i++) {
+            result += array[i];
+        }
+
+
+        return result / array.length;
+
+
+
+    }
 
 
 
