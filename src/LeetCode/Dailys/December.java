00261c251d14b888557a7acbec358cd2771f47bb
jim <jamesmagee517@gmail.com>
2025-12-09 20:32:47 -0500

    count number of computer unlocking permutations

--- src/LeetCode/Dailys/December.java
+++ src/LeetCode/Dailys/December.java
@@ -294,6 +294,25 @@ public class December {
 
 
 
+    // 3577. count the number of computer unlocking permutations
+    public int countPermutations(int[] complexity) {
+
+        int n = complexity.length;
+        for (int i = 1; i < n; i++) {
+            if (complexity[i] <= complexity[0]) {
+                return 0;
+            }
+        }
+
+
+        int answer = 1;
+        int mod = 1000000007;
+        for (int i = 2; i < n; i++) {
+            answer = (int) (((long) answer * i) % mod);
+        }
+        return answer;
+    }
+
 
 
 
