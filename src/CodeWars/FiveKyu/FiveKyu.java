f2e3573743412ca688ab65a09b48d68bb0bf5bd0
jim <jamesmagee517@gmail.com>
2025-12-13 18:31:43 -0500

    can you get the loop?

--- src/CodeWars/FiveKyu/FiveKyu.java
+++ src/CodeWars/FiveKyu/FiveKyu.java
@@ -548,6 +548,26 @@ public class FiveKyu {
 
 
 
+    // can you get the loop?
+//    public static int loopSize(Node node) {
+//        Node a = node;
+//        Node b = node.getNext();
+//
+//        while (a != b)
+//        {
+//            a = a.getNext();
+//            b = b.getNext().getNext();
+//        }
+//
+//        int len = 0;
+//
+//        do {
+//            b = b.getNext();
+//            len++;
+//        } while (a != b);
+//
+//        return len;
+//    }
 
 
 
