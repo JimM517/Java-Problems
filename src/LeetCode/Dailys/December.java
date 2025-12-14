f834496de1f0c756faf57e4447cc74d89040f3d2
jim <jamesmagee517@gmail.com>
2025-12-14 09:50:23 -0500

    number of ways to divide a long corridor

--- src/LeetCode/Dailys/December.java
+++ src/LeetCode/Dailys/December.java
@@ -457,6 +457,33 @@ public class December {
 
 
 
+    // 2147. number of ways to divide a long corridor
+    public int numberOfWays(String corridor) {
+
+        final int MOD = 1_000_000_007;
+
+        // Initial values of three variables
+        int zero = 0;
+        int one = 0;
+        int two = 1;
+
+        // Compute using derived equations
+        for (char thing : corridor.toCharArray()) {
+            if (thing == 'S') {
+                zero = one;
+                int temp = one;
+                one = two;
+                two = temp;
+            } else {
+                two = (two + zero) % MOD;
+            }
+        }
+
+        // Return the result
+        return zero;
+
+
+    }
 
 
 
