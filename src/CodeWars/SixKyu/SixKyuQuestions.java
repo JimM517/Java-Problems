01227e6783598ab0fd64764915f6215ec2598137
jim <jamesmagee517@gmail.com>
2025-12-16 17:46:08 -0500

    roman numerals encoder

--- src/CodeWars/SixKyu/SixKyuQuestions.java
+++ src/CodeWars/SixKyu/SixKyuQuestions.java
@@ -1777,6 +1777,24 @@ public static BigInteger binomial(int n, int k) {
 
 
 
+    // roman numerals encoder
+    public static String solution(int n) {
+        int[] intValue = {1000,900,500,400,100,90,50,40,10,9,5,4,1};
+        String[] code = {"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
+
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < intValue.length; i++) {
+            while (n >= intValue[i]) {
+                sb.append(code[i]);
+                n = n - intValue[i];
+            }
+        }
+        return sb.toString();
+    }
+
+
 
 
 
--- src/LeetCodeOneFifty/Arrays/ArrayProblems.java
+++ src/LeetCodeOneFifty/Arrays/ArrayProblems.java
@@ -732,8 +732,8 @@ public class ArrayProblems {
     // 12. Integer to Roman
     public String intToRoman(int num) {
 
-        int intValue[] = {1000,900,500,400,100,90,50,40,10,9,5,4,1};
-        String code[] = {"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
+        int[] intValue = {1000,900,500,400,100,90,50,40,10,9,5,4,1};
+        String[] code = {"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
 
 
         StringBuilder sb = new StringBuilder();
