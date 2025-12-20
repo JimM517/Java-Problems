9cb7fe41fff66f7d21b560b311452b0d78274656
jim <jamesmagee517@gmail.com>
2025-12-20 10:13:59 -0500

    delete columns to make sorted

--- src/LeetCode/Dailys/December.java
+++ src/LeetCode/Dailys/December.java
@@ -726,6 +726,25 @@ class Result {
 
 
 
+        // delete columns to make sorted
+    public int minDeletionSize(String[] strs) {
+
+        int n = strs.length;
+        int m = strs[0].length();
+
+        int count = 0;
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (strs[j].charAt(i) < strs[j - 1].charAt(i)) {
+                    count++;
+                    break;
+                }
+            }
+        }
+        return count;
+
+    }
 
 
 
