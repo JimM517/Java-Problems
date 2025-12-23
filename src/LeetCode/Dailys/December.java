f283e5cc11d28cfed7848bcdc5ff239646565b04
jim <jamesmagee517@gmail.com>
2025-12-22 20:28:10 -0500

    delete columns to make sorted III

--- src/LeetCode/Dailys/December.java
+++ src/LeetCode/Dailys/December.java
@@ -779,6 +779,26 @@ class Result {
 
 
 
+    // 960. Delete columns to make sorted III
+    public int minDeletionSizeThree(String[] strs) {
+        int W = strs[0].length();
+        int[] dp = new int[W];
+        Arrays.fill(dp, 1);
+        for (int i = W-2; i >= 0; --i)
+            search: for (int j = i+1; j < W; ++j) {
+                for (String row: strs)
+                    if (row.charAt(i) > row.charAt(j))
+                        continue search;
+
+                dp[i] = Math.max(dp[i], 1 + dp[j]);
+            }
+
+        int kept = 0;
+        for (int x: dp)
+            kept = Math.max(kept, x);
+        return W - kept;
+    }
+
 
 
 
