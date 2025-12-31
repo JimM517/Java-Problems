658b51d4cd2ae717df6e7c7e5921d63acae8a067
jim <jamesmagee517@gmail.com>
2025-12-31 09:59:56 -0500

    valid braces

--- src/CodeWars/SixKyu/SixKyuQuestions.java
+++ src/CodeWars/SixKyu/SixKyuQuestions.java
@@ -2026,7 +2026,32 @@ public static BigInteger binomial(int n, int k) {
 
 
 
+    // valid braces
+public static boolean isValid(String braces) {
 
+    Stack<Character> stack = new Stack<>();
+    for (char c : braces.toCharArray()) {
+
+        if (c == '(' || c == '[' || c == '{') {
+            stack.push(c);
+        } else if (c == ')' || c == ']' || c == '}') {
+            if (stack.isEmpty()) {
+                return false;
+            }
+
+
+            char top = stack.pop();
+            if ((c == ')' && top != '(' || c == ']' && top != '[' || c == '}' && top != '{')) {
+                return false;
+            }
+
+        }
+
+
+    }
+    return stack.isEmpty();
+
+}
 
 
 
