06ad7e0b2e68c764f436d30df5e28392ca08df47
jim <jamesmagee517@gmail.com>
2026-01-03 09:39:18 -0500

    number of ways to paint a N X 3 grid

--- src/LeetCode/Dailys/TwoSix/January.java
+++ src/LeetCode/Dailys/TwoSix/January.java
@@ -60,6 +60,20 @@ public class January {
 
 
 
+    // 1411. number of ways to paint a N X 3 grid
+    public int numOfWays(int n) {
+        final int MOD = 1_000_000_007;
+        long A = 6, B = 6;
+
+        for (int i = 2; i <= n; i++) {
+            long newA = (2 * A + 2 * B) % MOD;
+            long newB = (2 * A + 3 * B) % MOD;
+            A = newA;
+            B = newB;
+        }
+
+        return (int) ((A + B) % MOD);
+    }
 
 
 
