f784fd81bf855f1bb7676bfd9e48021b2f504e73
jim <jamesmagee517@gmail.com>
2026-01-03 10:16:15 -0500

    pagination helper

--- src/CodeWars/FiveKyu/FiveKyu.java
+++ src/CodeWars/FiveKyu/FiveKyu.java
@@ -826,7 +826,45 @@ public class FiveKyu {
 
 
 
+// paginationHelper
+    public class PaginationHelper<I> {
 
+        private List<I> collection;
+        private int itemsPerPage;
+
+        public PaginationHelper(List<I> collection, int itemsPerPage) {
+            this.collection = collection;
+            this.itemsPerPage = itemsPerPage;
+        }
+
+        public int itemCount() {
+            return collection.size();
+        }
+
+        public int pageCount() {
+
+            return (collection.size() + itemsPerPage - 1) / itemsPerPage;
+
+        }
+
+        public int pageItemCount(int pageIndex) {
+            if (pageIndex < 0 || pageIndex >= pageCount()) {
+                return - 1;
+            }
+
+            if (pageIndex == pageCount() - 1) {
+                return collection.size() - pageIndex * itemsPerPage;
+            }
+            return itemsPerPage;
+        }
+
+        public int pageIndex(int pageIndex) {
+            if (pageIndex < 0 || pageIndex >= collection.size()) {
+                return  - 1;
+            }
+            return pageIndex / itemsPerPage;
+        }
+}
 
 
 
