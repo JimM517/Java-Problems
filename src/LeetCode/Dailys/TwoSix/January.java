6e601674bb51063b1c1082900e048aa22ccd198b
jim <jamesmagee517@gmail.com>
2026-01-04 09:39:31 -0500

    four divisors

--- src/LeetCode/Dailys/TwoSix/January.java
+++ src/LeetCode/Dailys/TwoSix/January.java
@@ -80,6 +80,38 @@ public class January {
 
 
 
+    // 1390. four divisors
+    public int sumFourDivisors(int[] nums) {
+
+        int totalSum = 0;
+
+        for (int n : nums) {
+            int sum = 0;
+            int count = 0;
+
+            for (int d = 1; d * d <= n; d++) {
+                if (n % d == 0) {
+                    int other = n / d;
+
+                    sum += d;
+                    count++;
+
+                    if (other != d) {
+                        sum += other;
+                        count++;
+                    }
+
+                    if (count > 4) break;
+                }
+            }
+
+            if (count == 4) {
+                totalSum += sum;
+            }
+        }
+
+        return totalSum;
+    }
 
 
 
