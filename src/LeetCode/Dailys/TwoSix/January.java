398911579b50ae7f0b621f1697735c6a539dad33
jim <jamesmagee517@gmail.com>
2026-01-05 20:36:12 -0500

    fixed semicolon

--- src/LeetCode/Dailys/TwoSix/January.java
+++ src/LeetCode/Dailys/TwoSix/January.java
@@ -151,7 +151,7 @@ public class January {
         TreeNode left;
         TreeNode right;
         TreeNode(){};
-        TreeNode(int val) { this.val = val};
+        TreeNode(int val) { this.val = val; };
         TreeNode(int val, TreeNode left, TreeNode right) {
             this.val = val;
             this.left = left;
