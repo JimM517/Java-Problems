6ff0ca669a6ccfff6fbebf39b917960e567189ee
jim <jamesmagee517@gmail.com>
2026-01-07 20:23:56 -0500

    max dot product of two subsequences

--- src/LeetCode/Dailys/TwoSix/January.java
+++ src/LeetCode/Dailys/TwoSix/January.java
@@ -231,6 +231,31 @@ public int maxLevelSum(TreeNode root) {
 
 
 
+    // 1458. max dot product of two subsequences
+    public int maxDotProduct(int[] nums1, int[] nums2) {
+
+        int n = nums1.length;
+        int m = nums2.length;
+
+        int NEG = (int)-1e9;
+
+        int[][] dp = new int[n + 1][m + 1];
+
+        for(int i=0;i<=n;i++)
+            for(int j=0;j<=m;j++)
+                dp[i][j] = NEG;
+
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                int take = nums1[i-1]*nums2[j-1] + Math.max(0, dp[i-1][j-1]);
+                dp[i][j] = Math.max(take, Math.max(dp[i-1][j], dp[i][j-1]));
+            }
+        }
+
+        return dp[n][m];
+
+
+    }
 
 
 
