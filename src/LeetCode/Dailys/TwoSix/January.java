65e7bb68e6f0692cd08f3851c72b594df5299f31
jim <jamesmagee517@gmail.com>
2026-01-12 18:20:40 -0500

    minimum time visiting all points

--- src/LeetCode/Dailys/TwoSix/January.java
+++ src/LeetCode/Dailys/TwoSix/January.java
@@ -387,6 +387,34 @@ public int maxLevelSum(TreeNode root) {
 
 
 
+    // 1266. minimum time visiting all points
+    public int minTimeToVisitAllPoints(int[][] points) {
+
+        int answer = 0;
+
+        for (int i = 0; i < points.length - 1; i++) {
+
+            int currX = points[i][0];
+            int currY = points[i][1];
+
+            int targetX = points[i + 1][0];
+            int targetY = points[i + 1][1];
+
+            answer += Math.max(Math.abs(targetX - currX), Math.abs(targetY - currY));
+
+
+        }
+
+
+
+
+        return answer;
+
+
+
+
+    }
+
 
 
 
