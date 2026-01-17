d8e020e33a9d31d8207b17c7a3989dfc0e10e260
jim <jamesmagee517@gmail.com>
2026-01-17 14:49:03 -0500

    observed pins

--- src/CodeWars/FourKyu/FourKyu.java
+++ src/CodeWars/FourKyu/FourKyu.java
@@ -209,7 +209,34 @@ public class FourKyu {
 
 
 
+// observed pins
+    private static final Map<Character, String> ADJACENTS = new HashMap<>() {{
+    put('1', "124");
+    put('2', "2135");
+    put('3', "326");
+    put('4', "4157");
+    put('5', "54268");
+    put('6', "6953");
+    put('7', "748");
+    put('8', "87590");
+    put('9', "986");
+    put('0', "00");
+}};
 
+    public static List<String> getPins(String observed) {
+
+        List<String> ans = Arrays.asList("");
+
+        for (char c: observed.toCharArray()) {
+
+            List<String> tmp = new ArrayList<String>();
+            for (char cc: ADJACENTS.get(c).toCharArray()) {
+                for (String s: ans) tmp.add(s+cc);
+            }
+            ans = tmp;
+        }
+        return ans;
+    }
 
 
 
