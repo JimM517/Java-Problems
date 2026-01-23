229e57273066ec28aaf802ce3346d8758cc4c24d
jim <jamesmagee517@gmail.com>
2026-01-22 20:16:54 -0500

    construct the minimum bitwise array II

--- src/LeetCode/Dailys/TwoSix/January.java
+++ src/LeetCode/Dailys/TwoSix/January.java
@@ -813,6 +813,24 @@ public class January {
 
 
 
+    // 3315. construct the minimum bitwise array II
+    public int[] minBitWiseArrayII(List<Integer> nums) {
+
+        int n = nums.size();
+        int[] result = new int[n];
+        for (int i = 0; i < n; i++) {
+            int x = nums.get(i);
+            int res = -1;
+            int d = 1;
+            while ((x & d) != 0) {
+                res = x - d;
+                d <<= 1;
+            }
+            result[i] = res;
+        }
+        return result;
+    }
+
 
 
 
