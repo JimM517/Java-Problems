7b8e8c911bd691cf08703159cede744fb34f9c16
jim <jamesmagee517@gmail.com>
2026-01-24 09:36:57 -0500

    min max pair sum in array with faster solution

--- src/LeetCode/Dailys/TwoSix/January.java
+++ src/LeetCode/Dailys/TwoSix/January.java
@@ -1028,7 +1028,23 @@ class Node {
 
 
 
+    // minimize max pair sum ->>>>> faster solution
+    public int minPairSumTwo(int[] nums) {
 
+        Arrays.sort(nums);
+
+        int i = 0;
+        int j = nums.length - 1;
+        int maxSum = 0;
+
+        while (i < j) {
+
+            maxSum = Math.max(maxSum, nums[i] + nums[j]);
+            i++;
+            j--;
+        }
+        return maxSum;
+    }
 
 
 
