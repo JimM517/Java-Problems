3f354a4f46419d9e5d75b85fec0f96c18fd054db
jim <jamesmagee517@gmail.com>
2026-01-25 10:04:20 -0500

    min difference between highest and lowest k scores

--- src/LeetCode/Dailys/TwoSix/January.java
+++ src/LeetCode/Dailys/TwoSix/January.java
@@ -1053,6 +1053,22 @@ class Node {
 
 
 
+    // 1984. min difference between highest and lowest of k scores
+    public int minimumDifference(int[] nums, int k) {
+
+        Arrays.sort(nums);
+
+        int n = nums.length;
+        int result = Integer.MAX_VALUE;
+        for (int i = 0; i + k - 1 < n; i++) {
+            result = Math.min(result, nums[i + k - 1] - nums[i]);
+        }
+
+        return result;
+
+
+
+    }
 
 
 
