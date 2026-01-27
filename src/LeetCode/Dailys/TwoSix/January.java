c9afda90e245eac8dc4aa10446a42fc5c13edc18
jim <jamesmagee517@gmail.com>
2026-01-26 19:58:55 -0500

    min absolute difference

--- src/LeetCode/Dailys/TwoSix/January.java
+++ src/LeetCode/Dailys/TwoSix/January.java
@@ -1075,6 +1075,24 @@ class Node {
 
 
 
+    // 1200 minimum absolute difference
+    public List<List<Integer>> minimumAbsDifference(int[] arr) {
+
+        List<List<Integer>> result = new ArrayList<>();
+        Arrays.sort(arr);
+        int min = Integer.MAX_VALUE;
+
+        for (int i = 1; i < arr.length; i++) {
+            min = Math.min(min, arr[i] - arr[i - 1]);
+        }
+
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] - arr[i - 1] == min) {
+                result.add(Arrays.asList(arr[i - 1], arr[i]));
+            }
+        }
+        return result;
+    }
 
 
 
