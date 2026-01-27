7454318153936f5e890fbf639ef417319a7ffe5d
jim <jamesmagee517@gmail.com>
2026-01-27 18:28:22 -0500

    rock paper scissors

--- src/CodeWars/EightKyu/EightKyu.java
+++ src/CodeWars/EightKyu/EightKyu.java
@@ -1241,6 +1241,33 @@ public class EightKyu {
 
 
 
+    // rock paper scissors!
+    public static String rps(String p1, String p2) {
+
+        if (p1.equals("rock") && p2.equals("scissors")) {
+            return "Player 1 won!";
+        }
+
+        if (p1.equals("scissors") && p2.equals("paper")) {
+            return "Player 1 won!";
+        }
+
+        if (p1.equals("paper") && p2.equals("rock")) {
+            return "Player 1 won!";
+        }
+
+        if (p1.equals(p2)) {
+            return "Draw!";
+        }
+
+
+
+        return "Player 2 won!";
+
+
+    }
+
+
 
 
 
