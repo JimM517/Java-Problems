e9a43a34bef4e02cf0901c9366c98551654b5bc4
jim <jamesmagee517@gmail.com>
2026-02-01 10:41:02 -0500

    divide an array into subarrays with minimum cost I

--- src/LeetCode/Dailys/TwoSix/February.java
+++ src/LeetCode/Dailys/TwoSix/February.java
@@ -5,7 +5,25 @@ public class February {
 
 
 
+// 3010. divide an array into subarrays with minimum cost I
+    public int minimumCost(int[] nums) {
 
+        int first = nums[0];
+
+        int min1 = Integer.MAX_VALUE;
+        int min2 = Integer.MAX_VALUE;
+
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] < min1) {
+                min2 = min1;
+                min1 = nums[i];
+            } else if (nums[i] < min2) {
+                min2 = nums[i];
+            }
+        }
+
+        return first + min1 + min2;
+    }
 
 
 
