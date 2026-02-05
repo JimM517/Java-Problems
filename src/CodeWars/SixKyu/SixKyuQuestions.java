a9a289ac1cd9f88863539f4902e745b763d49f40
jim <jamesmagee517@gmail.com>
2026-02-04 20:43:21 -0500

    ackermann function

--- src/CodeWars/SixKyu/SixKyuQuestions.java
+++ src/CodeWars/SixKyu/SixKyuQuestions.java
@@ -2390,6 +2390,22 @@ public static String octopus(String idea) {
 
 
 
+    // ackermann function
+    public static int Ackermann(int m, int n) {
+
+        if (m < 0 || n < 0) {
+            return -1;
+        }
+        if (m == 0) {
+            return n + 1;
+        }
+        if (m > 0 && n == 0) {
+            return Ackermann(m - 1, 1);
+        }
+
+        return Ackermann(m - 1, Ackermann(m, n - 1));
+    }
+
 
 
 
