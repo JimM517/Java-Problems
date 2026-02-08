ad403da7a995a16aad926c63620a700013c97528
jim <jamesmagee517@gmail.com>
2026-02-08 09:55:50 -0500

    balanced binary tree

--- src/LeetCode/Dailys/TwoSix/February.java
+++ src/LeetCode/Dailys/TwoSix/February.java
@@ -298,11 +298,46 @@ public long maxSumTrionic(int[] nums) {
 
 
 
+// 110. balanced binary tree
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(){};
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+}
 
+public boolean isBalanced(TreeNode root) {
 
+        return checkHeight(root) != -1;
+    }
 
+public int checkHeight(TreeNode node) {
 
+        if (node == null) return -0;
 
+        int leftHeight = checkHeight(node.left);
+        if (leftHeight == -1) {
+            return -1;
+        }
+
+        int rightHeight = checkHeight(node.right);
+        if (rightHeight == -1) {
+            return -1;
+        }
+
+        if (Math.abs(leftHeight - rightHeight) > 1) {
+            return -1;
+        }
+
+        return Math.max(leftHeight, rightHeight) + 1;
+
+}
 
 
 
