10d2abf6fe4b47912226b8590894850ca82fd793
jim <jamesmagee517@gmail.com>
2026-02-14 09:32:16 -0500

    length and two values

--- src/CodeWars/SevenKyu/SevenKyu.java
+++ src/CodeWars/SevenKyu/SevenKyu.java
@@ -2280,6 +2280,25 @@ public static int roundToNext5(int number) {
 
 
 
+    // length and two values
+    public static String[] alternate(int n, String firstValue, String secondValue) {
+
+
+        String[] arr = new String[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = (i % 2 == 0) ? firstValue : secondValue;
+        }
+
+
+
+
+
+        return arr;
+
+
+    }
+
 
 
 
