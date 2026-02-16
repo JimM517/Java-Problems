37c7e650c9ea3217c16e817c87e728a78b6367bb
jim <jamesmagee517@gmail.com>
2026-02-16 09:23:40 -0500

    reverse bits

--- src/LeetCode/Dailys/TwoSix/February.java
+++ src/LeetCode/Dailys/TwoSix/February.java
@@ -541,6 +541,20 @@ public int checkHeight(TreeNode node) {
 
 
 
+    // 190. reverse bits
+    public int reverseBits(int n) {
+
+        int result = 0;
+
+        for (int i = 0; i < 32; i++) {
+            result <<= 1;
+            result |= (n & 1);
+            n >>= 1;
+        }
+
+        return result;
+
+    }
 
 
 
