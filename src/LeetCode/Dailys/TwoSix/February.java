e0c870e3b66dd10f1c2182c7836db5aa0567d749
jim <jamesmagee517@gmail.com>
2026-02-24 17:59:47 -0500

    sum of root to leaf binary numbers

--- src/LeetCode/Dailys/TwoSix/February.java
+++ src/LeetCode/Dailys/TwoSix/February.java
@@ -749,11 +749,41 @@ public int checkHeight(TreeNode node) {
 
 
 
+    // 1022. sum of root to leaf binary numbers
+//    public class TreeNode {
+//        int val;
+//        TreeNode left;
+//        TreeNode right;
+//        TreeNode(){};
+//        TreeNode(int val) { this.val = val; }
+//        TreeNode(int val, TreeNode left, TreeNode right) {
+//            this.val = val;
+//            this.left = left;
+//            this.right = right;
+//        }
+//    }
+
+public int sumRootToLeaf(TreeNode root) {
+
+    return dfs(root, 0);
 
 
+}
 
 
+private int dfs(TreeNode node, int current) {
+        if (node == null) {
+            return 0;
+        }
 
+        current = current * 2 + node.val;
+
+        if (node.left == null && node.right == null) {
+            return current;
+        }
+
+        return dfs(node.left, current) + dfs(node.right, current);
+}
 
 
 
