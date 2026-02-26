fc1ed2717c119e3d3abadfd0334d1dcc4d1269c2
jim <jamesmagee517@gmail.com>
2026-02-25 20:29:11 -0500

    num steps to reduce num in binary rep to one

--- src/LeetCode/Dailys/TwoSix/February.java
+++ src/LeetCode/Dailys/TwoSix/February.java
@@ -789,6 +789,47 @@ private int dfs(TreeNode node, int current) {
 
 
 
+// 1404. number of steps to reduce a number in binary representation to one
+    public int numSteps(String s) {
+        int N = s.length();
+
+        int operations = 0;
+        int carry = 0;
+
+        for (int i = N - 1; i > 0; i--) {
+            int digit = Character.getNumericValue(s.charAt(i)) + carry;
+            if (digit % 2 == 1) {
+                operations += 2;
+                carry = 1;
+            } else {
+                operations++;
+            }
+
+        }
+        return operations + carry;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
