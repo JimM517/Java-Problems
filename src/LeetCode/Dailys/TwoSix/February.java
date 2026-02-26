40b02aa18f52650abd767c0fb5e4e37be2067451
jim <jamesmagee517@gmail.com>
2026-02-25 20:33:30 -0500

    sort ints by the number of 1 bits

--- src/LeetCode/Dailys/TwoSix/February.java
+++ src/LeetCode/Dailys/TwoSix/February.java
@@ -789,6 +789,49 @@ private int dfs(TreeNode node, int current) {
 
 
 
+
+
+
+
+
+
+
+// 1356. sort integers by the number of 1 bits
+    public int[] sortByBits(int[] arr) {
+        Integer[] nums = Arrays.stream(arr).boxed().toArray(Integer[]::new);
+        Comparator<Integer> comparator = new CustomComparator();
+        Arrays.sort(nums, comparator);
+        return Arrays.stream(nums).mapToInt(Integer::intValue).toArray();
+    }
+
+
+
+    class CustomComparator implements Comparator<Integer> {
+        @Override
+        public int compare(Integer a, Integer b) {
+            if (Integer.bitCount(a) == Integer.bitCount(b)) {
+                return a - b;
+            }
+
+            return Integer.bitCount(a) - Integer.bitCount(b);
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // 1404. number of steps to reduce a number in binary representation to one
     public int numSteps(String s) {
         int N = s.length();
