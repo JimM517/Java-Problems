05deee5fe3430ad0e053845fc7c61711127447e9
jim <jamesmagee517@gmail.com>
2026-03-01 14:23:50 -0500

    partitioning into minimum number of deci-binary numbers

--- src/LeetCode/Dailys/TwoSix/March.java
+++ src/LeetCode/Dailys/TwoSix/March.java
@@ -1,4 +1,126 @@
 package LeetCode.Dailys.TwoSix;
 
 public class March {
+
+
+
+
+    // 1689. partitioning into minimum number of deci-binary numbers
+    public int minPartitions(String n) {
+
+        int minPartitions = Integer.MIN_VALUE;
+
+        for (char c : n.toCharArray()) {
+
+            int current = Character.getNumericValue(c);
+
+            minPartitions = Math.max(minPartitions, current);
+
+        }
+
+        return minPartitions;
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
