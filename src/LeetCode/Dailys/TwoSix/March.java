ac2d9bb78f6588226a1deeab2bb2bb37f74cb2f4
jim <jamesmagee517@gmail.com>
2026-03-02 18:25:46 -0500

    min swaps to arrange binary grid

--- src/LeetCode/Dailys/TwoSix/March.java
+++ src/LeetCode/Dailys/TwoSix/March.java
@@ -1,5 +1,7 @@
 package LeetCode.Dailys.TwoSix;
 
+import java.util.Arrays;
+
 public class March {
 
 
@@ -26,7 +28,45 @@ public class March {
 
 
 
+// 1536. minimum swaps to arrange binary grid
+    public int minSwaps(int[][] grid) {
 
+        int n = grid.length;
+        int[] pos = new int[n];
+
+        Arrays.fill(pos, -1);
+        for (int i = 0; i < n; i++) {
+            for (int j = n - 1; j >= 0; j--) {
+                if (grid[i][j] == 1) {
+                    pos[i] = j;
+                    break;
+                }
+            }
+        }
+        int answer = 0;
+        for (int i = 0; i < n; i++) {
+            int k = -1;
+            for (int j = i; j < n; j++) {
+                if (pos[j] <= i) {
+                    answer += j - i;
+                    k = j;
+                    break;
+                }
+            }
+            if (k >= 0) {
+                for (int j = k; j > i; j--) {
+                    int temp = pos[j];
+                    pos[j] = pos[j - 1];
+                    pos[j - 1] = temp;
+                }
+            } else {
+                return -1;
+            }
+        }
+
+        return answer;
+
+    }
 
 
 
