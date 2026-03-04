e5738953827ac62c30a7196d84e2c32bae1fbbcd
jim <jamesmagee517@gmail.com>
2026-03-03 19:59:02 -0500

    find kth bit in Nth binary string

--- src/LeetCode/Dailys/TwoSix/March.java
+++ src/LeetCode/Dailys/TwoSix/March.java
@@ -72,6 +72,24 @@ public class March {
 
 
 
+    // 1545 find kth bit in nth binary string
+    public char findKthBit(int n, int k) {
+
+        StringBuilder sequence = new StringBuilder("0");
+
+        for (int i = 1; i < n && k > sequence.length(); i++) {
+            sequence.append("1");
+
+            for (int j = sequence.length() - 2; j >= 0; j--) {
+                char invertedBit = (sequence.charAt(j) == '1') ? '0' : '1';
+                sequence.append(invertedBit);
+            }
+        }
+
+        return sequence.charAt(k - 1);
+
+
+    }
 
 
 
