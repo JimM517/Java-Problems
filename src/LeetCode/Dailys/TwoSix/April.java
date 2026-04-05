f8c5fc6f27334bfe9647f40b88d1f53d671475c9
jim <jamesmagee517@gmail.com>
2026-04-05 09:52:14 -0400

    robot return to origin

--- src/LeetCode/Dailys/TwoSix/April.java
+++ src/LeetCode/Dailys/TwoSix/April.java
@@ -5,7 +5,34 @@ public class April {
 
 
 
+// 657. robot return to origin
+    public boolean judgeCircle(String moves) {
 
+        int x = 0;
+        int y = 0;
+
+        for (int i = 0; i < moves.length(); i++) {
+
+            if (moves.charAt(i) == 'R') {
+                x++;
+            }
+            else if (moves.charAt(i) == 'L') {
+                x--;
+            }
+            else if (moves.charAt(i) == 'U') {
+                y++;
+            }
+            else if (moves.charAt(i) == 'D') {
+                y--;
+            }
+
+        }
+
+
+
+        return x == 0 && y == 0;
+
+    }
 
 
 
