a8e5fa737e9a2b4e514177ed24cd0b3688268388
James Magee <james@jamess-air.myfiosgateway.com>
2026-05-30 13:22:36 -0400

    find smallest int in array

--- src/CodeWars/Duplicates.java
+++ src/CodeWars/Duplicates.java
@@ -82,7 +82,20 @@ public class Duplicates {
     }
 
 
+    // find the smallest integer in the array
+    public static int findSmallestInt(int[] args) {
 
+        int min = Integer.MAX_VALUE;
+
+        for (int i : args) {
+            min = Math.min(min, i);
+        }
+
+        return min;
+
+
+
+    }
 
 
 
