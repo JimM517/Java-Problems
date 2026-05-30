a3610cf388c689a035545b92c24efa7a16952494
James Magee <james@jamess-air.myfiosgateway.com>
2026-05-30 08:21:03 -0400

    two sum

--- src/CodeWars/Duplicates.java
+++ src/CodeWars/Duplicates.java
@@ -1,4 +1,151 @@
 package CodeWars;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Duplicates {
+
+
+
+
+    // two sum
+    public static int[] twoSum(int[] numbers, int target) {
+
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < numbers.length; i++) {
+
+            if (map.containsKey(target - numbers[i])) {
+                return new int[] {map.get(target - numbers[i]), i};
+            } else {
+                map.put(numbers[i], i);
+            }
+
+
+        }
+
+
+        return new int[]{-1, -1};
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
