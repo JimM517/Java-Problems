658ee5f92b515620434c7665a53f5b44db4243f2
James Magee <james@jamess-air.myfiosgateway.com>
2026-06-02 19:29:24 -0400

    maximum contiguos sum

--- src/CodeWars/Duplicates.java
+++ src/CodeWars/Duplicates.java
@@ -139,6 +139,24 @@ public class Duplicates {
 
 
 
+    // maximum contiguous sum
+    public static int maxContiguousSum(final int[] arr) {
+
+        int current = 0;
+        int max = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            current += arr[i];
+            if (current > max) {
+                max = current;
+            }
+            if (current < 0) {
+                current = 0;
+            }
+        }
+        return max;
+
+    }
 
 
 
