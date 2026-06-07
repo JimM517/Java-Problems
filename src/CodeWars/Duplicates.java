deae231c1a15444c212b6c16d1b403567a279868
James Magee <james@jamess-air.myfiosgateway.com>
2026-06-07 13:39:38 -0400

    sum positive

--- src/CodeWars/Duplicates.java
+++ src/CodeWars/Duplicates.java
@@ -164,7 +164,19 @@ public class Duplicates {
 
 
 
+    // sum of positive
+    public static int sum(int[] arr) {
 
+        int total = 0;
+        for (int num : arr) {
+            if (num > 0) {
+                total += num;
+            }
+        }
+
+        return total;
+
+    }
 
 
 
