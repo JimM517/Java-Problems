c8c476f66ad1faf02ae2663327b5d6d941dabb99
James Magee <james@jamess-air.myfiosgateway.com>
2026-06-19 13:13:33 -0400

    area of a circle

--- src/CodeWars/SevenKyu/SevenKyu.java
+++ src/CodeWars/SevenKyu/SevenKyu.java
@@ -2694,6 +2694,18 @@ public static int roundToNext5(int number) {
 
 
 
+    // area of a circle
+    //pie r^2
+    public static double area(double radius) {
+
+        if (radius <= 0) {
+            throw new IllegalArgumentException();
+        }
+
+        return Math.PI * Math.pow(radius, 2);
+
+    }
+
 
 
 
