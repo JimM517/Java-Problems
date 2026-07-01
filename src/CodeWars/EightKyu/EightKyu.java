c06eb7f3a8d463f8dc4604d97a0737a053a7538b
James Magee <james@jamess-air.myfiosgateway.com>
2026-07-01 19:32:25 -0400

    leonardo dicaprio and oscars

--- src/CodeWars/EightKyu/EightKyu.java
+++ src/CodeWars/EightKyu/EightKyu.java
@@ -1633,6 +1633,23 @@ public class EightKyu {
 
 
 
+    // leonardo dicaprio and oscars
+    public static String leo(final int oscar) {
+
+        if (oscar == 88) {
+            return "Leo finally won the oscar! Leo is happy";
+        } else if (oscar == 86) {
+            return "Not even for Wolf of wallstreet?!";
+        } else if (oscar < 88) {
+            return "When will you give Leo an Oscar?";
+        } else {
+            return "Leo got one already!";
+        }
+
+
+
+
+    }
 
 
 
